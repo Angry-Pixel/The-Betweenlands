a81978ad818f69d033f8b02e3a658e3256379b04
TheCyberBrick <samuelboerlin@bluewin.ch>
2015-02-11 19:16:12 +0100

    Initial commit

---
+++ java/thebetweenlands/TheBetweenlands.java
@@ -0,0 +1,8 @@
+package thebetweenlands;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "TheBetweenlands", name = "The Betweenlands", version = "1.7.10-1.0")
+public class TheBetweenlands {
+
+}
