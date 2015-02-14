fca8bf4d2a805e99ebadf12605f1c61375932542
vadis365 <curve_linear@hotmail.com>
2015-02-14 16:26:15 +0000

    tile entity register

--- java/thebetweenlands/proxy/CommonProxy.java
+++ java/thebetweenlands/proxy/CommonProxy.java
@@ -1,11 +1,12 @@
 package thebetweenlands.proxy;
 
 import net.minecraft.tileentity.TileEntity;
+import thebetweenlands.tileentities.TileEntityDruidAltar;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
 	public void registerTileEntities() {
-		//registerTileEntity(TileEntityName.class, "tileEntityName"); **EXAMPLE**
+		registerTileEntity(TileEntityDruidAltar.class, "druidAltar");
 	}
 
 	private void registerTileEntity(Class<? extends TileEntity> cls, String baseName) {
