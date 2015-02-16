8c29213e35f3ebac0231450ca44c2b948b1db946
vadis365 <curve_linear@hotmail.com>
2015-02-16 20:21:39 +0000

    don't need this it is unbreakable

--- java/thebetweenlands/blocks/terrain/BlockBetweenlandsBedrock.java
+++ java/thebetweenlands/blocks/terrain/BlockBetweenlandsBedrock.java
@@ -7,7 +7,6 @@ import thebetweenlands.creativetabs.ModCreativeTabs;
 public class BlockBetweenlandsBedrock extends Block {
 	public BlockBetweenlandsBedrock() {
 		super(Material.rock);
-		setHardness(6000000.0F);
 		setResistance(6000000.0F);
 		setStepSound(soundTypePiston);
 		disableStats();
