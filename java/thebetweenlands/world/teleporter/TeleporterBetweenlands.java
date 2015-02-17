694de0c7abcf3f729c64ea971ccca0a08f0d5f8c
vadis365 <curve_linear@hotmail.com>
2015-02-17 00:59:14 +0000

    teleporter class
    just wanted to stop nether portals in my overworld test

--- java/thebetweenlands/world/teleporter/TeleporterBetweenlands.java
+++ java/thebetweenlands/world/teleporter/TeleporterBetweenlands.java
@@ -1,13 +1,18 @@
 package thebetweenlands.world.teleporter;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
 
-public class TeleporterBetweenlands
-        extends Teleporter
+public class TeleporterBetweenlands extends Teleporter
 {
 	//TODO: No special functionality yet, just for testing purposes
 	public TeleporterBetweenlands(WorldServer worldServer) {
 		super(worldServer);
+	}
+
+	//Just putting this here to stop nether portals appearing in the overworld
+	@Override
+	public void placeInPortal(Entity pEntity, double posX, double posY, double posZ, float rotationYaw) {
 	}
 }
