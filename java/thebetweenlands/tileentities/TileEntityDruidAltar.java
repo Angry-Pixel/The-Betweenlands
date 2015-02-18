f5a2a7abc0b46394dbc7794c86b9e513d8ba53ef
vadis365 <curve_linear@hotmail.com>
2015-02-18 21:13:31 +0000

    looks pretty cool (and works)

--- java/thebetweenlands/tileentities/TileEntityDruidAltar.java
+++ java/thebetweenlands/tileentities/TileEntityDruidAltar.java
@@ -31,6 +31,7 @@ public class TileEntityDruidAltar extends TileEntityBasicInventory  {
 	private static final float ROTATION_SPEED = 2.0F;
 
 	public int craftingProgress = 0;
+	private boolean circleShouldRevert = true;
 
 	//11 seconds crafting time
 	public static final int CRAFTING_TIME = 20*11;
@@ -41,6 +42,12 @@ public class TileEntityDruidAltar extends TileEntityBasicInventory  {
 
 	@Override
 	public void updateEntity() {
+		if(!worldObj.isRemote && circleShouldRevert) {
+			checkDruidCircleMeta(worldObj);
+			circleShouldRevert = false;
+			System.out.println("Reverted Circle Blocks");
+		}
+		
 		if (worldObj.isRemote) {
 			rotation += ROTATION_SPEED;
 			if (rotation >= 360.0F) {
@@ -144,22 +151,34 @@ public class TileEntityDruidAltar extends TileEntityBasicInventory  {
 		TheBetweenlands.networkWrapper.sendToAllAround(new AltarCraftingProgressMessage(xCoord, yCoord, zCoord, -1), new TargetPoint(dim, xCoord + 0.5D, yCoord + 0.5D, zCoord + 0.5D, 64D));
 		//Sets client crafting progress to 1
 		TheBetweenlands.networkWrapper.sendToAllAround(new AltarCraftingProgressMessage(xCoord, yCoord, zCoord, 1), new TargetPoint(dim, xCoord + 0.5D, yCoord + 0.5D, zCoord + 0.5D, 64D));
-	
-		for(int x = -8; x < 8; x++) {
-        	for(int y = -8; y < 8; y++) {
-        		for(int z = -8; z < 8; z++) {
-                	Block block = world.getBlock(this.xCoord + x, this.yCoord + y, this.zCoord + z);
-                	if(block == BLBlockRegistry.druidStone1
-                		|| block == BLBlockRegistry.druidStone2
-                		|| block == BLBlockRegistry.druidStone3
-                		|| block == BLBlockRegistry.druidStone4
-                		|| block == BLBlockRegistry.druidStone5) {
-                		int meta = world.getBlockMetadata(this.xCoord + x, this.yCoord + y, this.zCoord + z);
-                		world.setBlockMetadataWithNotify(this.xCoord + x, this.yCoord + y, this.zCoord + z, meta + 4, 2);
-                	}
-                }
-            }
-        }
+		//Does the metadata stuff for the circle animated textures
+		checkDruidCircleMeta(world);
+	}
+
+	private void checkDruidCircleMeta(World world) {
+		int baseRadius = 6;
+		for (int y = 0; y < 6; y++) {
+			for (int x = baseRadius * - 1; x <= baseRadius; ++x) {
+				for (int z = baseRadius * - 1; z <= baseRadius; ++z) {
+					double dSq = x * x + z * z;
+	                if( Math.round(Math.sqrt(dSq)) == baseRadius) {
+					Block block = world.getBlock(xCoord + x, yCoord + y, zCoord + z);
+					if (block == BLBlockRegistry.druidStone1
+							|| block == BLBlockRegistry.druidStone2
+							|| block == BLBlockRegistry.druidStone3
+							|| block == BLBlockRegistry.druidStone4
+							|| block == BLBlockRegistry.druidStone5) {
+						int meta = world.getBlockMetadata(xCoord + x, yCoord + y, zCoord + z);
+						if (craftingProgress == 0 && meta >= 4 || circleShouldRevert && meta >= 4) {
+							world.setBlockMetadataWithNotify(this.xCoord + x, yCoord + y, zCoord + z, meta - 4, 3);
+						} else if (craftingProgress == 1 && meta < 4) {
+							world.setBlockMetadataWithNotify(this.xCoord + x, yCoord + y, zCoord + z, meta + 4, 3);
+						}
+					}
+				}
+			}
+		}
+		}
 	}
 
 	private void stopCraftingProcess() {
@@ -174,22 +193,8 @@ public class TileEntityDruidAltar extends TileEntityBasicInventory  {
 		TheBetweenlands.networkWrapper.sendToAllAround(new AltarCraftingProgressMessage(xCoord, yCoord, zCoord, -2), new TargetPoint(dim, xCoord + 0.5D, yCoord + 0.5D, zCoord + 0.5D, 64D));
 		//Sets client crafting progress to 0
 		TheBetweenlands.networkWrapper.sendToAllAround(new AltarCraftingProgressMessage(xCoord, yCoord, zCoord, 0), new TargetPoint(dim, xCoord + 0.5D, yCoord + 0.5D, zCoord + 0.5D, 64D));
-	
-		for(int x = -8; x < 8; x++) {
-        	for(int y = -8; y < 8; y++) {
-        		for(int z = -8; z < 8; z++) {
-                	Block block = world.getBlock(this.xCoord + x, this.yCoord + y, this.zCoord + z);
-                	if(block == BLBlockRegistry.druidStone1
-                		|| block == BLBlockRegistry.druidStone2
-                		|| block == BLBlockRegistry.druidStone3
-                		|| block == BLBlockRegistry.druidStone4
-                		|| block == BLBlockRegistry.druidStone5) {
-                		int meta = world.getBlockMetadata(this.xCoord + x, this.yCoord + y, this.zCoord + z);
-                		world.setBlockMetadataWithNotify(this.xCoord + x, this.yCoord + y, this.zCoord + z, meta - 4, 2);
-                	}
-                }
-            }
-        }
+		//Does the metadata stuff for the circle animated textures
+		checkDruidCircleMeta(world);
 	}
 
 	public void sendCraftingProgressPacket() {
