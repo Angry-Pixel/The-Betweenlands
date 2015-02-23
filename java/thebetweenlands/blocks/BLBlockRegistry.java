77989126c54000b8dfdda590afdaa86d09d12cb6
vadis365 <curve_linear@hotmail.com>
2015-02-23 01:42:08 +0000

    another note to devs

--- java/thebetweenlands/blocks/BLBlockRegistry.java
+++ java/thebetweenlands/blocks/BLBlockRegistry.java
@@ -48,7 +48,7 @@ public class BLBlockRegistry
 	public static final Block aquaMiddleGemOre = new BlockGenericOre("aquaMiddleGemOre", EnumMaterialsBL.AQUA_MIDDLE_GEM);
 	public static final Block crimsonMiddleGemOre = new BlockGenericOre("crimsonMiddleGemOre", EnumMaterialsBL.CRIMSON_MIDDLE_GEM);
 	public static final Block greenMiddleGemOre = new BlockGenericOre("greenMiddleGemOre", EnumMaterialsBL.GREEN_MIDDLE_GEM);
-	public static final Block octineOre = new BlockGenericOre("octineOre", null);
+	public static final Block octineOre = new BlockGenericOre("octineOre", null); //setting null drops item block
 	public static final Block sulfurOre = new BlockGenericOre("sulfurOre", EnumMaterialsBL.SULFUR);
 	public static final Block valoniteOre = new BlockGenericOre("valoniteOre", EnumMaterialsBL.VALONITE_SHARD);
 	public static final Block lifeCrystalOre = new BlockGenericOre("lifeCrystalOre", EnumMaterialsBL.LIFE_CRYSTAL);
