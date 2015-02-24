0bb0504a2354477562f5bea782d1de681f813b6b
vadis365 <curve_linear@hotmail.com>
2015-02-24 19:44:19 +0000

    removed druid spawner item drop

--- java/thebetweenlands/blocks/BlockDruidSpawner.java
+++ java/thebetweenlands/blocks/BlockDruidSpawner.java
@@ -1,14 +1,9 @@
 package thebetweenlands.blocks;
 
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import thebetweenlands.creativetabs.ModCreativeTabs;
 
-import java.util.Random;
+public class BlockDruidSpawner extends BlockSpawner {
 
-public class BlockDruidSpawner
-        extends BlockSpawner
-{
 	public BlockDruidSpawner(String mobName) {
 		super("thebetweenlands." + mobName);
 		setHarvestLevel("pickaxe", 0);
@@ -17,13 +12,4 @@ public class BlockDruidSpawner
 		setCreativeTab(ModCreativeTabs.blocks);
 	}
 
-	@Override
-	public Item getItemDropped(int id, Random rand, int fortune) {
-		return Items.magma_cream;
-	}
-
-	@Override
-	public int quantityDropped(Random rand) {
-		return 1 + rand.nextInt(3);
-	}
 }
