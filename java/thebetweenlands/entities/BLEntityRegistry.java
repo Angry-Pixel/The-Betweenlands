eefc4d2378f5a50e6f1bbad7a48184e3ed644fb9
vadis365 <curve_linear@hotmail.com>
2015-03-05 22:46:40 +0000

    Eggs Closes #56

--- java/thebetweenlands/entities/BLEntityRegistry.java
+++ java/thebetweenlands/entities/BLEntityRegistry.java
@@ -19,12 +19,12 @@ public class BLEntityRegistry
         // EntityRegistry.registerModEntity(EntityDarkDruid.class, "darkDruid", 2, TheBetweenlands.instance, 30, 3, true);
 
         // Entity registrations can be with or without spawn egg for non mob entites eg. projectiles etc.
-        registerEntity(0, EntityDarkDruid.class, "darkDruid", -1251634, -13032944);
-        registerEntity(1, EntityAngler.class, "angler", -1251634, -13032944);
-        registerEntity(2, EntitySludge.class, "sludge", -1251634, -13032944);
-        registerEntity(3, EntitySwampHag.class, "swampHag", -1251634, -13032944);
-        registerEntity(4, EntityTarBeast.class, "tarBeast", -1251634, -13032944);
-        registerEntity(5, EntityWight.class, "wight", -1251634, -13032944);
+        registerEntity(0, EntityDarkDruid.class, "darkDruid", 0x000000, 0xFF0000);
+        registerEntity(1, EntityAngler.class, "angler", 0x243B0B, 0x00FFFF);
+        registerEntity(2, EntitySludge.class, "sludge", 0x3A2F0B, 0x5F4C0B);
+        registerEntity(3, EntitySwampHag.class, "swampHag", 0x0B3B0B, 0xDBA901);
+        registerEntity(4, EntityTarBeast.class, "tarBeast", 0x000000, 0x2E2E2E);
+        registerEntity(5, EntityWight.class, "wight", 0xECF8E0, 0x243B0B);
     }
 
     private static final void registerEntity(int id, Class<? extends Entity> entityClass, String name) {
