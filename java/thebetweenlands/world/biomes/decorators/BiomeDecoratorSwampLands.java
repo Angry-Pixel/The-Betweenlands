021931f07c7f40dce65e77e837c8022fb6247e94
vadis365 <curve_linear@hotmail.com>
2015-03-14 20:46:48 +0000

    tree gen

--- java/thebetweenlands/world/biomes/decorators/BiomeDecoratorSwampLands.java
+++ java/thebetweenlands/world/biomes/decorators/BiomeDecoratorSwampLands.java
@@ -1,9 +1,34 @@
 package thebetweenlands.world.biomes.decorators;
 
+import net.minecraft.world.gen.feature.WorldGenerator;
 import thebetweenlands.world.biomes.decorators.base.BiomeDecoratorBaseBetweenlands;
+import thebetweenlands.world.biomes.decorators.data.SurfaceType;
+import thebetweenlands.world.feature.trees.WorldGenSapTree;
+import thebetweenlands.world.feature.trees.WorldGenWeedWoodTree;
 
-public class BiomeDecoratorSwampLands
-        extends BiomeDecoratorBaseBetweenlands
-{
+public class BiomeDecoratorSwampLands extends BiomeDecoratorBaseBetweenlands {
+	
+	private final WorldGenerator genTreeWeedwood = new WorldGenWeedWoodTree();
+	private final WorldGenerator genTreeSap = new WorldGenSapTree();
+	
+	@Override
+	public void decorate() {
+		for (attempt = 0; attempt < 120; attempt++) {
+			xx = x + rand.nextInt(5) + 12;
+			yy = 80 + rand.nextInt(15);
+			zz = z + rand.nextInt(5) + 12;
 
+			if (checkSurface(SurfaceType.SWAMP_GRASS, xx, yy, zz) && checkSurface(SurfaceType.SWAMP_GRASS, xx - 2, yy, zz - 2) && checkSurface(SurfaceType.SWAMP_GRASS, xx + 2, yy, zz + 2) && checkSurface(SurfaceType.SWAMP_GRASS, xx + 2, yy, zz - 2) && checkSurface(SurfaceType.SWAMP_GRASS, xx - 2, yy, zz + 2))
+			genTreeWeedwood.generate(world, rand, xx, yy, zz);
+		}
+		
+		for (attempt = 0; attempt < 50; attempt++) {
+			xx = x + offsetXZ();
+			yy = 80 + rand.nextInt(15);
+			zz = z + offsetXZ();
+
+			if (checkSurface(SurfaceType.SWAMP_GRASS, xx, yy, zz) && checkSurface(SurfaceType.SWAMP_GRASS, xx - 1, yy, zz - 1) && checkSurface(SurfaceType.SWAMP_GRASS, xx + 1, yy, zz + 1) && checkSurface(SurfaceType.SWAMP_GRASS, xx + 1, yy, zz - 1) && checkSurface(SurfaceType.SWAMP_GRASS, xx - 1, yy, zz + 1))
+			genTreeSap.generate(world, rand, xx, yy, zz);
+		}
+	}
 }
---
+++ java/thebetweenlands/world/biomes/decorators/data/SurfaceType.java
@@ -0,0 +1,30 @@
+package thebetweenlands.world.biomes.decorators.data;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import thebetweenlands.blocks.BLBlockRegistry;
+
+public enum SurfaceType {
+	SWAMP_GRASS,
+	GRASS,
+	DIRT,
+	SAND,
+	MIXED;
+
+	public boolean matchBlock(Block block) {
+		switch (this) {
+			case SWAMP_GRASS:
+			return block == BLBlockRegistry.swampGrass || block == Blocks.mycelium;
+			case GRASS:
+				return block == Blocks.grass || block == Blocks.mycelium;
+			case DIRT:
+				return block == BLBlockRegistry.swampDirt || block == Blocks.dirt;
+			case SAND:
+				return block == Blocks.sand;
+			case MIXED:
+				return block == Blocks.grass || block == Blocks.dirt || block == Blocks.sand || block == BLBlockRegistry.swampGrass || block == BLBlockRegistry.swampDirt;
+			default:
+				return false;
+		}
+	}
+}
