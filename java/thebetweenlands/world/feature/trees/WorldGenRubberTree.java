b12701a711fb21f37ac093f8c96d9c020c3c16ea
vadis365 <curve_linear@hotmail.com>
2015-03-20 21:59:21 +0000

    rubber tree changed completely

--- java/thebetweenlands/world/feature/trees/WorldGenRubberTree.java
+++ java/thebetweenlands/world/feature/trees/WorldGenRubberTree.java
@@ -2,7 +2,6 @@ package thebetweenlands.world.feature.trees;
 
 import java.util.Random;
 
-import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import thebetweenlands.blocks.BLBlockRegistry;
@@ -13,13 +12,11 @@ public class WorldGenRubberTree extends WorldGenerator {
 
 	private BlockBLLog log = (BlockBLLog) BLBlockRegistry.rubberTreeLog;
 	private BlockBLLeaves leaves = (BlockBLLeaves) BLBlockRegistry.rubberTreeLeaves;
-	private boolean alternate;
 
 	public boolean generate(World world, Random rand, int x, int y, int z) {
 
-		int height = rand.nextInt(8) + 16;
-		int maxRadius = 5;
-		alternate = rand.nextBoolean();
+		int height = rand.nextInt(8) + 8;
+		int maxRadius = 4;
 
 		for (int xx = x - maxRadius; xx <= x + maxRadius; xx++)
 			for (int zz = z - maxRadius; zz <= z + maxRadius; zz++)
@@ -31,32 +28,13 @@ public class WorldGenRubberTree extends WorldGenerator {
 			world.setBlock(x, yy, z, log, 0, 2);
 
 			if (yy == y + height - 1)
-				createLeaves(world, x, yy, z);
+				createMainCanopy(world, rand, x, yy, z, maxRadius);
 
-			if (yy == y + height - 8 || yy == y + height - 12) {
-				if (alternate) {
-					createBranch(world, rand, x + 1, yy - rand.nextInt(2), z, 1, 1);
-					createBranch(world, rand, x- 1, yy - rand.nextInt(2), z, 2, 1);
-					alternate = false;
-
-				} else {
-					createBranch(world, rand, x, yy - rand.nextInt(2), z + 1, 3, 1);
-					createBranch(world, rand, x, yy - rand.nextInt(2), z - 1, 4, 1);
-					alternate = true;
-				}
-			}
-
-			if (yy == y + height - 4) {
-				if (alternate) {
-					createBranch(world, rand, x + 1, yy- rand.nextInt(2), z, 1, 1);
-					createBranch(world, rand, x - 1, yy- rand.nextInt(2), z, 2, 1);
-					alternate = false;
-
-				} else {
-					createBranch(world, rand, x, yy- rand.nextInt(2), z + 1, 3, 1);
-					createBranch(world, rand, x, yy- rand.nextInt(2), z - 1, 4, 1);
-					alternate = true;
-				}
+			if (yy == y + height - 2) {
+				createBranch(world, rand, x + 1, yy, z, 1, 1);
+				createBranch(world, rand, x- 1, yy, z, 2, 1);
+				createBranch(world, rand, x, yy, z + 1, 3, 1);
+				createBranch(world, rand, x, yy, z - 1, 4, 1);
 			}
 		}
 		return true;
@@ -71,49 +49,30 @@ public class WorldGenRubberTree extends WorldGenerator {
 				meta = 0;
 			}
 
-			if (dir == 1) {
+			if (dir == 1)
 				world.setBlock(x + i, y, z, log, meta == 0 ? 0 : 4, 2);
-				if(i == branchLength) {
-					createLeaves(world, x + i, y, z);
-					placeLeaves(world, x + i + 2, y, z);
-				}
-			}
 
-			if (dir == 2) {
+			if (dir == 2)
 				world.setBlock(x - i, y, z, log, meta == 0 ? 0 : 4, 2);
-				if(i == branchLength) {
-					createLeaves(world, x - i, y, z);
-					placeLeaves(world, x - i - 2, y, z);
-				}
-			}
 
-			if (dir == 3) {
+			if (dir == 3)
 				world.setBlock(x, y, z + i, log,  meta == 0 ? 0 : 8, 2);
-				if(i == branchLength) {
-					createLeaves(world, x, y, z + i);
-					placeLeaves(world, x, y, z + i + 2);
-				}
-			}
 
-			if (dir == 4) {
+			if (dir == 4)
 				world.setBlock(x, y, z - i, log,  meta == 0 ? 0 : 8, 2);
-				if(i == branchLength) {
-					createLeaves(world, x, y, z - i);
-					placeLeaves(world, x, y, z - i - 2);
-				}
-			}
+
 		}
 	}
 
-	private void createLeaves(World world, int x, int y, int z) {
-		for(int d = - 1; d < 2; d++)
-			for(int d2 = - 1; d2 < 2; d2++)
-				placeLeaves(world, x + d, y, z + d2);
-	}
-
-	private void placeLeaves(World world, int x, int y, int z) {
-		if (world.getBlock(x, y, z) == Blocks.air)
-			world.setBlock(x, y, z, leaves);
-	}
+	private void createMainCanopy(World world, Random rand, int x, int y, int z, int maxRadius) {
+		for (int x1 = x - maxRadius; x1 <= x + maxRadius; x1++)
+			for (int z1 = z - maxRadius; z1 <= z + maxRadius; z1++)
+				for (int y1 = y; y1 < y + maxRadius; y1++) {
+					double dSq = Math.pow(x1 - x, 2.0D) + Math.pow(z1 - z, 2.0D) + Math.pow(y1 - y, 2.0D);
+					if (Math.round(Math.sqrt(dSq)) <= maxRadius)
+						if (world.getBlock(x1, y1, z1) != log && rand.nextInt(5) != 0)
+							world.setBlock(x1, y1, z1, leaves);
+				}
+		}
 
 }
