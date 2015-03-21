6dc18cbeaee7638216cd206484616e90ef033b78
TheCyberBrick <samuelboerlin@bluewin.ch>
2015-03-21 19:29:58 +0100

    Derp fix

--- java/thebetweenlands/blocks/BlockSludge.java
+++ java/thebetweenlands/blocks/BlockSludge.java
@@ -65,6 +65,11 @@ public class BlockSludge extends Block {
     }
     
     @Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+    
+    @Override
     public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_) {
         return null;
     }
