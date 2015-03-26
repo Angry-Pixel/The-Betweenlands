0ea75b41cc5b69400bd96729ff49143983854164
TheCyberBrick <samuelboerlin@bluewin.ch>
2015-03-26 21:47:27 +0100

    Fixed wisp bug

--- java/thebetweenlands/client/render/tileentity/TileEntityWispRenderer.java
+++ java/thebetweenlands/client/render/tileentity/TileEntityWispRenderer.java
@@ -24,7 +24,7 @@ public class TileEntityWispRenderer extends TileEntitySpecialRenderer {
 	@Override
 	public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float partialTicks) {
 		WispHandler.INSTANCE.tileList.add(new SimpleEntry(new SimpleEntry(this, tileEntity), new Vector3d(x, y, z)));
-		
+
 		ArrayList<Object> particleList = ((TileEntityWisp)tileEntity).particleList;
 
 		double dist = Minecraft.getMinecraft().renderViewEntity.getDistance(x + RenderManager.renderPosX, y + RenderManager.renderPosY, z + RenderManager.renderPosZ);
@@ -32,26 +32,28 @@ public class TileEntityWispRenderer extends TileEntitySpecialRenderer {
 			return;
 		}
 
-		if(System.nanoTime() - ((TileEntityWisp)tileEntity).lastSpawn >= (500f - 500.0f * ConfigHandler.WISP_QUALITY / 100.0f) * 1000000L) {
-			((TileEntityWisp)tileEntity).lastSpawn = System.nanoTime();
+		if(particleList.size() < 1000 && !Minecraft.getMinecraft().isGamePaused()) {
+			if(System.nanoTime() - ((TileEntityWisp)tileEntity).lastSpawn >= (500f - 500.0f * ConfigHandler.WISP_QUALITY / 100.0f) * 1000000L) {
+				((TileEntityWisp)tileEntity).lastSpawn = System.nanoTime();
 
-			int wispMeta = tileEntity.getBlockMetadata();
+				int wispMeta = tileEntity.getBlockMetadata();
 
-			particleList.add(new EntityWispFX(
-					tileEntity.getWorldObj(), 
-					x + 0.5 + RenderManager.renderPosX, 
-					y + 0.5F +RenderManager.renderPosY, 
-					z + 0.5 + RenderManager.renderPosZ,
-					0.0D, 0.0D, 0.0D,
-					3.0F, 50, BLBlockRegistry.wisp.colors[wispMeta]));
+				particleList.add(new EntityWispFX(
+						tileEntity.getWorldObj(), 
+						x + 0.5 + RenderManager.renderPosX, 
+						y + 0.5F +RenderManager.renderPosY, 
+						z + 0.5 + RenderManager.renderPosZ,
+						0.0D, 0.0D, 0.0D,
+						3.0F, 50, BLBlockRegistry.wisp.colors[wispMeta]));
 
-			particleList.add(new EntityWispFX(
-					tileEntity.getWorldObj(), 
-					x + 0.5 + RenderManager.renderPosX, 
-					y + 0.5F + RenderManager.renderPosY, 
-					z + 0.5 + RenderManager.renderPosZ,
-					0.0D, 0.0D, 0.0D,
-					2.0F, 50, BLBlockRegistry.wisp.colors[wispMeta + 1]));
+				particleList.add(new EntityWispFX(
+						tileEntity.getWorldObj(), 
+						x + 0.5 + RenderManager.renderPosX, 
+						y + 0.5F + RenderManager.renderPosY, 
+						z + 0.5 + RenderManager.renderPosZ,
+						0.0D, 0.0D, 0.0D,
+						2.0F, 50, BLBlockRegistry.wisp.colors[wispMeta + 1]));
+			}
 		}
 	}
 
