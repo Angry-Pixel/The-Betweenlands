c2766f15c0ff11a88d3facf0ce4fa418a57980e7
vadis365 <curve_linear@hotmail.com>
2015-04-06 16:53:34 +0100

    forgot this

--- java/thebetweenlands/entities/mobs/EntitySwampHag.java
+++ java/thebetweenlands/entities/mobs/EntitySwampHag.java
@@ -1,12 +1,23 @@
 package thebetweenlands.entities.mobs;
 
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import thebetweenlands.entities.entityAI.EntityAIBLBreakDoor;
 import thebetweenlands.items.AxeBL;
 import thebetweenlands.items.ItemMaterialsBL;
 import thebetweenlands.items.ItemMaterialsBL.EnumMaterialsBL;
@@ -22,9 +33,23 @@ public class EntitySwampHag extends EntityMob implements IEntityBL {
 	AnimationMathHelper animationBreathe = new AnimationMathHelper();
 	private int animationTick;
 	private byte randomLivingSound;
+
 	public EntitySwampHag(World world) {
 		super(world);
-		setSize(1F, 2.4F);
+		this.getNavigator().setBreakDoors(true);
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIBLBreakDoor(this, Blocks.wooden_door, 10));
+       // this.tasks.addTask(2, new EntityAIBLBreakDoor(this, Blocks.iron_door, 20));
+        this.tasks.addTask(3, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1D, false));
+        this.tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityVillager.class, 1D, false));
+        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWander(this, 1D));
+        this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(8, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, false));
+        this.setSize(0.6F, 1.8F);
 	}
 	
 	@Override
@@ -36,9 +61,14 @@ public class EntitySwampHag extends EntityMob implements IEntityBL {
 	}
 
 	@Override
+    protected boolean isAIEnabled() {
+    	return true;
+    }
+
+	@Override
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
-		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.5D);
+		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.23000000417232513D);
 		getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(40.0D);
 		getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(4.0D);
 		getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(32.0D);
@@ -83,6 +113,7 @@ public class EntitySwampHag extends EntityMob implements IEntityBL {
 		setJawState((byte) 1);
 		return "thebetweenlands:swampHagDeath";
 	}
+
 	@Override
 	public void onLivingUpdate() {
 
@@ -109,7 +140,6 @@ public class EntitySwampHag extends EntityMob implements IEntityBL {
 			jawFloat = animationTalk.swing(2.0F, 0.1F, false);
 		else if (getJawState() == 1 && getTalkSound() == 3 || getJawState() == 1 && getTalkSound() == 4)
 			jawFloat = animationTalk.swing(0.4F, 1.2F, false);
-
 			super.onLivingUpdate();	
 	}
 	
@@ -145,5 +175,5 @@ public class EntitySwampHag extends EntityMob implements IEntityBL {
 		}
 		return super.attackEntityFrom(source, damage);
 	}
-
+	
 }
