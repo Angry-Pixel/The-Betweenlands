d8ecb7ba45afb54be4b1668821080353ba2e0b89
OnyxDarkKnight <sor1n.iliutza16@gmail.com>
2015-04-22 20:38:39 +0100

    Halved the time decay decreases

--- java/thebetweenlands/event/player/DecayEventHandler.java
+++ java/thebetweenlands/event/player/DecayEventHandler.java
@@ -21,83 +21,76 @@ import thebetweenlands.utils.IDecayFood;
 import java.util.Map;
 import java.util.UUID;
 
-public class DecayEventHandler
-{
-    public static DecayEventHandler INSTANCE = new DecayEventHandler();
+public class DecayEventHandler {
+	public static DecayEventHandler INSTANCE = new DecayEventHandler();
 
-    public PotionEffect slowness_0 = new PotionEffect(Potion.moveSlowdown.getId(), 1, 0, true);
-    public PotionEffect slowness_1 = new PotionEffect(Potion.moveSlowdown.getId(), 1, 1, true);
-    public PotionEffect slowness_2 = new PotionEffect(Potion.moveSlowdown.getId(), 1, 2, true);
+	public PotionEffect slowness_0 = new PotionEffect(Potion.moveSlowdown.getId(), 1, 0, true);
+	public PotionEffect slowness_1 = new PotionEffect(Potion.moveSlowdown.getId(), 1, 1, true);
+	public PotionEffect slowness_2 = new PotionEffect(Potion.moveSlowdown.getId(), 1, 2, true);
 
-    public Map<UUID, Integer> corruptionBuffer = Maps.newHashMap();
+	public Map<UUID, Integer> corruptionBuffer = Maps.newHashMap();
 
-    @SubscribeEvent
-    public void entityConstructing(EntityEvent.EntityConstructing event)
-    {
-        if (event.entity instanceof EntityPlayer)
-        {
-            event.entity.registerExtendedProperties(EntityPropertiesDecay.getId(), new EntityPropertiesDecay());
-        }
-    }
+	@SubscribeEvent
+	public void entityConstructing(EntityEvent.EntityConstructing event) {
+		if (event.entity instanceof EntityPlayer) {
+			event.entity.registerExtendedProperties(EntityPropertiesDecay.getId(), new EntityPropertiesDecay());
+		}
+	}
 
-    @SubscribeEvent
-    public void joinWorld(EntityJoinWorldEvent event)
-    {
-        if (!event.world.isRemote && event.entity instanceof EntityPlayer)
-        {
-            EntityPlayer player = (EntityPlayer) event.entity;
+	@SubscribeEvent
+	public void joinWorld(EntityJoinWorldEvent event) {
+		if (!event.world.isRemote && event.entity instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.entity;
 
-            int decayLevel = DecayManager.getDecayLevel(player);
+			int decayLevel = DecayManager.getDecayLevel(player);
 
-            TheBetweenlands.networkWrapper.sendTo(new MessageSyncPlayerDecay(decayLevel), (EntityPlayerMP) player);
-        }
-        else if (event.world.isRemote && event.entity instanceof EntityPlayer)
-        {
-            TheBetweenlands.proxy.corruptPlayerSkin((EntityPlayerSP) event.entity, DecayManager.getCorruptionLevel((EntityPlayer) event.entity));
-        }
-    }
+			TheBetweenlands.networkWrapper.sendTo(new MessageSyncPlayerDecay(decayLevel), (EntityPlayerMP) player);
+		} else if (event.world.isRemote && event.entity instanceof EntityPlayer) {
+			TheBetweenlands.proxy.corruptPlayerSkin((EntityPlayerSP) event.entity, DecayManager.getCorruptionLevel((EntityPlayer) event.entity));
+		}
+	}
 
-    @SubscribeEvent
-    public void useItem(PlayerUseItemEvent.Finish event)
-    {
-        if (DecayManager.enableDecay(event.entityPlayer) && event.item.getItem() instanceof IDecayFood)
-        {
-            IDecayFood food = (IDecayFood) event.item.getItem();
-            DecayManager.setDecayLevel(DecayManager.getDecayLevel(event.entityPlayer) + food.getDecayHealAmount(), event.entityPlayer);
-        }
-    }
+	@SubscribeEvent
+	public void useItem(PlayerUseItemEvent.Finish event) {
+		if (DecayManager.enableDecay(event.entityPlayer) && event.item.getItem() instanceof IDecayFood) {
+			IDecayFood food = (IDecayFood) event.item.getItem();
+			DecayManager.setDecayLevel(DecayManager.getDecayLevel(event.entityPlayer) + food.getDecayHealAmount(), event.entityPlayer);
+		}
+	}
 
-    @SubscribeEvent
-    public void playerTick(TickEvent.PlayerTickEvent event)
-    {
-        if (DecayManager.enableDecay(event.player))
-        {
-            event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(DecayManager.getPlayerHearts(event.player));
+	@SubscribeEvent
+	public void playerTick(TickEvent.PlayerTickEvent event) {
+		if (DecayManager.enableDecay(event.player)) {
+			event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(DecayManager.getPlayerHearts(event.player));
 
-            if (DecayManager.getDecayLevel(event.player) <= 4) event.player.addPotionEffect(slowness_2);
-            else if (DecayManager.getDecayLevel(event.player) <= 8) event.player.addPotionEffect(slowness_1);
-            else if (DecayManager.getDecayLevel(event.player) <= 12) event.player.addPotionEffect(slowness_0);
+			if (DecayManager.getDecayLevel(event.player) <= 4)
+				event.player.addPotionEffect(slowness_2);
+			else if (DecayManager.getDecayLevel(event.player) <= 8)
+				event.player.addPotionEffect(slowness_1);
+			else if (DecayManager.getDecayLevel(event.player) <= 12)
+				event.player.addPotionEffect(slowness_0);
 
-            if (event.player.ticksExisted % 1024 == 0)
-            {
-                DecayManager.setDecayLevel(DecayManager.getDecayLevel(event.player) - 1, event.player);
-            }
-        }
-        else if (event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).getAttributeValue() != 20d) event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20d);
-        else if (event.player.isPotionActive(Potion.moveSlowdown.getId())) event.player.removePotionEffect(Potion.moveSlowdown.getId());
+			// Oli, if you ever want to change decay speed, change the number
+			// that the ticksExisted is divided by.
+			// Right now every multiple of 2048 the decay level is decreased by 1
+			if (!event.player.worldObj.isRemote)
+				System.out.println(event.player.ticksExisted);
+			if (event.player.ticksExisted % 2048 == 0) {
+				DecayManager.setDecayLevel(DecayManager.getDecayLevel(event.player) - 1, event.player);
+			}
+		} else if (event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).getAttributeValue() != 20d)
+			event.player.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20d);
+		else if (event.player.isPotionActive(Potion.moveSlowdown.getId()))
+			event.player.removePotionEffect(Potion.moveSlowdown.getId());
 
-        if (event.player.worldObj.isRemote)
-        {
-            if (!corruptionBuffer.containsKey(event.player.getPersistentID()))
-            {
-                corruptionBuffer.put(event.player.getPersistentID(), DecayManager.getCorruptionLevel(event.player));
-                TheBetweenlands.proxy.corruptPlayerSkin((EntityPlayerSP) event.player, DecayManager.getCorruptionLevel(event.player));
-            }
-            else if (corruptionBuffer.get(event.player.getPersistentID()) != DecayManager.getCorruptionLevel(event.player))
-            {
-                corruptionBuffer.put(event.player.getPersistentID(), DecayManager.getCorruptionLevel(event.player));
-                TheBetweenlands.proxy.corruptPlayerSkin((EntityPlayerSP) event.player, DecayManager.getCorruptionLevel(event.player));
-            }
-        }
-    }
+		if (event.player.worldObj.isRemote) {
+			if (!corruptionBuffer.containsKey(event.player.getPersistentID())) {
+				corruptionBuffer.put(event.player.getPersistentID(), DecayManager.getCorruptionLevel(event.player));
+				TheBetweenlands.proxy.corruptPlayerSkin((EntityPlayerSP) event.player, DecayManager.getCorruptionLevel(event.player));
+			} else if (corruptionBuffer.get(event.player.getPersistentID()) != DecayManager.getCorruptionLevel(event.player)) {
+				corruptionBuffer.put(event.player.getPersistentID(), DecayManager.getCorruptionLevel(event.player));
+				TheBetweenlands.proxy.corruptPlayerSkin((EntityPlayerSP) event.player, DecayManager.getCorruptionLevel(event.player));
+			}
+		}
+	}
 }
