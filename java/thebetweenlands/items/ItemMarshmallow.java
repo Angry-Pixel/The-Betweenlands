a8f3ac663f8a9449da3e37b19e910c09cb635c77
Oli <angrypixelminecraft@gmail.com>
2015-05-11 21:44:31 +0100

    Marshmallows!

---
+++ java/thebetweenlands/items/ItemMarshmallow.java
@@ -0,0 +1,26 @@
+package thebetweenlands.items;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+
+public class ItemMarshmallow
+        extends ItemFood
+{
+    public ItemMarshmallow() {
+        super(4, 10F, false);
+    }
+
+    @Override
+    protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player) {
+        super.onFoodEaten(stack, world, player);
+
+        if( player != null ) {
+            player.addPotionEffect(new PotionEffect(Potion.moveSpeed.getId(), 400, 1));
+            player.addPotionEffect(new PotionEffect(Potion.jump.getId(), 400, 1));
+        }
+    }
+}
