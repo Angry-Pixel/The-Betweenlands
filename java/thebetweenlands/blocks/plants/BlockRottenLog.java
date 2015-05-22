bfa83cffbf1550bfafeeef07ebbd7212d1a4950b
imdutch21 <bartpklomp@gmail.com>
2015-05-22 17:57:00 +0200

    fixes the darkness of rotten logs

--- java/thebetweenlands/blocks/plants/BlockRottenLog.java
+++ java/thebetweenlands/blocks/plants/BlockRottenLog.java
@@ -70,4 +70,9 @@ public class BlockRottenLog extends Block {
 	public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_) {
 		return ItemMaterialsBL.createStack(EnumMaterialsBL.ROTTEN_BARK).getItem();
 	}
+
+	@Override
+	public boolean isOpaqueCube(){
+		return false;
+	}
 }
