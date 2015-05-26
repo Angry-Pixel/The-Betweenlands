92e039a06e088f50fb35c758dd5a34c7d0f85768
iLexiconn <ilexiconn@gmail.com>
2015-05-26 23:57:29 +0200

    More tweaks!

--- java/thebetweenlands/world/biomes/BiomePatchyIslands.java
+++ java/thebetweenlands/world/biomes/BiomePatchyIslands.java
@@ -31,14 +31,14 @@ extends BiomeGenBaseBetweenlands
 		.addFeature(new AlgaeNoiseFeature());
 		this.waterColorMultiplier = 0x184220;
 
-		spawnableMonsterList.add(new SpawnListEntry(EntitySwampHag.class, 30, 1, 1));
-		spawnableMonsterList.add(new SpawnListEntry(EntityTarBeast.class, 15, 1, 1));
-		spawnableMonsterList.add(new SpawnListEntry(EntityWight.class, 2, 1, 1));
+		spawnableMonsterList.add(new SpawnListEntry(EntitySwampHag.class, 20, 1, 1));
+		// spawnableMonsterList.add(new SpawnListEntry(EntityTarBeast.class, 15, 1, 1)); TODO
+		spawnableMonsterList.add(new SpawnListEntry(EntityWight.class, 5, 0, 0));
 		spawnableWaterCreatureList.add(new SpawnListEntry(EntityAngler.class, 20, 1, 2));
-        spawnableMonsterList.add(new SpawnListEntry(EntityDragonFly.class, 20, 2, 4));
-        spawnableMonsterList.add(new SpawnListEntry(EntityFirefly.class, 50, 10, 20));
-        spawnableMonsterList.add(new SpawnListEntry(EntityLeech.class, 15, 1, 1));
-        spawnableCreatureList.add(new SpawnListEntry(EntityMireSnail.class, 20, 1, 2));
-        spawnableMonsterList.add(new SpawnListEntry(EntityBloodSnail.class, 5, 1, 1));
+        	spawnableMonsterList.add(new SpawnListEntry(EntityDragonFly.class, 20, 2, 4));
+        	spawnableMonsterList.add(new SpawnListEntry(EntityFirefly.class, 100, 10, 20));
+        	spawnableMonsterList.add(new SpawnListEntry(EntityLeech.class, 15, 1, 1));
+        	spawnableCreatureList.add(new SpawnListEntry(EntityMireSnail.class, 30, 1, 2));
+        	spawnableMonsterList.add(new SpawnListEntry(EntityBloodSnail.class, 15, 1, 1));
 	}
 }
