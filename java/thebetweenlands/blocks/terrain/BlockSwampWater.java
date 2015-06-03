07bdb91cd886ae9fddcb7e70f2a8c27ed8410989
vadis365 <curve_linear@hotmail.com>
2015-06-03 21:54:31 +0100

    wasn't that

--- java/thebetweenlands/blocks/terrain/BlockSwampWater.java
+++ java/thebetweenlands/blocks/terrain/BlockSwampWater.java
@@ -27,7 +27,7 @@ public class BlockSwampWater extends BlockFluidClassic {
 
 	protected boolean canSpread = true;
 	protected boolean hasBoundingBox = false;
-	protected boolean canCollide = true;
+	protected boolean canCollide = false;
 
 	private static final HashMap<Block, IWaterRenderer> SPECIAL_RENDERERS = new HashMap<>();
 
