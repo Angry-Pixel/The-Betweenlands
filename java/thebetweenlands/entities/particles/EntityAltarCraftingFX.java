f56f38bdff7f0fa614dfca05b6371bcc7dd2dc29
pau101 <paul.fulham0@gmail.com>
2015-06-03 17:16:54 -0700

    fixes EntityAlterCraftingFX jitter and skip
    - fixes NullPointerExceptions and ClassCastExceptions occurring when the
    alter is destroyed while crafting is in progress
    The jitter was caused by random motion that is applied during initiation
    of the partial effect from the parent constructor.
    The skip was caused by the call to super.onUpdate() which set the
    previous x, y, and z after onUpdate had already moved the particle.

--- java/thebetweenlands/entities/particles/EntityAltarCraftingFX.java
+++ java/thebetweenlands/entities/particles/EntityAltarCraftingFX.java
@@ -1,19 +1,21 @@
 package thebetweenlands.entities.particles;
 
+import javax.vecmath.Vector3d;
+
 import net.minecraft.client.particle.EntityFX;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import thebetweenlands.client.render.tileentity.TileEntityDruidAltarRenderer;
 import thebetweenlands.tileentities.TileEntityDruidAltar;
-
-import javax.vecmath.Vector3d;
 
 public class EntityAltarCraftingFX extends EntityFX {
 	private TileEntityDruidAltar target;
 	private final Vector3d startPoint;
 	private final Vector3d endPoint;
-	
+
 	public EntityAltarCraftingFX(World world, double x, double y, double z, double motionX, double motionY, double motionZ, float scale, TileEntityDruidAltar target) {
 		super(world, x, y, z, motionX, motionY, motionZ);
+		this.motionX = this.motionY = this.motionZ = 0;
 		this.target = target;
 		this.startPoint = new Vector3d(x, y, z);
 		this.endPoint = new Vector3d(target.xCoord + 0.5D, target.yCoord + TileEntityDruidAltarRenderer.FINAL_HEIGHT + 1.05D, target.zCoord + 0.5D);
@@ -27,27 +29,27 @@ public class EntityAltarCraftingFX extends EntityFX {
 
     @Override
     public void onUpdate() {
-    	if(this.target == null) {
-    		this.setDead();
-    	}
     	//TODO I have no fucking idea why it only works like this, for some reason this.target != te
-    	TileEntityDruidAltar te = (TileEntityDruidAltar)this.worldObj.getTileEntity(this.target.xCoord, this.target.yCoord, this.target.zCoord);
-    	double relProgress = (double) te.craftingProgress / (double) TileEntityDruidAltar.CRAFTING_TIME;
-    	if(relProgress == 1.0D || te.craftingProgress == 0) {
+    	TileEntity tileEntity = this.worldObj.getTileEntity(this.target.xCoord, this.target.yCoord, this.target.zCoord);
+    	double craftingProgress = 0;
+    	if(tileEntity instanceof TileEntityDruidAltar) {
+    		craftingProgress = ((TileEntityDruidAltar) tileEntity).craftingProgress;
+    	}
+    	if(this.particleAge++ >= this.particleMaxAge || craftingProgress == 0) {
     		this.setDead();
     	}
+    	craftingProgress /= TileEntityDruidAltar.CRAFTING_TIME;
     	Vector3d xzDiff = new Vector3d(this.endPoint.x, this.endPoint.y, this.endPoint.z);
     	xzDiff.sub(new Vector3d(this.startPoint.x, this.endPoint.y, this.startPoint.z));
     	Vector3d yDiff = new Vector3d(this.endPoint.x, this.endPoint.y, this.endPoint.z);
     	yDiff.sub(new Vector3d(this.endPoint.x, this.startPoint.y, this.endPoint.z));
-    	xzDiff.scale(relProgress);
-    	yDiff.scale(Math.pow(relProgress, 6));
-    	this.prevPosX = this.posX;
-    	this.prevPosY = this.posY;
-    	this.prevPosZ = this.posZ;
+    	xzDiff.scale(craftingProgress);
+    	yDiff.scale(Math.pow(craftingProgress, 6));
+        this.prevPosX = this.posX;
+        this.prevPosY = this.posY;
+        this.prevPosZ = this.posZ;
     	this.setPosition(this.startPoint.x + xzDiff.x, this.startPoint.y + yDiff.y, this.startPoint.z + xzDiff.z);
-    	particleGreen = (float) (1F - relProgress);
-    	particleBlue = (float) (1F - relProgress);
-    	super.onUpdate();
+    	particleGreen = (float) (1F - craftingProgress);
+    	particleBlue = (float) (1F - craftingProgress);
     }
 }
