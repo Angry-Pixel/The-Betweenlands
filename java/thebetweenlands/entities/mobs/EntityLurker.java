c6b6817b443731cd5cacff46976f7d30a01b3bb6
Aoredon <Aoredon@gmail.com>
2015-06-07 13:30:20 +0100

    Lurker stuff e__e

--- java/thebetweenlands/entities/mobs/EntityLurker.java
+++ java/thebetweenlands/entities/mobs/EntityLurker.java
@@ -1,57 +1,64 @@
 package thebetweenlands.entities.mobs;
 
-import thebetweenlands.blocks.BLBlockRegistry;
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.DamageSource;
+import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
+import thebetweenlands.blocks.BLBlockRegistry;
 
 public class EntityLurker extends EntityMob implements IEntityBL {
+	private ChunkCoordinates movementTarget;
+	
 	public EntityLurker(World world) {
 		super(world);
 		
-		this.setSize(2F, 0.9F);
+		this.setSize(1.5F, 0.9F);
 		
-		this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAIWander(this, 1D));
-		this.tasks.addTask(2, new EntityAILookIdle(this));
+		this.tasks.taskEntries.clear();
+		this.targetTasks.taskEntries.clear();
+		
+		this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityAngler.class, 0.5D, false));
+		this.tasks.addTask(2, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityAngler.class, 8.0F));
+		this.tasks.addTask(4, new EntityAILookIdle(this));
+
+		this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityAngler.class, 0, true));
 	}
 	
 	@Override
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
-		
-		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.25D);
-		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(10.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(0.5D);
+
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(4.5D);
 		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(16.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(1.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.25D);
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(25.0D);
 	}
 	
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
-		
-		if (this.worldObj.difficultySetting == EnumDifficulty.PEACEFUL) {
-			this.setDead();
+
+		if (this.isInWater()) {
+			if (this.getAttackTarget() != null &&
+					this.getAir() >= 80) {
+				this.faceEntity(this.getAttackTarget(), this.rotationPitch, 360F);
+				this.moveForward += 1F;
+				this.motionY += Math.signum(this.getAttackTarget().posY - this.posY) * 0.03D;
+			} else {
+				this.motionY += 0.03D;
+			}
 		}
 	}
-	
-	@Override
-	public void onEntityUpdate() {
-		super.onEntityUpdate();
-	}
-	
-	@Override
-    public boolean isInWater() {
-        return this.worldObj.handleMaterialAcceleration(this.boundingBox, Material.water, this);
-    }
 	
 	@Override
 	public boolean getCanSpawnHere() {
@@ -60,7 +67,7 @@ public class EntityLurker extends EntityMob implements IEntityBL {
 	}
 	
 	@Override
-	public boolean isAIEnabled() {
+	protected boolean isAIEnabled() {
 		return true;
 	}
 }
