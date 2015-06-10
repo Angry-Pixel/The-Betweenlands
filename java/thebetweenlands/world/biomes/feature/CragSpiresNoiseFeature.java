4cad6b6962a9c7ff7c02eba45a0ce2fa3ac83704
TheCyberBrick <samuelboerlin@bluewin.ch>
2015-06-10 21:27:03 +0200

    Improved crag rock gen

--- java/thebetweenlands/world/biomes/feature/CragSpiresNoiseFeature.java
+++ java/thebetweenlands/world/biomes/feature/CragSpiresNoiseFeature.java
@@ -15,7 +15,7 @@ import java.util.Random;
 public class CragSpiresNoiseFeature extends BiomeNoiseFeature {
 	private NoiseGeneratorPerlin spireNoiseGen;
 	private double[] spireNoise = new double[256];
-	
+
 	@Override
 	public void initializeNoiseGen(Random rng, BiomeGenBaseBetweenlands biome) {
 		this.spireNoiseGen = new NoiseGeneratorPerlin(rng, 4);
@@ -38,26 +38,23 @@ public class CragSpiresNoiseFeature extends BiomeNoiseFeature {
 			BiomeGenBase[] chunksForGeneration, Random rng) { 
 		int sliceSize = chunkBlocks.length / 256;
 		double noise = this.spireNoise[x * 16 + z] / 1.5f + 2.4f;
-		if(noise <= 0) {
-			if(-noise * 12 < 1) {
-				return;
+		int layerHeight = WorldProviderBetweenlands.LAYER_HEIGHT;
+		if(chunkBlocks[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, layerHeight, z, sliceSize)] != provider.layerBlock) {
+			return;
+		}
+		int lowestBlock = 0;
+		for(int yOff = 0; yOff < layerHeight; yOff++) {
+			int y = layerHeight - yOff;
+			Block currentBlock = chunkBlocks[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, y, z, sliceSize)];
+			if(currentBlock != provider.layerBlock) {
+				lowestBlock = y;
+				break;
 			}
-			int layerHeight = WorldProviderBetweenlands.LAYER_HEIGHT;
-			if(chunkBlocks[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, layerHeight, z, sliceSize)] != provider.layerBlock) {
-				return;
-			}
-			int lowestBlock = 0;
-			for(int yOff = 0; yOff < layerHeight; yOff++) {
-				int y = layerHeight - yOff;
-				Block currentBlock = chunkBlocks[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, y, z, sliceSize)];
-				if(currentBlock != provider.layerBlock) {
-					lowestBlock = y;
-					break;
-				}
-			}
-			if(WorldProviderBetweenlands.LAYER_HEIGHT - lowestBlock < 3) {
-				return;
-			}
+		}
+		if(WorldProviderBetweenlands.LAYER_HEIGHT - lowestBlock < 3) {
+			return;
+		}
+		if(-noise * 12 >= 1) {
 			for(int y = lowestBlock; y < layerHeight; y++) {
 				chunkBlocks[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, y, z, sliceSize)] = BLBlockRegistry.genericStone;
 				chunkMeta[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, y, z, sliceSize)] = BlockGenericStone.META_CRAGROCK;
@@ -71,6 +68,28 @@ public class CragSpiresNoiseFeature extends BiomeNoiseFeature {
 				} else if(yOff == rockHeight - 1) {
 					chunkMeta[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, y, z, sliceSize)] = BlockGenericStone.META_MOSSYCRAGROCK1;
 				} else {
+					chunkMeta[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, y, z, sliceSize)] = BlockGenericStone.META_CRAGROCK;
+				}
+			}
+		} else {
+			boolean validSpire = false;
+			for(int xo = -4; xo < 4; xo++) {
+				for(int zo = -4; zo < 4; zo++) {
+					int nx = x + xo;
+					int nz = z + zo;
+					nx = nx < 0 ? 0 : (nx > 15 ? 15 : nx);
+					nz = nz < 0 ? 0 : (nz > 15 ? 15 : nz);
+					double sNoise = this.spireNoise[nx * 16 + nz] / 1.5f + 2.4f;
+					if(-sNoise * 12 >= 1) {
+						validSpire = true;
+						break;
+					}
+				}
+			}
+			if(validSpire) {
+				int rockHeight = (int)Math.floor(-noise * 12);
+				for(int y = lowestBlock; y < layerHeight + rockHeight; y++) {
+					chunkBlocks[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, y, z, sliceSize)] = BLBlockRegistry.genericStone;
 					chunkMeta[BiomeGenBaseBetweenlands.getBlockArrayIndex(x, y, z, sliceSize)] = BlockGenericStone.META_CRAGROCK;
 				}
 			}
