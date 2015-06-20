de7aaa535b6837280e15084cabe18da4f00a2430
vadis365 <curve_linear@hotmail.com>
2015-06-20 23:27:35 +0100

    attempt at making this class more legible

--- java/thebetweenlands/blocks/terrain/BlockFarmedDirt.java
+++ java/thebetweenlands/blocks/terrain/BlockFarmedDirt.java
@@ -29,6 +29,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class BlockFarmedDirt extends Block implements ISubBlocksBlock {
 
 	public static final String[] iconPaths = new String[] { "purifiedSwampDirt", "dugSwampDirt", "dugSwampGrass", "dugPurifiedSwampDirt", "fertDirt", "fertGrass", "fertPurifiedSwampDirt", "fertDirtDecayed", "fertGrassDecayed" };
+	public final int PURE_SWAMP_DIRT = 0, DUG_SWAMP_DIRT = 1, DUG_SWAMP_GRASS = 2, DUG_PURE_SWAMP_DIRT = 3, FERT_DIRT = 4, FERT_GRASS = 5, FERT_PURE_SWAMP_DIRT_MIN = 6, FERT_DIRT_DECAYED = 7, FERT_GRASS_DECAYED = 8, FERT_PURE_SWAMP_DIRT_MID = 9, FERT_PURE_SWAMP_DIRT_MAX = 10;
+	public final int COMPOSTING_MODIFIER = 3, DECAY_CURE = 3, DECAY_CAUSE = 3;
+	public final int MATURE_CROP = 7, DECAYED_CROP = 8;
+	public int DECAY_TIME = 10, DUG_SOIL_REVERT_TIME = 10;
 	@SideOnly(Side.CLIENT)
 	private IIcon[] icons;
 	private IIcon sideIconDirt, sideIconGrass;
@@ -45,32 +49,30 @@ public class BlockFarmedDirt extends Block implements ISubBlocksBlock {
 
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int metadata, float hitX, float hitY, float hitZ) {
-			int meta = world.getBlockMetadata(x, y, z);
+			int meta = getDamageValue(world, x, y, z);
 			ItemStack stack = player.getCurrentEquippedItem();
 			if (stack !=null && stack.getItem() instanceof SpadeBL) {
 				if (!world.isRemote) {
-					if(meta == 0) {
-						world.setBlockMetadataWithNotify(x, y, z, 3, 3);
+					if(meta == PURE_SWAMP_DIRT) {
+						world.setBlockMetadataWithNotify(x, y, z, DUG_PURE_SWAMP_DIRT, 3);
 						world.playSoundEffect((double)((float)x + 0.5F), (double)((float)y + 0.5F), (double)((float)z + 0.5F), stepSound.getStepResourcePath(), (stepSound.getVolume() + 1.0F) / 2.0F, stepSound.getPitch() * 0.8F);
 						world.playAuxSFXAtEntity(null, 2001, x, y + 1, z, Block.getIdFromBlock(world.getBlock(x, y, z)));
-						player.getCurrentEquippedItem().damageItem(1, player);
+						stack.damageItem(1, player);
 					}
 				}
 				return true;
 			}	
 		if (stack != null && stack.getItem() == BLItemRegistry.materialsBL && stack.getItemDamage() == EnumMaterialsBL.COMPOST.ordinal()) {
 			if (!world.isRemote) {
-				if (meta == 1 || meta == 2) {
-					world.setBlockMetadataWithNotify(x, y, z, meta + 3, 3);
-					world.playAuxSFX(2005, x, y + 1, z, 0);
-					world.playSoundEffect((double) ((float) x + 0.5F), (double) ((float) y + 0.5F), (double) ((float) z + 0.5F), stepSound.getStepResourcePath(), (stepSound.getVolume() + 1.0F) / 2.0F, stepSound.getPitch() * 0.8F);
+				if (meta == DUG_SWAMP_DIRT || meta == DUG_SWAMP_GRASS) {
+					world.setBlockMetadataWithNotify(x, y, z, meta + COMPOSTING_MODIFIER, 3);
+					playCompostEffects(world, x, y, z);
 					if (!player.capabilities.isCreativeMode)
 						stack.stackSize--;
 				}
-				if (meta == 3) {
-					world.setBlockMetadataWithNotify(x, y, z, 10, 3);
-					world.playAuxSFX(2005, x, y + 1, z, 0);
-					world.playSoundEffect((double) ((float) x + 0.5F), (double) ((float) y + 0.5F), (double) ((float) z + 0.5F), stepSound.getStepResourcePath(), (stepSound.getVolume() + 1.0F) / 2.0F, stepSound.getPitch() * 0.8F);
+				if (meta == DUG_PURE_SWAMP_DIRT) {
+					world.setBlockMetadataWithNotify(x, y, z, FERT_PURE_SWAMP_DIRT_MAX, 3);
+					playCompostEffects(world, x, y, z);
 					if (!player.capabilities.isCreativeMode)
 						stack.stackSize--;
 				}
@@ -79,8 +81,8 @@ public class BlockFarmedDirt extends Block implements ISubBlocksBlock {
 		}
 		if (stack != null && stack.getItem() == BLItemRegistry.materialsBL && stack.getItemDamage() == EnumMaterialsBL.PLANT_TONIC.ordinal()) {
 			if (!world.isRemote) {
-				if (meta == 7 || meta == 8)
-					world.setBlockMetadataWithNotify(x, y, z, meta - 3, 3);
+				if (meta == FERT_DIRT_DECAYED || meta == FERT_GRASS_DECAYED)
+					world.setBlockMetadataWithNotify(x, y, z, meta - DECAY_CURE, 3);
 			}
 			if (!player.capabilities.isCreativeMode) {
 				stack.stackSize--;
@@ -92,15 +94,19 @@ public class BlockFarmedDirt extends Block implements ISubBlocksBlock {
 		return false;
 	}
 
+	public void playCompostEffects(World world, int x, int y, int z) {
+		world.playAuxSFX(2005, x, y + 1, z, 0);
+		world.playSoundEffect((double) ((float) x + 0.5F), (double) ((float) y + 0.5F), (double) ((float) z + 0.5F), stepSound.getStepResourcePath(), (stepSound.getVolume() + 1.0F) / 2.0F, stepSound.getPitch() * 0.8F);
+	}
+
 	@Override
 	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int meta, int fortune) {
 		ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
-		
-		if(meta == 0 || meta == 3 ||meta == 6 || meta == 9 ||meta == 10)
+		if(meta == PURE_SWAMP_DIRT || meta == DUG_PURE_SWAMP_DIRT || meta == FERT_PURE_SWAMP_DIRT_MIN || meta == FERT_PURE_SWAMP_DIRT_MID ||meta == FERT_PURE_SWAMP_DIRT_MAX)
 			drops.add(new ItemStack(Item.getItemFromBlock(this), 1, 0));
-		if(meta == 1 || meta == 4 || meta == 7)
+		if(meta == DUG_SWAMP_DIRT || meta == FERT_DIRT || meta == FERT_DIRT_DECAYED)
 			drops.add(new ItemStack(Item.getItemFromBlock(BLBlockRegistry.swampDirt), 1, 0));
-		if(meta == 2 || meta == 5 || meta == 8)
+		if(meta == DUG_SWAMP_GRASS || meta == FERT_GRASS || meta == FERT_GRASS_DECAYED)
 			drops.add(new ItemStack(Item.getItemFromBlock(BLBlockRegistry.swampGrass), 1, 0));
 		
 		return drops;
@@ -108,22 +114,22 @@ public class BlockFarmedDirt extends Block implements ISubBlocksBlock {
 
 	@Override
 	public void updateTick(World world, int x, int y, int z, Random rand) {
-		int meta = world.getBlockMetadata(x, y, z);
+		int meta = getDamageValue(world, x, y, z);
 		//Decay rate of composted blocks
-		if(world.rand.nextInt(10) == 0) {
-			if(meta == 4 || meta == 5) {
-				world.setBlockMetadataWithNotify(x, y, z, meta + 3, 3);
+		if(world.rand.nextInt(DECAY_TIME) == 0) {
+			if(meta == FERT_DIRT || meta == FERT_GRASS) {
+				world.setBlockMetadataWithNotify(x, y, z, meta + DECAY_CAUSE, 3);
 				//Update decay to plants above
-				if(world.getBlock(x, y + 1, z) instanceof BlockBLGenericCrop && world.getBlockMetadata(x, y + 1, z) == 7)
-					world.setBlockMetadataWithNotify(x, y + 1, z, 8, 3);
+				if(getCropAboveBlock(world, x, y, z) instanceof BlockBLGenericCrop && getCropAboveBlockDamageValue(world, x, y, z) == MATURE_CROP)
+					world.setBlockMetadataWithNotify(x, y + 1, z, DECAYED_CROP, 3);
 			}
 		}
-		
+
 		//Dug dirt reverts to un-dug
-		if(world.rand.nextInt(10) == 0) {
-			if(meta == 1 || meta == 3)
+		if(world.rand.nextInt(DUG_SOIL_REVERT_TIME) == 0) {
+			if(meta == DUG_SWAMP_DIRT || meta == DUG_PURE_SWAMP_DIRT)
 				world.setBlock(x, y, z, BLBlockRegistry.swampDirt, 0, 3);
-			if(meta == 2)
+			if(meta == DUG_SWAMP_GRASS)
 				world.setBlock(x, y, z, BLBlockRegistry.swampGrass, 0, 3);
 		}
 	}
@@ -133,7 +139,6 @@ public class BlockFarmedDirt extends Block implements ISubBlocksBlock {
 		sideIconDirt = reg.registerIcon("thebetweenlands:swampDirt");
 		sideIconGrass = reg.registerIcon("thebetweenlands:swampGrassSide");
 		icons = new IIcon[iconPaths.length];
-
 		int i = 0;
 		for (String path : iconPaths)
 			icons[i++] = reg.registerIcon("thebetweenlands:" + path);
@@ -141,33 +146,28 @@ public class BlockFarmedDirt extends Block implements ISubBlocksBlock {
 
 	@Override
 	public IIcon getIcon(int side, int meta) {
-		if (meta < 0 || meta >= icons.length + 2)
+		if (meta < DUG_PURE_SWAMP_DIRT || meta >= icons.length + 2)
 			return null;
-		
-		if(meta == 3 || meta == 6)
+		if(meta == DUG_PURE_SWAMP_DIRT || meta == FERT_PURE_SWAMP_DIRT_MIN)
 			if (side == 1)
 				return icons[meta];
 			else
 				return icons[0];
-		
-		if(meta == 9 || meta == 10)
+		if(meta == FERT_PURE_SWAMP_DIRT_MID || meta == FERT_PURE_SWAMP_DIRT_MAX)
 			if (side == 1)
 				return icons[6];
 			else
 				return icons[0];
-
-		if(meta == 1 || meta == 4 || meta == 7)
+		if(meta == DUG_SWAMP_DIRT || meta == FERT_DIRT || meta == FERT_DIRT_DECAYED)
 			if (side == 1)
 				return icons[meta];
 			else
 				return sideIconDirt;
-
-		if(meta == 2 || meta == 5 || meta == 8)
+		if(meta == DUG_SWAMP_GRASS || meta == FERT_GRASS || meta == FERT_GRASS_DECAYED)
 			if (side == 1)
 				return icons[meta];
 			else
 				return sideIconGrass;
-
 		return icons[meta];
 	}
 
@@ -189,40 +189,41 @@ public class BlockFarmedDirt extends Block implements ISubBlocksBlock {
 		return world.getBlockMetadata(x, y, z);
 	}
 
+	public Block getCropAboveBlock(World world, int x, int y, int z) {
+		return world.getBlock(x, y + 1, z);
+	}
+
+	public int getCropAboveBlockDamageValue(World world, int x, int y, int z) {
+		return world.getBlockMetadata(x, y + 1, z);
+	}
+
 	@Override
 	public Class<? extends ItemBlock> getItemBlockClass() {
 		return ItemBlockGeneric.class;
 	}
-	
+
 	@Override
 	public void randomDisplayTick(World world, int x, int y, int z, Random rand) {
 		double pixel = 0.0625D;
-		int meta = world.getBlockMetadata(x, y, z);
-		if (meta == 7 || meta == 8) {
+		int meta = getDamageValue(world, x, y, z);
+		if (meta == FERT_DIRT_DECAYED || meta == FERT_GRASS_DECAYED) {
 			if (rand.nextInt(10) == 0) {
-				for (int l = 0; l < 3; l++) {
+				for (int l = 0; l < 4; l++) {
 					double particleX = x + rand.nextFloat();
 					double particleY = y + rand.nextFloat();
 					double particleZ = z + rand.nextFloat();
-
 					if (l == 0 && !world.getBlock(x, y + 2, z).isOpaqueCube())
 						particleY = y + 1 + pixel;
-
 					if (l == 1 && !world.getBlock(x, y - 1, z).isOpaqueCube())
 						particleY = y - pixel;
-
 					if (l == 2 && !world.getBlock(x, y, z + 1).isOpaqueCube())
 						particleZ = z + 1 + pixel;
-
 					if (l == 3 && !world.getBlock(x, y, z - 1).isOpaqueCube())
 						particleZ = z - pixel;
-
 					if (l == 4 && !world.getBlock(x + 1, y, z).isOpaqueCube())
 						particleX = x + 1 + pixel;
-
 					if (l == 5 && !world.getBlock(x - 1, y, z).isOpaqueCube())
 						particleX = x - pixel;
-
 					if (particleX < x || particleX > x + 1 || particleY < y || particleY > y + 1 || particleZ < z || particleZ > z + 1) {
 						TheBetweenlands.proxy.spawnCustomParticle("dirtDecay", world, particleX, particleY, particleZ, 0, 0, 0, 0);
 					}
