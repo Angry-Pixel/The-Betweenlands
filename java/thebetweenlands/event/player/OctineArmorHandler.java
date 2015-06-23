a65531731d28dcda27c1099a8760136e8def150f
pau101 <paul.fulham0@gmail.com>
2015-06-22 17:57:54 -0700

    improves octine armor handling

--- java/thebetweenlands/event/player/OctineArmorHandler.java
+++ java/thebetweenlands/event/player/OctineArmorHandler.java
@@ -1,40 +1,31 @@
 package thebetweenlands.event.player;
 
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import thebetweenlands.items.BLItemRegistry;
+import thebetweenlands.items.OctineArmor;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class OctineArmorHandler {
 	@SubscribeEvent
 	public void onEntityOnFire(LivingHurtEvent event) {
 		if (event.entityLiving instanceof EntityPlayer) {
-			if(event.source == DamageSource.inFire || event.source == DamageSource.onFire || event.source == DamageSource.lava) {
-				float damage = 1;
-				
-				ItemStack boots = ((EntityPlayer) event.entityLiving).inventory.armorInventory[0];
-				ItemStack legs = ((EntityPlayer) event.entityLiving).inventory.armorInventory[1];
-				ItemStack chest = ((EntityPlayer) event.entityLiving).inventory.armorInventory[2];
-				ItemStack helm = ((EntityPlayer) event.entityLiving).inventory.armorInventory[3];
-				
-				if (boots != null && boots.getItem() == BLItemRegistry.octineBoots)
-					damage -= 0.25D;
-				if (legs != null && legs.getItem() == BLItemRegistry.octineLeggings)
-					damage -= 0.25D;
-				if (chest != null && chest.getItem() == BLItemRegistry.octineChestplate)
-					damage -= 0.25D;
-				if (helm != null && helm.getItem() == BLItemRegistry.octineHelmet)
-					damage -= 0.25D;
-				
-				if (event.ammount * damage <= 0) {
+			if (event.source == DamageSource.inFire || event.source == DamageSource.onFire || event.source == DamageSource.lava) {
+				float damageMultiplier = 1;
+				ItemStack[] armor = ((EntityPlayer) event.entityLiving).inventory.armorInventory;
+				float reductionAmount = 1F / armor.length;
+				for (int i = 0; i < armor.length; i++) {
+					if (armor[i] != null && armor[i].getItem() instanceof OctineArmor) {
+						damageMultiplier -= reductionAmount;
+					}
+				}
+				if (damageMultiplier < 0.001F) {
 					event.setCanceled(true);
 					event.entityLiving.extinguish();
+				} else {
+					event.ammount *= damageMultiplier;
 				}
-				else
-					event.ammount *= damage;
-				System.out.println("Damage: " + event.ammount * damage);
 			}
 		}
 	}
