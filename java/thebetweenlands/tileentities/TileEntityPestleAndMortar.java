416a51ede0aac9d2b5022612d4c36d1cf2c3d077
vadis365 <curve_linear@hotmail.com>
2015-07-15 22:12:38 +0100

    P&M particles and sound

--- java/thebetweenlands/tileentities/TileEntityPestleAndMortar.java
+++ java/thebetweenlands/tileentities/TileEntityPestleAndMortar.java
@@ -1,11 +1,13 @@
 package thebetweenlands.tileentities;
 
+import net.minecraft.block.Block;
 import net.minecraft.inventory.ICrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
+import thebetweenlands.blocks.BLBlockRegistry;
 import thebetweenlands.inventory.container.ContainerPestleAndMortar;
 import thebetweenlands.items.BLItemRegistry;
 import thebetweenlands.recipes.PestleAndMortarRecipe;
@@ -31,6 +33,10 @@ public class TileEntityPestleAndMortar extends TileEntityBasicInventory {
 		if (pestleInstalled() && !outputIsFull()) {
 			if (output != null && inventory[2] == null || output != null && inventory[2] != null && inventory[2].isItemEqual(output)) {
 				progress++;
+				if(progress == 1)
+					worldObj.playSoundEffect(xCoord, yCoord, zCoord, "thebetweenlands:grind", 2F, 1F);
+				if(progress == 64 || progress == 84)
+					worldObj.playAuxSFXAtEntity(null, 2001, xCoord, yCoord, zCoord, Block.getIdFromBlock(BLBlockRegistry.weedwoodLeaves));
 				if (inventory[1] != null && !getStackInSlot(1).getTagCompound().getBoolean("active"))
 					getStackInSlot(1).getTagCompound().setBoolean("active", true);
 				if (progress > 84) {
