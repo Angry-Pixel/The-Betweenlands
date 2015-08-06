f1d876ebcb362c0da86890fdb7638a3e8d60dd92
pau101 <paul.fulham0@gmail.com>
2015-08-05 21:11:48 -0700

    makes cave entrances significantly less flooded

--- java/thebetweenlands/world/feature/gen/cave/MapGenCavesBetweenlandsExperimental.java
+++ java/thebetweenlands/world/feature/gen/cave/MapGenCavesBetweenlandsExperimental.java
@@ -1,10 +1,12 @@
 package thebetweenlands.world.feature.gen.cave;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.MapGenBase;
+import thebetweenlands.blocks.BLBlockRegistry;
 import thebetweenlands.utils.MathUtils;
 import thebetweenlands.world.biomes.base.BiomeGenBaseBetweenlands;
 
@@ -29,10 +31,10 @@ public class MapGenCavesBetweenlandsExperimental extends MapGenBase {
 		for (int bx = 0; bx < 16; bx++) {
 			for (int bz = 0; bz < 16; bz++) {
 				int x = cx + bx, z = cz + bz;
-				Block fillerBlock = world.getBiomeGenForCoords(x, z).fillerBlock;
 				int xzIndex = (bz * 16 + bx) * slice;
 				int level = 0;
 				while (!(blocks[xzIndex + level] != null && blocks[xzIndex + level++].getMaterial().isLiquid()) && level < seaLevel);
+				boolean brokeSurface = false;
 				for (int y = 0; y <= level; y++) {
 					double noise = cave.eval(x * 0.08, y * 0.15, z * 0.08) + form.eval(x * 0.5, y * 0.3, z * 0.5) * 0.4;
 					double limit = -0.3;
@@ -45,23 +47,46 @@ public class MapGenCavesBetweenlandsExperimental extends MapGenBase {
 							noise += (cut * 0.5 + 0.5) * t;
 							if (level < seaLevel) {
 								noise += (seaLevel - level) / 6D;
-							}
+							} 
 						}
 					}
 					if (noise < limit) {
-						int index = BiomeGenBaseBetweenlands.getBlockArrayIndex(bx, y, bz, slice);
+						if (y == level) {
+							brokeSurface = true;
+						}
+						int index = xzIndex + y;
 						if (blocks[index] == null || blocks[index].getMaterial().isLiquid()) {
 							continue;
 						}
 						blocks[index] = Blocks.air;
 					} else if (y == level && noise < limit + 0.5) {
 						double h = MathUtils.linearTransformd(noise, limit, limit + 0.5, 0, 1);
+						if (h < 0.5) {
+							brokeSurface = true;
+						}
 						// https://www.desmos.com/calculator/nhbhw6jwk6
 						double f1 = Math.sin(2 * Math.PI * (Math.pow(1 - h, 2) - 0.25)) * 0.5 + 0.5;
 						double f2 = Math.sqrt(Math.pow(0.5, 2) - Math.pow(h - 0.5, 2));
 						int height = (int) ((f1 + f2) / 1.4576 * 3);
-						for (int dy = 0; dy < height; dy++) {
-							blocks[BiomeGenBaseBetweenlands.getBlockArrayIndex(bx, y + dy, bz, slice)] = fillerBlock;
+						for (int dy = -1; dy < height; dy++) {
+							blocks[xzIndex + y + dy] = BLBlockRegistry.swampDirt;
+						}
+					}
+				}
+				if (brokeSurface) {
+					int newlevel = seaLevel + 1;
+					while (newlevel > 0) {
+						if (blocks[xzIndex + newlevel] != null) {
+							Material material = blocks[xzIndex + newlevel].getMaterial();
+							if (blocks[xzIndex + newlevel] != BLBlockRegistry.algae && material != Material.air && !material.isLiquid()) {
+								break;
+							}
+						}
+						newlevel--;
+					}
+					if (newlevel > 0) {
+						for (int y = newlevel; y <= seaLevel; y++) {
+							blocks[xzIndex + y] = Blocks.air;
 						}
 					}
 				}
