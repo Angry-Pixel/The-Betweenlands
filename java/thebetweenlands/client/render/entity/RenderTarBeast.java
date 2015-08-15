0a24c6b0bb7354a3f60099f7fbb500d0cb74d01c
vadis365 <curve_linear@hotmail.com>
2015-08-15 20:43:26 +0100

    New Tar Beast Model & Texture

--- java/thebetweenlands/client/render/entity/RenderTarBeast.java
+++ java/thebetweenlands/client/render/entity/RenderTarBeast.java
@@ -1,45 +1,18 @@
 package thebetweenlands.client.render.entity;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
 import thebetweenlands.client.model.entity.ModelTarBeast;
-import thebetweenlands.entities.mobs.EntityTarBeast;
-import thebetweenlands.utils.LightingUtil;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class RenderTarBeast extends RenderLiving {
 	private final ResourceLocation texture = new ResourceLocation("thebetweenlands:textures/entity/tarBeast.png");
-	private final ResourceLocation eyeTexture = new ResourceLocation("thebetweenlands:textures/entity/tarBeastEyes.png");
 
 	public RenderTarBeast() {
 		super(new ModelTarBeast(), 0.7F);
-		setRenderPassModel(new ModelTarBeast());
-	}
-
-	protected int setTarBeastEyeBrightness(EntityTarBeast entity, int pass, float partialTickTime) {
-		if (pass == 1) {
-			bindTexture(eyeTexture);
-			GL11.glEnable(GL11.GL_BLEND);
-			GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE);
-			LightingUtil.INSTANCE.setLighting(255);
-			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			return 1;
-		} else if (pass == 2) {
-			LightingUtil.INSTANCE.revert();
-			GL11.glDisable(GL11.GL_BLEND);
-		}
-
-		return -1;
-	}
-
-	@Override
-	protected int shouldRenderPass(EntityLivingBase entity, int pass, float partialTickTime) {
-		return setTarBeastEyeBrightness((EntityTarBeast) entity, pass, partialTickTime);
 	}
 
 	@Override
