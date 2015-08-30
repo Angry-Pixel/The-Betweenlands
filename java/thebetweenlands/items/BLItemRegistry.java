a2b450bef72a157ffb5b7f5259a5b7213bec5730
imdutch21 <bartpklomp@gmail.com>
2015-08-30 19:25:12 +0200

    removed sound test

--- java/thebetweenlands/items/BLItemRegistry.java
+++ java/thebetweenlands/items/BLItemRegistry.java
@@ -26,7 +26,6 @@ public class BLItemRegistry {
     public static final Item swampTalisman = new SwampTalisman();
     public static final Item materialsBL = new ItemMaterialsBL();
     public static final Item materialCrushed = new ItemMaterialsCrushed().setUnlocalizedName("thebetweenlands.groundStuff"); // TODO fix needing unloc name here to stop null in registry
-    public static final Item soundTest = new ItemSoundTest().setUnlocalizedName("thebetweenlands.testItemSound");
     public static final Item lifeCrystal = new ItemLifeCrystal().setUnlocalizedName("thebetweenlands.lifeCrystal").setTextureName("thebetweenlands:lifeCrystal");
 
     // WEAPONS TOOLS
@@ -157,7 +156,7 @@ public class BLItemRegistry {
         ModCreativeTabs.items.setTab(swampTalisman, materialsBL, sapBall, rottenFood, flatheadMushroomItem, blackHatMushroomItem, bulbCappedMushroomItem, anglerMeatRaw, anglerMeatCooked, frogLegsRaw, frogLegsCooked, snailFleshRaw,
                                      snailFleshCooked, swampKelp, friedSwampKelp, reedDonut, jamDonut, krakenTentacle, krakenCalamari, middleFruit, mincePie, weepingBluePetal,
                                      wightsHeart, yellowDottedFungus, siltCrabClaw, crabstick, nettleSoup, sludgeJello, middleFruitJello, sapJello, marshmallow, marshmallowPink, weedwoodBucket, weedwoodStagnantWater, weedwoodBucketWater, weedwoodBucketTar, lifeCrystal, gertsDonut);
-        ModCreativeTabs.specials.setTab(skullMask, testItem, spawnEggs, angryPebble, scroll, soundTest, voodooDoll, ringOfPower, swiftPick, wightsBane, manual, tarminion);
+        ModCreativeTabs.specials.setTab(skullMask, testItem, spawnEggs, angryPebble, scroll, voodooDoll, ringOfPower, swiftPick, wightsBane, manual, tarminion);
         ModCreativeTabs.gears.setTab(weedwoodSword, weedwoodPickaxe, weedwoodAxe, weedwoodShovel, betweenstoneSword, betweenstonePickaxe, betweenstoneAxe, betweenstoneShovel, octineSword, octinePickaxe, octineAxe, octineShovel, valoniteSword, valonitePickaxe, valoniteAxe, valoniteShovel);
         ModCreativeTabs.gears.setTab(lurkerSkinHelmet, lurkerSkinChestplate, lurkerSkinLeggings, lurkerSkinBoots, boneHelmet, boneChestplate, boneLeggings, boneBoots, octineHelmet, octineChestplate, octineLeggings, octineBoots, valoniteHelmet, valoniteChestplate, valoniteLeggings, valoniteBoots, weedwoodBow, anglerToothArrow, poisonedAnglerToothArrow, octineArrow, basiliskArrow, explorerHat, rubberBoots, rubberBootsImproved);
         ModCreativeTabs.plants.setTab(middleFruitSeeds);
