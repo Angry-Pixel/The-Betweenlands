63e11b841de7c05ce5b9a400c2c4b32d0419070a
vadis365 <curve_linear@hotmail.com>
2015-08-31 22:10:20 +0100

    Tarminion drip overlay

--- java/thebetweenlands/client/render/item/ItemTarminionRenderer.java
+++ java/thebetweenlands/client/render/item/ItemTarminionRenderer.java
@@ -11,7 +11,6 @@ import org.lwjgl.opengl.GL11;
 
 import thebetweenlands.client.model.entity.ModelTarminion;
 import thebetweenlands.entities.EntityThrownTarminion;
-import thebetweenlands.items.ItemTarminion;
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -21,6 +20,8 @@ public class ItemTarminionRenderer extends Render implements IItemRenderer {
 
 	private final ModelTarminion model;
 	public final ResourceLocation texture = new ResourceLocation("thebetweenlands:textures/entity/tarminion.png");
+	public final ResourceLocation textureDrip = new ResourceLocation("thebetweenlands:textures/entity/tarminionOverlay.png");
+
 	public ItemTarminionRenderer() {
 		model = new ModelTarminion();
 	}
@@ -37,40 +38,43 @@ public class ItemTarminionRenderer extends Render implements IItemRenderer {
 
 	@Override
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+		float ticks = (float) (720.0 * (System.currentTimeMillis() & 0x3FFFL) / 0x3FFFL);
 		FMLClientHandler.instance().getClient().getTextureManager().bindTexture(texture);
 		switch (type) {
 			case ENTITY:
-				render(0.0F, 2.5F, 0.0F, 1.5D, (ItemTarminion) item.getItem());
+				render(0.0F, 2.5F, 0.0F, 1.5D, ticks);
 				break;
 			case EQUIPPED:
-				renderEquipped(0.3F, 5F, 1F, 3D, (ItemTarminion) item.getItem());
+				renderEquipped(0.3F, 5F, 1F, 3D, ticks);
 				break;
 			case EQUIPPED_FIRST_PERSON:
-				renderFirstPerson(0F, 4.75F, -1F, 3D, (ItemTarminion) item.getItem());
+				renderFirstPerson(0F, 4.75F, -1F, 3D, ticks);
 				break;
 			case INVENTORY:
-				renderInventory(0.1F, 4.625F, 0.0F, 3D, (ItemTarminion) item.getItem());
+				renderInventory(0.1F, 4.625F, 0.0F, 3D);
 				break;
 			default:
 				break;
 		}
 	}
 
-	private void renderEquipped(float x, float y, float z, double size, ItemTarminion item) {
+	private void renderEquipped(float x, float y, float z, double size, float ticks) {
 		GL11.glPushMatrix();
 		GL11.glTranslatef(x, y, z);
 		GL11.glScaled(-size, -size, size);
 		model.render();
+		renderDrips(ticks);
 		GL11.glPopMatrix();
 	}
 
-	private void render(float x, float y, float z, double size, ItemTarminion item) {
+	private void render(float x, float y, float z, double size, float ticks) {
 		if (RenderItem.renderInFrame) {
 			GL11.glPushMatrix();
-			GL11.glTranslatef(x, y +0.28F, z);
+			GL11.glTranslatef(x, y + 0.28F, z);
 			GL11.glRotatef(180F, 1F, 0, 0);
 			GL11.glScaled(2F, 2F, 2F);
 			model.render();
+			renderDrips(ticks);
 			GL11.glPopMatrix();
 		} else {
 			GL11.glPushMatrix();
@@ -78,21 +82,23 @@ public class ItemTarminionRenderer extends Render implements IItemRenderer {
 			GL11.glRotatef(180F, 1F, 0, 0);
 			GL11.glScaled(size, size, size);
 			model.render();
+			renderDrips(ticks);
 			GL11.glPopMatrix();
 		}
 	}
 
-	private void renderFirstPerson(float x, float y, float z, double size, ItemTarminion item) {
+	private void renderFirstPerson(float x, float y, float z, double size, float ticks) {
 		GL11.glPushMatrix();
 		GL11.glTranslatef(x, y, z);
 		GL11.glRotatef(160F, 1F, 0, 0);
 		GL11.glRotatef(65F, 0, 1F, 0);
 		GL11.glScaled(size, size, size);
 		model.render();
+		renderDrips(ticks);
 		GL11.glPopMatrix();
 	}
 
-	private void renderInventory(float x, float y, float z, double size, ItemTarminion item) {
+	private void renderInventory(float x, float y, float z, double size) {
 		GL11.glPushMatrix();
 		GL11.glTranslatef(x, y, z);
 		GL11.glRotatef(135F, 1F, 0, 0);
@@ -101,6 +107,23 @@ public class ItemTarminionRenderer extends Render implements IItemRenderer {
 		GL11.glScaled(size, size, size);
 		model.render();
 		GL11.glPopMatrix();
+	}
+
+	public void renderDrips(float ticks) {
+		FMLClientHandler.instance().getClient().getTextureManager().bindTexture(textureDrip);
+		GL11.glMatrixMode(GL11.GL_TEXTURE);
+		GL11.glLoadIdentity();
+		float yScroll = ticks * 0.004F;
+		GL11.glTranslatef(0F, -yScroll, 0.0F);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		float colour = 0.5F;
+		GL11.glColor4f(colour, colour, colour, 1.0F);
+		GL11.glDisable(GL11.GL_LIGHTING);
+		model.render();
+		GL11.glMatrixMode(GL11.GL_TEXTURE);
+		GL11.glLoadIdentity();
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glEnable(GL11.GL_LIGHTING);
 	}
 
 	@Override
