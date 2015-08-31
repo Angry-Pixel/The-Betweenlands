ce513af4ccffa3b1df855a72278b5b3017e03b96
Oli <angrypixelminecraft@gmail.com>
2015-08-31 20:16:14 +0100

    Dont ask

---
+++ java/thebetweenlands/items/ItemForbiddenFig.java
@@ -0,0 +1,52 @@
+package thebetweenlands.items;
+
+import java.util.List;
+
+import thebetweenlands.TheBetweenlands;
+import thebetweenlands.utils.IDecayFood;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+
+public class ItemForbiddenFig
+        extends ItemFood implements IDecayFood
+{
+    public ItemForbiddenFig() {
+        super(20, 5.0F, false);
+    }
+    
+    public int getDecayHealAmount()
+    {
+        return 20;
+    }
+    
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean flag) {
+		ItemForbiddenFig item = (ItemForbiddenFig) stack.getItem();
+		if (item == BLItemRegistry.forbiddenFig)
+			list.add("Looks tempting...");
+	}
+
+    @Override
+    protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player) {
+        super.onFoodEaten(stack, world, player);
+
+        if( player != null ) {
+			player.addChatMessage(new ChatComponentText("Cursed are those who eat the Forbidden Fig!"));
+            player.addPotionEffect(new PotionEffect(Potion.blindness.getId(), 1200, 1));
+            player.addPotionEffect(new PotionEffect(Potion.weakness.getId(), 1200, 1));
+			world.playSoundAtEntity(player, "thebetweenlands:fig", 0.7F, 0.8F);
+			
+			 
+        }
+    }
+}
