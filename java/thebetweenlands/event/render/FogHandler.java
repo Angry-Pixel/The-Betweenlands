d062a62e3393d4bc1682d0bd76dcaac66cd5895e
TheCyberBrick <samuelboerlin@bluewin.ch>
2015-10-15 21:25:49 +0200

    Fixed underwater fog not working when the camera is too close to a block

--- java/thebetweenlands/event/render/FogHandler.java
+++ java/thebetweenlands/event/render/FogHandler.java
@@ -1,5 +1,12 @@
 package thebetweenlands.event.render;
 
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ActiveRenderInfo;
@@ -12,9 +19,6 @@ import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.client.event.EntityViewRenderEvent.FogColors;
 import net.minecraftforge.client.event.EntityViewRenderEvent.FogDensity;
 import net.minecraftforge.client.event.EntityViewRenderEvent.RenderFogEvent;
-
-import org.lwjgl.opengl.GL11;
-
 import thebetweenlands.TheBetweenlands;
 import thebetweenlands.blocks.BLBlockRegistry;
 import thebetweenlands.blocks.terrain.BlockSwampWater;
@@ -23,11 +27,6 @@ import thebetweenlands.utils.confighandler.ConfigHandler;
 import thebetweenlands.world.WorldProviderBetweenlands;
 import thebetweenlands.world.biomes.base.BiomeGenBaseBetweenlands;
 import thebetweenlands.world.events.EnvironmentEventRegistry;
-import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 public class FogHandler {
 	public static final FogHandler INSTANCE = new FogHandler();
@@ -35,15 +34,15 @@ public class FogHandler {
 	public float getCurrentFogStart() {
 		return this.currentFogStart;
 	}
-	
+
 	public float getCurrentFogEnd() {
 		return this.currentFogEnd;
 	}
-	
+
 	public int getCurrentFogMode() {
 		return this.fogMode;
 	}
-	
+
 	public boolean hasDenseFog() {
 		World world = Minecraft.getMinecraft().theWorld;
 		if(world.provider instanceof WorldProviderBetweenlands) {
@@ -90,13 +89,13 @@ public class FogHandler {
 	public void onClientTick(TickEvent.ClientTickEvent event) {
 		World world = TheBetweenlands.proxy.getClientWorld();
 		EntityPlayer player = TheBetweenlands.proxy.getClientPlayer();
-		
+
 		if(world == null || player == null || this.farPlaneDistance == 0.0F || player.dimension != ConfigHandler.DIMENSION_ID) return;
-		
+
 		BiomeGenBase biome = world.getBiomeGenForCoords(
 				MathHelper.floor_double(player.posX),
 				MathHelper.floor_double(player.posZ));
-		
+
 		float fogStart = this.farPlaneDistance * 0.25F;
 		float fogEnd = this.farPlaneDistance;
 		if(biome instanceof BiomeGenBaseBetweenlands) {
@@ -104,13 +103,13 @@ public class FogHandler {
 			fogStart = bgbb.getFogStart(this.farPlaneDistance);
 			fogEnd = bgbb.getFogEnd(this.farPlaneDistance);
 		}
-		
+
 		//Dense fog
 		if(this.hasDenseFog()) {
 			fogStart /= 5.0f;
 			fogEnd /= 3.0f;
 		}
-		
+
 		//Underground fog
 		if(player.posY < WorldProviderBetweenlands.CAVE_START) {
 			float multiplier = ((float)(WorldProviderBetweenlands.CAVE_START - player.posY) / WorldProviderBetweenlands.CAVE_START);
@@ -125,10 +124,10 @@ public class FogHandler {
 			this.currentFogStart = fogStart;
 			this.currentFogEnd = fogEnd;
 		}
-		
+
 		this.lastFogStart = this.currentFogStart;
 		this.lastFogEnd = this.currentFogEnd;
-		
+
 		if(Math.abs(this.currentFogStart - fogStart) > 0.1f) {
 			float currentFogStartIncr = Math.abs(this.currentFogStart - fogStart)/this.farPlaneDistance/3.0f;
 			if(this.currentFogStart > fogStart) {
@@ -159,18 +158,16 @@ public class FogHandler {
 			if(renderView == null || renderView.dimension != ConfigHandler.DIMENSION_ID) {
 				return;
 			}
-			if(renderView.isInWater()) {
-				Block block = ActiveRenderInfo.getBlockAtEntityViewpoint(world, renderView, (float) event.renderPartialTicks);
-				if(block instanceof BlockSwampWater) {
-					BiomeGenBase biome = world.getBiomeGenForCoords(
-							MathHelper.floor_double(renderView.posX),
-							MathHelper.floor_double(renderView.posZ));
-					if(biome instanceof BiomeGenBaseBetweenlands) {
-						int colorMultiplier = BLBlockRegistry.swampWater.colorMultiplier(world, MathHelper.floor_double(renderView.posX), MathHelper.floor_double(renderView.posY), MathHelper.floor_double(renderView.posZ));
-						event.red = (float)(colorMultiplier >> 16 & 255) / 255.0F;
-						event.green = (float)(colorMultiplier >> 8 & 255) / 255.0F;
-						event.blue = (float)(colorMultiplier & 255) / 255.0F;
-					}
+			Block block = ActiveRenderInfo.getBlockAtEntityViewpoint(world, renderView, (float) event.renderPartialTicks);
+			if(block instanceof BlockSwampWater) {
+				BiomeGenBase biome = world.getBiomeGenForCoords(
+						MathHelper.floor_double(renderView.posX),
+						MathHelper.floor_double(renderView.posZ));
+				if(biome instanceof BiomeGenBaseBetweenlands) {
+					int colorMultiplier = BLBlockRegistry.swampWater.colorMultiplier(world, MathHelper.floor_double(renderView.posX), MathHelper.floor_double(renderView.posY), MathHelper.floor_double(renderView.posZ));
+					event.red = (float)(colorMultiplier >> 16 & 255) / 255.0F;
+					event.green = (float)(colorMultiplier >> 8 & 255) / 255.0F;
+					event.blue = (float)(colorMultiplier & 255) / 255.0F;
 				}
 			}
 		}
@@ -186,18 +183,16 @@ public class FogHandler {
 			if(renderView == null || renderView.dimension != ConfigHandler.DIMENSION_ID) {
 				return;
 			}
-			if(renderView.isInWater()) {
-				Block block = ActiveRenderInfo.getBlockAtEntityViewpoint(world, renderView, (float) event.renderPartialTicks);
-				if(block instanceof BlockSwampWater) {
-					this.fogMode = GL11.GL_EXP;
-					GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
-	                if (renderView.isPotionActive(Potion.waterBreathing)) {
-	                	event.density = 0.1F;
-	                } else {
-	                	event.density = 0.4F;
-	                }
-					event.setCanceled(true);
+			Block block = ActiveRenderInfo.getBlockAtEntityViewpoint(world, renderView, (float) event.renderPartialTicks);
+			if(block instanceof BlockSwampWater) {
+				this.fogMode = GL11.GL_EXP;
+				GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
+				if (renderView.isPotionActive(Potion.waterBreathing)) {
+					event.density = 0.1F;
+				} else {
+					event.density = 0.4F;
 				}
+				event.setCanceled(true);
 			}
 		}
 	}
