4b93d7bf8830be8392c9852981a53c4a8b376b0b
imdutch21 <bartpklomp@gmail.com>
2015-10-30 22:07:10 +0100

    easier way of adding item pages

--- java/thebetweenlands/manual/gui/entries/IManualEntryItem.java
+++ java/thebetweenlands/manual/gui/entries/IManualEntryItem.java
@@ -8,11 +8,18 @@ import net.minecraft.item.ItemBlock;
  */
 //TODO implement this in all items
 public interface IManualEntryItem {
-    //If any of these don't have to do with the Entity return null
+    //If any of these don't have to do with the Item return null
 
+
+    /* UNUSED
     String manualStats(int meta);
 
     String manualLore(int meta);
 
     String manualTrivia(int meta);
+    */
+
+    String manualName(int meta);
+
+    Item getItem();
 }
---
+++ java/thebetweenlands/manual/gui/entries/ManualEntryItem.java
@@ -0,0 +1,30 @@
+package thebetweenlands.manual.gui.entries;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import thebetweenlands.manual.gui.GuiManualBase;
+import thebetweenlands.manual.gui.ManualPage;
+import thebetweenlands.manual.gui.widgets.ItemWidget;
+import thebetweenlands.manual.gui.widgets.ManualWidgetsBase;
+import thebetweenlands.manual.gui.widgets.TextWidget;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Bart on 30-10-2015.
+ */
+public class ManualEntryItem extends ManualEntry {
+
+    public ManualEntryItem(IManualEntryItem item, GuiManualBase manual, ManualWidgetsBase... recipes){
+        super(new ManualPage(new TextWidget(manual, 5, 5, "manual." + item.manualName(0) + ".title"), new ItemWidget(manual, (127/2)-24, 77, new ItemStack(item.getItem()), 3)), new ManualPage(new TextWidget(manual, 5, 5, "manual." + item.manualName(0)+ ".description")), new ManualPage(recipes));
+    }
+
+    public ManualEntryItem(ArrayList<ItemStack> item, String name, GuiManualBase manual, ManualWidgetsBase... recipes){
+        super(new ManualPage(new TextWidget(manual, 5, 5, "manual." + name + ".title"), new ItemWidget(manual, (127/2)-24, 77, item, 3)), new ManualPage(new TextWidget(manual, 5, 5, "manual." + name + ".description")), new ManualPage(recipes));
+    }
+
+    public ManualEntryItem(IManualEntryItem item, int meta, GuiManualBase manual, ManualWidgetsBase... recipes){
+        super(new ManualPage(new TextWidget(manual, 5, 5, "manual." + item.manualName(meta) + ".title"), new ItemWidget(manual, (127/2)-24, 77, new ItemStack(item.getItem(), 1, meta), 3)), new ManualPage(new TextWidget(manual, 5, 5, "manual." + item.manualName(meta)+ ".description")), new ManualPage(recipes));
+    }
+}
