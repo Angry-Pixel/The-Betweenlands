f28392a5e85d0ca217e09831a7eb6e99601b79af
BobMowzie <jnad325@gmail.com>
2015-11-29 15:32:26 -0500

    Changed swamp hag jaw behavior

--- java/thebetweenlands/client/model/entity/ModelSwampHag.java
+++ java/thebetweenlands/client/model/entity/ModelSwampHag.java
@@ -210,9 +210,10 @@ public class ModelSwampHag extends MowzieModelBase {
 		EntitySwampHag hag = (EntitySwampHag) entity;
 
 		jaw.rotateAngleX = hag.jawFloat;
-		head2.rotateAngleX = hag.jawFloat - 0.8196066167365371F;
+//		head2.rotateAngleX = hag.jawFloat - 0.8196066167365371F;
+        head2.rotateAngleX = - 0.8196066167365371F;
 
-		head1.rotateAngleY = rotationYaw / (180F / (float)Math.PI) - 0.045553093477052F;
+        head1.rotateAngleY = rotationYaw / (180F / (float)Math.PI) - 0.045553093477052F;
 		head1.rotateAngleX = rotationPitch / (180F / (float)Math.PI) - 0.8196066167365371F;
 		head1.rotateAngleZ = rotationPitch / (180F / (float)Math.PI)  + 0.045553093477052F;
 		if (hag.getAttackTarget() != null) { // TODO make this work after some zzzzzzzzzzzz
