1998a8e3c3ac36768dbae7dd11b48f5a7c7195af
imdutch21 <bartpklomp@gmail.com>
2016-01-14 17:31:09 +0100

    fixes crash

--- java/thebetweenlands/event/entity/PageDiscoveringEvent.java
+++ java/thebetweenlands/event/entity/PageDiscoveringEvent.java
@@ -16,19 +16,21 @@ import thebetweenlands.manual.ManualManager;
 public class PageDiscoveringEvent {
     @SubscribeEvent
     public void onLivingUpdate(LivingEvent.LivingUpdateEvent event){
-        if (event.entityLiving != null && (event.entityLiving instanceof IEntityBL || event.entityLiving instanceof EntityDarkDruid)){
+        if (event.entityLiving != null && (event.entityLiving instanceof IEntityBL || event.entityLiving instanceof EntityDarkDruid)) {
             EntityLiving entity = (EntityLiving) event.entityLiving;
             EntityPlayer player = event.entityLiving.worldObj.getClosestPlayerToEntity(event.entityLiving, 20);
-            Vec3 vec3 = player.getLook(1.0F).normalize();
-            Vec3 vec31 = Vec3.createVectorHelper(entity.posX - player.posX, entity.boundingBox.minY + (double) (entity.height / 2.0F) - (player.posY + (double) player.getEyeHeight()), entity.posZ - player.posZ);
-            double d0 = vec31.lengthVector();
-            vec31 = vec31.normalize();
-            double d1 = vec3.dotProduct(vec31);
-            if (d1 > 1.0D - 0.025D / d0 && player.canEntityBeSeen(entity)) {
-                if (entity instanceof IEntityBL )
-                    ManualManager.playerDiscoverPage(player, ((IEntityBL) event.entityLiving).pageName(), BLItemRegistry.manualGuideBook);
-                if (entity instanceof EntityDarkDruid)
-                    ManualManager.playerDiscoverPage(player, "darkDruid", BLItemRegistry.manualGuideBook);
+            if (player != null && player.inventory.hasItem(BLItemRegistry.manualGuideBook)) {
+                Vec3 vec3 = player.getLook(1.0F).normalize();
+                Vec3 vec31 = Vec3.createVectorHelper(entity.posX - player.posX, entity.boundingBox.minY + (double) (entity.height / 2.0F) - (player.posY + (double) player.getEyeHeight()), entity.posZ - player.posZ);
+                double d0 = vec31.lengthVector();
+                vec31 = vec31.normalize();
+                double d1 = vec3.dotProduct(vec31);
+                if (d1 > 1.0D - 0.025D / d0 && player.canEntityBeSeen(entity)) {
+                    if (entity instanceof IEntityBL)
+                        ManualManager.playerDiscoverPage(player, ((IEntityBL) event.entityLiving).pageName(), BLItemRegistry.manualGuideBook);
+                    if (entity instanceof EntityDarkDruid)
+                        ManualManager.playerDiscoverPage(player, "darkDruid", BLItemRegistry.manualGuideBook);
+                }
             }
         }
     }
