551c5c2d7f41dfa07fca62ff71dc098445a71a1f
imdutch21 <bartpklomp@gmail.com>
2016-01-19 18:49:33 +0100

    added new font, need less space between letters though

--- java/thebetweenlands/TheBetweenlands.java
+++ java/thebetweenlands/TheBetweenlands.java
@@ -1,9 +1,5 @@
 package thebetweenlands;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -17,35 +13,24 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.resources.IReloadableResourceManager;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
 import thebetweenlands.blocks.BLBlockRegistry;
 import thebetweenlands.blocks.BLFluidRegistry;
-import thebetweenlands.command.CommandBLEvent;
-import thebetweenlands.command.CommandDecay;
-import thebetweenlands.command.CommandAspectDiscovery;
-import thebetweenlands.command.CommandFindPage;
-import thebetweenlands.command.CommandResetAspects;
-import thebetweenlands.command.CommandTickSpeed;
+import thebetweenlands.command.*;
 import thebetweenlands.entities.BLEntityRegistry;
 import thebetweenlands.entities.properties.BLEntityPropertiesRegistry;
 import thebetweenlands.event.elixirs.ElixirCommonHandler;
-import thebetweenlands.event.entity.AttackDamageHandler;
-import thebetweenlands.event.entity.MiscEntitySyncHandler;
-import thebetweenlands.event.entity.PageDiscoveringEvent;
-import thebetweenlands.event.entity.PowerRingHandler;
-import thebetweenlands.event.entity.VolarPadGlideHandler;
-import thebetweenlands.event.player.ArmorHandler;
-import thebetweenlands.event.player.BonemealEventHandler;
-import thebetweenlands.event.player.DecayEventHandler;
-import thebetweenlands.event.player.OverworldItemEventHandler;
-import thebetweenlands.event.player.PlayerPortalHandler;
-import thebetweenlands.event.player.RottenFoodHandler;
-import thebetweenlands.event.player.SiltCrabClipHandler;
+import thebetweenlands.event.entity.*;
+import thebetweenlands.event.player.*;
 import thebetweenlands.event.world.EnvironmentEventHandler;
 import thebetweenlands.items.BLItemRegistry;
 import thebetweenlands.lib.ModInfo;
@@ -57,14 +42,7 @@ import thebetweenlands.network.base.impl.IDPacketObjectSerializer;
 import thebetweenlands.network.message.MessageLoadAspects;
 import thebetweenlands.network.message.MessageSyncEnvironmentEvent;
 import thebetweenlands.network.message.MessageWeedwoodRowboatInput;
-import thebetweenlands.network.packet.server.PacketAttackTarget;
-import thebetweenlands.network.packet.server.PacketDruidAltarProgress;
-import thebetweenlands.network.packet.server.PacketDruidTeleportParticle;
-import thebetweenlands.network.packet.server.PacketGemProc;
-import thebetweenlands.network.packet.server.PacketRevengeTarget;
-import thebetweenlands.network.packet.server.PacketSnailHatchParticle;
-import thebetweenlands.network.packet.server.PacketTickspeed;
-import thebetweenlands.network.packet.server.PacketWeedWoodBushRustle;
+import thebetweenlands.network.packet.server.*;
 import thebetweenlands.proxy.CommonProxy;
 import thebetweenlands.recipes.RecipeHandler;
 import thebetweenlands.utils.PotionHelper;
@@ -77,148 +55,160 @@ import thebetweenlands.world.feature.structure.WorldGenDruidCircle;
 import thebetweenlands.world.storage.WorldDataBase;
 import thebetweenlands.world.teleporter.TeleporterHandler;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 @Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION, guiFactory = ModInfo.CONFIG_GUI)
-public class TheBetweenlands
-{
-	@SidedProxy(modId = ModInfo.ID, clientSide = ModInfo.CLIENTPROXY_LOCATION, serverSide = ModInfo.COMMONPROXY_LOCATION)
-	public static CommonProxy proxy;
+public class TheBetweenlands {
+    @SidedProxy(modId = ModInfo.ID, clientSide = ModInfo.CLIENTPROXY_LOCATION, serverSide = ModInfo.COMMONPROXY_LOCATION)
+    public static CommonProxy proxy;
 
-	@Instance(ModInfo.ID)
-	public static TheBetweenlands instance;
+    @Instance(ModInfo.ID)
+    public static TheBetweenlands instance;
 
-	/// Network ///
-	public static SimpleNetworkWrapper networkWrapper;
-	public static final SidedPacketHandler sidedPacketHandler = new SidedPacketHandler();
-	public static final IDPacketObjectSerializer packetRegistry = new IDPacketObjectSerializer();
-	@SidedProxy(modId = ModInfo.ID, clientSide = ModInfo.CLIENTPACKETPROXY_LOCATION, serverSide = ModInfo.COMMONPACKETPROXY_LOCATION)
-	public static CommonPacketProxy packetProxy;
-	public static File dir;
-	private static byte nextPacketId = 0;
+    /// Network ///
+    public static SimpleNetworkWrapper networkWrapper;
+    public static final SidedPacketHandler sidedPacketHandler = new SidedPacketHandler();
+    public static final IDPacketObjectSerializer packetRegistry = new IDPacketObjectSerializer();
+    @SidedProxy(modId = ModInfo.ID, clientSide = ModInfo.CLIENTPACKETPROXY_LOCATION, serverSide = ModInfo.COMMONPACKETPROXY_LOCATION)
+    public static CommonPacketProxy packetProxy;
+    public static File dir;
+    private static byte nextPacketId = 0;
 
-	public static ArrayList<String> unlocalizedNames = new ArrayList<>();
+    public static ArrayList<String> unlocalizedNames = new ArrayList<>();
 
-	@EventHandler
-	public static void preInit(FMLPreInitializationEvent event) {
-		//Configuration File
-		ConfigHandler.INSTANCE.loadConfig(event);
-		dir = event.getModConfigurationDirectory();
+    public static FontRenderer pixelLove;
 
-		//BL Registry
-		BLFluidRegistry.init();
-		BLBlockRegistry.init();
-		BLBiomeRegistry.init();
-		BLItemRegistry.init();
-		BLEntityRegistry.init();
+    @EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+        //Configuration File
+        ConfigHandler.INSTANCE.loadConfig(event);
+        dir = event.getModConfigurationDirectory();
 
-		GameRegistry.registerWorldGenerator(new WorldGenDruidCircle(), 0);
+        //BL Registry
+        BLFluidRegistry.init();
+        BLBlockRegistry.init();
+        BLBiomeRegistry.init();
+        BLItemRegistry.init();
+        BLEntityRegistry.init();
 
-		NetworkRegistry.INSTANCE.registerGuiHandler(instance, proxy);
+        GameRegistry.registerWorldGenerator(new WorldGenDruidCircle(), 0);
 
-		DimensionManager.registerProviderType(ModInfo.DIMENSION_ID, WorldProviderBetweenlands.class, true);
-		DimensionManager.registerDimension(ModInfo.DIMENSION_ID, ModInfo.DIMENSION_ID);
+        NetworkRegistry.INSTANCE.registerGuiHandler(instance, proxy);
 
-		//Message Registry
-		networkWrapper = NetworkRegistry.INSTANCE.newSimpleChannel(ModInfo.CHANNEL);
-		networkWrapper.registerMessage(MessageSyncEnvironmentEvent.class, MessageSyncEnvironmentEvent.class, 4, Side.CLIENT);
-		networkWrapper.registerMessage(MessageWeedwoodRowboatInput.class, MessageWeedwoodRowboatInput.class, 5, Side.SERVER);
-		networkWrapper.registerMessage(MessageLoadAspects.class, MessageLoadAspects.class, 6, Side.CLIENT);
-		BLEntityPropertiesRegistry.HANDLER.registerPacket(networkWrapper, 7);
+        DimensionManager.registerProviderType(ModInfo.DIMENSION_ID, WorldProviderBetweenlands.class, true);
+        DimensionManager.registerDimension(ModInfo.DIMENSION_ID, ModInfo.DIMENSION_ID);
 
-		sidedPacketHandler.setProxy(packetProxy).setNetworkWrapper(networkWrapper, 20, 21).setPacketSerializer(packetRegistry);
+        //Message Registry
+        networkWrapper = NetworkRegistry.INSTANCE.newSimpleChannel(ModInfo.CHANNEL);
+        networkWrapper.registerMessage(MessageSyncEnvironmentEvent.class, MessageSyncEnvironmentEvent.class, 4, Side.CLIENT);
+        networkWrapper.registerMessage(MessageWeedwoodRowboatInput.class, MessageWeedwoodRowboatInput.class, 5, Side.SERVER);
+        networkWrapper.registerMessage(MessageLoadAspects.class, MessageLoadAspects.class, 6, Side.CLIENT);
+        BLEntityPropertiesRegistry.HANDLER.registerPacket(networkWrapper, 7);
 
-		//Packet Registry
-		registerPacket(PacketDruidAltarProgress.class);
-		registerPacket(PacketDruidTeleportParticle.class);
-		registerPacket(PacketSnailHatchParticle.class);
-		registerPacket(PacketTickspeed.class);
-		registerPacket(PacketRevengeTarget.class);
-		registerPacket(PacketAttackTarget.class);
-		registerPacket(PacketWeedWoodBushRustle.class);
-		registerPacket(PacketGemProc.class);
-	}
+        sidedPacketHandler.setProxy(packetProxy).setNetworkWrapper(networkWrapper, 20, 21).setPacketSerializer(packetRegistry);
 
-	private static void registerPacket(Class<? extends IPacket> packetClass) {
-		packetRegistry.registerPacket(packetClass, nextPacketId++);
-	}
+        //Packet Registry
+        registerPacket(PacketDruidAltarProgress.class);
+        registerPacket(PacketDruidTeleportParticle.class);
+        registerPacket(PacketSnailHatchParticle.class);
+        registerPacket(PacketTickspeed.class);
+        registerPacket(PacketRevengeTarget.class);
+        registerPacket(PacketAttackTarget.class);
+        registerPacket(PacketWeedWoodBushRustle.class);
+        registerPacket(PacketGemProc.class);
+    }
 
-	@EventHandler
-	@SuppressWarnings("unchecked")
-	public void init(FMLInitializationEvent event) {
-		// Remove all the door recipes.
-		// This is needed otherwise our doors will not be craftable due to the
-		// recipe ordering
-		List<IRecipe> doorRecipes = new ArrayList<IRecipe>();
-		for (IRecipe recipe : (List<IRecipe>) CraftingManager.getInstance().getRecipeList())
-			if (recipe != null) {
-				ItemStack stack = recipe.getRecipeOutput();
-				if (stack != null && stack.getItem() == Items.wooden_door)
-					doorRecipes.add(recipe);
-			}
-		for (IRecipe recipe : doorRecipes)
-			CraftingManager.getInstance().getRecipeList().remove(recipe);
+    private static void registerPacket(Class<? extends IPacket> packetClass) {
+        packetRegistry.registerPacket(packetClass, nextPacketId++);
+    }
 
-		proxy.registerTileEntities();
-		proxy.init();
+    @EventHandler
+    @SuppressWarnings("unchecked")
+    public void init(FMLInitializationEvent event) {
+        // Remove all the door recipes.
+        // This is needed otherwise our doors will not be craftable due to the
+        // recipe ordering
+        List<IRecipe> doorRecipes = new ArrayList<IRecipe>();
+        for (IRecipe recipe : (List<IRecipe>) CraftingManager.getInstance().getRecipeList())
+            if (recipe != null) {
+                ItemStack stack = recipe.getRecipeOutput();
+                if (stack != null && stack.getItem() == Items.wooden_door)
+                    doorRecipes.add(recipe);
+            }
+        for (IRecipe recipe : doorRecipes)
+            CraftingManager.getInstance().getRecipeList().remove(recipe);
 
-		PotionHelper.initPotionArray();
-		PotionHelper.registerPotions();
+        proxy.registerTileEntities();
+        proxy.init();
 
-		FMLCommonHandler.instance().bus().register(ConfigHandler.INSTANCE);
-		FMLCommonHandler.instance().bus().register(DecayEventHandler.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(BLFluidRegistry.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(new ArmorHandler());
-		MinecraftForge.EVENT_BUS.register(new OverworldItemEventHandler());
-		MinecraftForge.EVENT_BUS.register(DecayEventHandler.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(RottenFoodHandler.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(new PlayerPortalHandler());
-		MinecraftForge.EVENT_BUS.register(new PowerRingHandler());
-		MinecraftForge.EVENT_BUS.register(new VolarPadGlideHandler());
-		MinecraftForge.EVENT_BUS.register(new PageDiscoveringEvent());
-		FMLCommonHandler.instance().bus().register(EnvironmentEventHandler.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(EnvironmentEventHandler.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(new BonemealEventHandler());
-		MinecraftForge.EVENT_BUS.register(new SiltCrabClipHandler());
-		MinecraftForge.EVENT_BUS.register(new MiscEntitySyncHandler());
-		MinecraftForge.EVENT_BUS.register(AttackDamageHandler.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(ElixirCommonHandler.INSTANCE);
-		FMLCommonHandler.instance().bus().register(ElixirCommonHandler.INSTANCE);
-		BLEntityPropertiesRegistry.HANDLER.registerHandler();
-		FMLCommonHandler.instance().bus().register(MobSpawnHandler.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(MobSpawnHandler.INSTANCE);
-		MinecraftForge.EVENT_BUS.register(WorldDataBase.WORLD_UNLOAD_HANDLER);
+        PotionHelper.initPotionArray();
+        PotionHelper.registerPotions();
 
-		RecipeHandler.init();
-		TeleporterHandler.init();
+        FMLCommonHandler.instance().bus().register(ConfigHandler.INSTANCE);
+        FMLCommonHandler.instance().bus().register(DecayEventHandler.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(BLFluidRegistry.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(new ArmorHandler());
+        MinecraftForge.EVENT_BUS.register(new OverworldItemEventHandler());
+        MinecraftForge.EVENT_BUS.register(DecayEventHandler.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(RottenFoodHandler.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(new PlayerPortalHandler());
+        MinecraftForge.EVENT_BUS.register(new PowerRingHandler());
+        MinecraftForge.EVENT_BUS.register(new VolarPadGlideHandler());
+        MinecraftForge.EVENT_BUS.register(new PageDiscoveringEvent());
+        FMLCommonHandler.instance().bus().register(EnvironmentEventHandler.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(EnvironmentEventHandler.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(new BonemealEventHandler());
+        MinecraftForge.EVENT_BUS.register(new SiltCrabClipHandler());
+        MinecraftForge.EVENT_BUS.register(new MiscEntitySyncHandler());
+        MinecraftForge.EVENT_BUS.register(AttackDamageHandler.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(ElixirCommonHandler.INSTANCE);
+        FMLCommonHandler.instance().bus().register(ElixirCommonHandler.INSTANCE);
+        BLEntityPropertiesRegistry.HANDLER.registerHandler();
+        FMLCommonHandler.instance().bus().register(MobSpawnHandler.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(MobSpawnHandler.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(WorldDataBase.WORLD_UNLOAD_HANDLER);
 
-		// Add the other door recipes back
-		CraftingManager.getInstance().getRecipeList().addAll(doorRecipes);
+        RecipeHandler.init();
+        TeleporterHandler.init();
 
-		RecurrentComplexModule.init();
-	}
+        // Add the other door recipes back
+        CraftingManager.getInstance().getRecipeList().addAll(doorRecipes);
 
-	@EventHandler
-	@SuppressWarnings("unchecked")
-	public void postInit(FMLPostInitializationEvent event) {
-		proxy.postInit();
-		if (ConfigHandler.DEBUG){
-			System.out.println("==================================================");
-			for (String name:unlocalizedNames){
-				System.out.println("needs translation: " + name);
-			}
-			System.out.println("==================================================");
-		}
-	}
+        RecurrentComplexModule.init();
+    }
 
-	@EventHandler
-	public void serverLoad(FMLServerStartingEvent event) {
-		event.registerServerCommand(new CommandBLEvent());
-		event.registerServerCommand(new CommandResetAspects());
-		event.registerServerCommand(new CommandDecay());
-		event.registerServerCommand(new CommandFindPage());
-		event.registerServerCommand(new CommandAspectDiscovery());
-		if (ConfigHandler.DEBUG) {
-			event.registerServerCommand(new CommandTickSpeed());
-		}
-		BLGamerules.INSTANCE.onServerStarting(event);
-	}
+    @EventHandler
+    @SuppressWarnings("unchecked")
+    public void postInit(FMLPostInitializationEvent event) {
+        pixelLove = new FontRenderer(Minecraft.getMinecraft().gameSettings, new ResourceLocation("thebetweenlands:textures/gui/manual/fontAtlas.png"), Minecraft.getMinecraft().renderEngine, false);
+
+        if (Minecraft.getMinecraft().gameSettings.language != null) {
+            pixelLove.setBidiFlag(Minecraft.getMinecraft().getLanguageManager().isCurrentLanguageBidirectional());
+        }
+        ((IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager()).registerReloadListener(pixelLove);
+
+        proxy.postInit();
+        if (ConfigHandler.DEBUG) {
+            System.out.println("==================================================");
+            for (String name : unlocalizedNames) {
+                System.out.println("needs translation: " + name);
+            }
+            System.out.println("==================================================");
+        }
+    }
+
+    @EventHandler
+    public void serverLoad(FMLServerStartingEvent event) {
+        event.registerServerCommand(new CommandBLEvent());
+        event.registerServerCommand(new CommandResetAspects());
+        event.registerServerCommand(new CommandDecay());
+        event.registerServerCommand(new CommandFindPage());
+        event.registerServerCommand(new CommandAspectDiscovery());
+        if (ConfigHandler.DEBUG) {
+            event.registerServerCommand(new CommandTickSpeed());
+        }
+        BLGamerules.INSTANCE.onServerStarting(event);
+    }
 }
