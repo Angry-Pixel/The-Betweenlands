28004c128f5b77e87c8de70425c560f83a062a83
BobMowzie <jnad325@gmail.com>
2016-02-16 16:16:20 -0500

    Dreadful mummy is ready to be developed

--- java/thebetweenlands/client/render/entity/RenderDreadfulMummy.java
+++ java/thebetweenlands/client/render/entity/RenderDreadfulMummy.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 import thebetweenlands.client.model.entity.ModelDreadfulMummy;
-import thebetweenlands.entities.mobs.EntityPeatMummy;
 
 @SideOnly(Side.CLIENT)
 public class RenderDreadfulMummy extends RenderLiving {
@@ -24,9 +23,6 @@ public class RenderDreadfulMummy extends RenderLiving {
 	
 	@Override
 	public void doRender(EntityLiving entity, double x, double y, double z, float yaw, float pitch) {
-		if(((EntityPeatMummy)entity).getSpawningProgress() < 0.1F) {
-			return;
-		}
         super.doRender(entity, x, y, z, yaw, pitch);
     }
 }
