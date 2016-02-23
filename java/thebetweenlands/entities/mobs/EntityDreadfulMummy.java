9fbae6d6fdd5aa76488f4a02899277b3d7eabd29
BobMowzie <jnad325@gmail.com>
2016-02-23 15:04:54 -0500

    Basic mummy spawning

--- java/thebetweenlands/entities/mobs/EntityDreadfulMummy.java
+++ java/thebetweenlands/entities/mobs/EntityDreadfulMummy.java
@@ -1,5 +1,6 @@
 package thebetweenlands.entities.mobs;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.world.World;
@@ -12,6 +13,8 @@ public class EntityDreadfulMummy extends EntityMob implements IEntityBL {
         super(p_i1738_1_);
     }
 
+    static final int SPAWN_MUMMY_COOLDOWN = 300;
+    int untilSpawnMummy = 0;
     @Override
     public String pageName() {
         return null;
@@ -20,10 +23,28 @@ public class EntityDreadfulMummy extends EntityMob implements IEntityBL {
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
-        getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(2);
+        getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(1);
         getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(110.0D);
         getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(1);
         getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(80.0D);
         getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(1.0D);
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        if (getEntityToAttack() != null) {
+            System.out.println("Spawn");
+            if (untilSpawnMummy <= 0) spawnMummy();
+        }
+        if(untilSpawnMummy > 0) untilSpawnMummy--;
+    }
+
+    private void spawnMummy() {
+        untilSpawnMummy = SPAWN_MUMMY_COOLDOWN;
+        EntityPeatMummy mummy = new EntityPeatMummy(worldObj);
+        mummy.setPosition(posX + (rand.nextInt(6) - 3), posY, posZ + (rand.nextInt(6) - 3));
+        worldObj.spawnEntityInWorld(mummy);
+        mummy.setAttackTarget((EntityLivingBase) getEntityToAttack());
     }
 }
