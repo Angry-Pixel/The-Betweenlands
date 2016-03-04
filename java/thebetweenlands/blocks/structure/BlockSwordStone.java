ef9889f3546ea02619968c78acdc57944e0ead91
vadis365 <curve_linear@hotmail.com>
2016-03-04 19:41:49 +0000

    to stop spam and show block

--- java/thebetweenlands/blocks/structure/BlockSwordStone.java
+++ java/thebetweenlands/blocks/structure/BlockSwordStone.java
@@ -2,7 +2,11 @@ package thebetweenlands.blocks.structure;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.world.IBlockAccess;
 import thebetweenlands.creativetabs.BLCreativeTabs;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockSwordStone extends Block {
 	public BlockSwordStone() {
@@ -14,12 +18,34 @@ public class BlockSwordStone extends Block {
 		setLightLevel(0.8F);
 		setCreativeTab(BLCreativeTabs.blocks);
 		setBlockName("thebetweenlands.swordStone");
-		setBlockTextureName("thebetweenlands:swordStone");
+		setBlockTextureName("thebetweenlands:polishedDentrothyst21"); //temp spam reduction 
 	}
 
 	@Override
-	public int getRenderType() {
-		return 0;
-	}
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister register) {
+        this.blockIcon = register.registerIcon(textureName);
+    }
+
+    @Override
+    public boolean shouldSideBeRendered (IBlockAccess iblockaccess, int x, int y, int z, int side) {
+        Block block = iblockaccess.getBlock(x, y, z);
+        return block != this;
+    }
+
+    @Override
+    public boolean renderAsNormalBlock() {
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
+        return false;
+    }
+
+    @Override
+    public int getRenderBlockPass () {
+        return 1;
+    }
 
 }
