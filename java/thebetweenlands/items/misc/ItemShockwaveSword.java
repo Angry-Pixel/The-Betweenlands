d8156a5c192c2819e497cfb087ca3b713a56b967
TheCyberBrick <samuelboerlin@bluewin.ch>
2016-03-12 21:29:17 +0100

    Fixed Shockwave Sword not extending ItemSwordBL

--- java/thebetweenlands/items/misc/ItemShockwaveSword.java
+++ java/thebetweenlands/items/misc/ItemShockwaveSword.java
@@ -2,10 +2,11 @@ package thebetweenlands.items.misc;
 
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.IIcon;
+import thebetweenlands.items.tools.ItemSwordBL;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemShockwaveSword extends ItemSword {
+public class ItemShockwaveSword extends ItemSwordBL {
 
 	@SideOnly(Side.CLIENT)
 	private IIcon[] icons;
