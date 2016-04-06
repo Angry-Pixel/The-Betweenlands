322491efa20a4498ef263d962fb674e94fc7b878
TheCyberBrick <samuelboerlin@bluewin.ch>
2016-04-06 20:55:53 +0200

    Fixed Syrmorite Shears not working on leaves

--- java/thebetweenlands/items/tools/ItemSyrmoriteShears.java
+++ java/thebetweenlands/items/tools/ItemSyrmoriteShears.java
@@ -1,10 +1,14 @@
 package thebetweenlands.items.tools;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -13,6 +17,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import net.minecraftforge.common.IShearable;
 import thebetweenlands.creativetabs.BLCreativeTabs;
 import thebetweenlands.items.ICorrodible;
 import thebetweenlands.utils.CorrodibleItemHelper;
@@ -62,6 +67,60 @@ public class ItemSyrmoriteShears extends Item implements ICorrodible {
 				player.addStat(StatList.mineBlockStatArray[Block.getIdFromBlock(block)], 1);
 				return true;
 			}
+		} else if(block instanceof IShearable) {
+			IShearable target = (IShearable)block;
+			if (target.isShearable(itemstack, player.worldObj, x, y, z)) {
+				ArrayList<ItemStack> drops = target.onSheared(itemstack, player.worldObj, x, y, z,
+						EnchantmentHelper.getEnchantmentLevel(Enchantment.fortune.effectId, itemstack));
+				Random rand = new Random();
+
+				for(ItemStack stack : drops) {
+					float f = 0.7F;
+					double d  = (double)(rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
+					double d1 = (double)(rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
+					double d2 = (double)(rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
+					EntityItem entityitem = new EntityItem(player.worldObj, (double)x + d, (double)y + d1, (double)z + d2, stack);
+					entityitem.delayBeforeCanPickup = 10;
+					player.worldObj.spawnEntityInWorld(entityitem);
+				}
+
+				itemstack.damageItem(1, player);
+				player.addStat(StatList.mineBlockStatArray[Block.getIdFromBlock(block)], 1);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onBlockDestroyed(ItemStack stack, World world, Block block, int x, int y, int z, EntityLivingBase entity) {
+		if (block instanceof ISyrmoriteShearable == false && block instanceof IShearable == false) {
+			return super.onBlockDestroyed(stack, world, block, x, y, z, entity);
+		} else {
+			return true;
+		}
+	}
+
+	@Override
+	public boolean itemInteractionForEntity(ItemStack itemstack, EntityPlayer player, EntityLivingBase entity) {
+		if (entity.worldObj.isRemote) {
+			return false;
+		}
+		if (entity instanceof IShearable) {
+			IShearable target = (IShearable)entity;
+			if (target.isShearable(itemstack, entity.worldObj, (int)entity.posX, (int)entity.posY, (int)entity.posZ)) {
+				ArrayList<ItemStack> drops = target.onSheared(itemstack, entity.worldObj, (int)entity.posX, (int)entity.posY, (int)entity.posZ,
+						EnchantmentHelper.getEnchantmentLevel(Enchantment.fortune.effectId, itemstack));
+
+				Random rand = new Random();
+				for(ItemStack stack : drops) {
+					EntityItem ent = entity.entityDropItem(stack, 1.0F);
+					ent.motionY += rand.nextFloat() * 0.05F;
+					ent.motionX += (rand.nextFloat() - rand.nextFloat()) * 0.1F;
+					ent.motionZ += (rand.nextFloat() - rand.nextFloat()) * 0.1F;
+				}
+				itemstack.damageItem(1, entity);
+			}
+			return true;
 		}
 		return false;
 	}
@@ -85,7 +144,7 @@ public class ItemSyrmoriteShears extends Item implements ICorrodible {
 	public void setCorrosionIcons(IIcon[][] corrosionIcons) {
 		this.corrosionIcons = corrosionIcons[0];
 	}
-	
+
 	@Override
 	public void onUpdate(ItemStack itemStack, World world, Entity holder, int slot, boolean isHeldItem) {
 		CorrodibleItemHelper.onUpdate(itemStack, world, holder, slot, isHeldItem);
