dff49b173ea388e61b072b90c418c9dd2fa23f23
vadis365 <curve_linear@hotmail.com>
2016-04-10 02:59:51 +0100

    frogs WIP remove before release
    Unfinished and badly coded atm

--- java/thebetweenlands/client/model/entity/ModelFrog.java
+++ java/thebetweenlands/client/model/entity/ModelFrog.java
@@ -3,6 +3,8 @@ package thebetweenlands.client.model.entity;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import thebetweenlands.entities.mobs.EntityFrog;
 
 public class ModelFrog extends ModelBase {
 	ModelRenderer torso;
@@ -20,46 +22,48 @@ public class ModelFrog extends ModelBase {
 		textureWidth = 64;
 		textureHeight = 32;
 
-		torso = new ModelRenderer(this, 0, 0);
-		torso.addBox(-2.5F, -1F, 0F, 5, 3, 6);
-		torso.setRotationPoint(0F, 19F, 0F);
-		setRotation(torso, -0.5948578F, 0F, 0F);
 		head = new ModelRenderer(this, 0, 10);
-		head.addBox(-1.5F, -1F, -3F, 3, 2, 3);
-		head.setRotationPoint(0F, 19F, 0F);
-		setRotation(head, -0.0743572F, 0F, 0F);
-		legfrontleft1 = new ModelRenderer(this, 25, 0);
-		legfrontleft1.addBox(0F, -1F, -0.5F, 1, 3, 2);
-		legfrontleft1.setRotationPoint(2F, 20F, 0F);
-		setRotation(legfrontleft1, -0.0743572F, 0.1858931F, -0.3717861F);
-		legfrontright1 = new ModelRenderer(this, 32, 0);
-		legfrontright1.addBox(-1F, -1F, -0.5F, 1, 3, 2);
-		legfrontright1.setRotationPoint(-2F, 20F, 0F);
-		setRotation(legfrontright1, -0.074351F, -0.185895F, 0.37179F);
+		head.setRotationPoint(0.0F, 19.0F, 0.0F);
+		head.addBox(-1.5F, -1.0F, -3.0F, 3, 2, 3, 0.0F);
+		setRotation(head, -0.07435719668865202F, 0.0F, 0.0F);
 		legfrontleft2 = new ModelRenderer(this, 25, 6);
-		legfrontleft2.addBox(1F, 0.5F, -0.5F, 1, 3, 1);
-		legfrontleft2.setRotationPoint(2F, 20F, 0F);
-		setRotation(legfrontleft2, -0.4089647F, 0F, 0.5948578F);
-		legfrontright2 = new ModelRenderer(this, 30, 6);
-		legfrontright2.addBox(-2F, 0.5F, -0.5F, 1, 3, 1);
-		legfrontright2.setRotationPoint(-2F, 20F, 0F);
-		setRotation(legfrontright2, -0.4089647F, 0F, -0.5948606F);
+		legfrontleft2.setRotationPoint(2.0F, 20.0F, 0.0F);
+		legfrontleft2.addBox(1.0F, 0.5F, -0.5F, 1, 3, 1, 0.0F);
+		setRotation(legfrontleft2, -0.40896469354629517F, 0.0F, 0.5948578119277954F);
+		torso = new ModelRenderer(this, 0, 0);
+		torso.setRotationPoint(0.0F, 19.0F, 0.0F);
+		torso.addBox(-2.5F, -1.0F, 0.0F, 5, 3, 6, 0.0F);
+		setRotation(torso, -0.5948578119277954F, 0.0F, 0.0F);
 		legbackleft1 = new ModelRenderer(this, 25, 11);
-		legbackleft1.addBox(-0.5F, -1.5F, -0.5F, 2, 2, 3);
-		legbackleft1.setRotationPoint(2F, 22F, 3F);
-		setRotation(legbackleft1, -0.2974289F, -0.1487144F, -0.1858931F);
-		legbackright1 = new ModelRenderer(this, 36, 11);
-		legbackright1.addBox(-1.5F, -1.5F, -0.5F, 2, 2, 3);
-		legbackright1.setRotationPoint(-2F, 22F, 3F);
-		setRotation(legbackright1, -0.2974216F, 0.1487195F, 0.185895F);
+		legbackleft1.setRotationPoint(2.0F, 22.0F, 3.0F);
+		legbackleft1.addBox(-0.5F, -1.5F, -0.5F, 2, 2, 3, 0.0F);
+		setRotation(legbackleft1, -0.296705972839036F, -0.15707963267948966F, -0.19198621771937624F);
 		legbackleft2 = new ModelRenderer(this, 25, 17);
-		legbackleft2.addBox(0.5F, 1F, -2F, 1, 1, 4);
-		legbackleft2.setRotationPoint(2F, 22F, 3F);
-		setRotation(legbackleft2, 0.1487144F, -0.1115358F, 0F);
+		legbackleft2.setRotationPoint(1.0F, 0.3F, 2.0F);
+		legbackleft2.addBox(-0.5F, 0.1F, -3.9F, 1, 1, 4, 0.0F);
+		setRotation(legbackleft2, 0.45378560551852565F, 0.0F, 0.19198621771937624F);
 		legbackright2 = new ModelRenderer(this, 36, 17);
-		legbackright2.addBox(-1.5F, 1F, -2F, 1, 1, 4);
-		legbackright2.setRotationPoint(-2F, 22F, 3F);
-		setRotation(legbackright2, 0.1487144F, 0.111544F, 0F);
+		legbackright2.setRotationPoint(-1.0F, 0.3F, 2.0F);
+		legbackright2.addBox(-0.5F, 0.1F, -3.9F, 1, 1, 4, 0.0F);
+		setRotation(legbackright2, 0.45378560551852565F, 0.0F, -0.19198621771937624F);
+		legbackright1 = new ModelRenderer(this, 36, 11);
+		legbackright1.setRotationPoint(-2.0F, 22.0F, 3.0F);
+		legbackright1.addBox(-1.5F, -1.5F, -0.5F, 2, 2, 3, 0.0F);
+		setRotation(legbackright1, -0.296705972839036F, 0.15707963267948966F, 0.19198621771937624F);
+		legfrontright2 = new ModelRenderer(this, 30, 6);
+		legfrontright2.setRotationPoint(-2.0F, 20.0F, 0.0F);
+		legfrontright2.addBox(-2.0F, 0.5F, -0.5F, 1, 3, 1, 0.0F);
+		setRotation(legfrontright2, -0.40896469354629517F, 0.0F, -0.5948606133460999F);
+		legfrontright1 = new ModelRenderer(this, 32, 0);
+		legfrontright1.setRotationPoint(-2.0F, 20.0F, 0.0F);
+		legfrontright1.addBox(-1.0F, -1.0F, -0.5F, 1, 3, 2, 0.0F);
+		setRotation(legfrontright1, -0.0743509978055954F, -0.18589499592781067F, 0.37178999185562134F);
+		legfrontleft1 = new ModelRenderer(this, 25, 0);
+		legfrontleft1.setRotationPoint(2.0F, 20.0F, 0.0F);
+		legfrontleft1.addBox(0.0F, -1.0F, -0.5F, 1, 3, 2, 0.0F);
+		setRotation(legfrontleft1, -0.07435719668865202F, 0.18589310348033902F, -0.3717860877513886F);
+		legbackleft1.addChild(legbackleft2);
+		legbackright1.addChild(legbackright2);
 	}
 
 	@Override
@@ -74,8 +78,6 @@ public class ModelFrog extends ModelBase {
 		legfrontright2.render(unitPixel);
 		legbackleft1.render(unitPixel);
 		legbackright1.render(unitPixel);
-		legbackleft2.render(unitPixel);
-		legbackright2.render(unitPixel);
 	}
 
 	private void setRotation(ModelRenderer model, float x, float y, float z) {
@@ -83,10 +85,46 @@ public class ModelFrog extends ModelBase {
 		model.rotateAngleY = y;
 		model.rotateAngleZ = z;
 	}
-
+	
 	@Override
-	public void setRotationAngles(float limbSwing, float limbSwingAngle, float entityTickTime, float rotationYaw, float rotationPitch, float unitPixel, Entity entity) {
-		super.setRotationAngles(limbSwing, limbSwingAngle, entityTickTime, rotationYaw, rotationPitch, unitPixel, entity);
+	public void setLivingAnimations(EntityLivingBase entity, float swing, float speed, float partialRenderTicks) {
+		EntityFrog frog = (EntityFrog)entity;
+		float leapProgress = frog.jumpticks + (frog.jumpticks - frog.prevJumpticks) * partialRenderTicks;
+		if (frog.onGround) {
+			legbackleft1.rotateAngleX = -0.296705972839036F;
+			legbackright1.rotateAngleX = -0.296705972839036F;
+			legbackleft2.rotateAngleX = 0.45378560551852565F;
+			legbackright2.rotateAngleX = 0.45378560551852565F;
+			
+			legfrontleft1.rotateAngleZ = -0.3717860877513886F;
+			legfrontright1.rotateAngleZ = 0.37178999185562134F;
+			legfrontleft2.rotateAngleZ = 0.5948578119277954F;
+			legfrontright2.rotateAngleZ = -0.5948606133460999F;
+		}
+		else {
+			if (frog.jumpticks > 0 && frog.jumpticks <= 7) {
+				legbackleft1.rotateAngleX = -0.296705972839036F + 0.15F * leapProgress;
+				legbackright1.rotateAngleX = -0.296705972839036F + 0.15F * leapProgress;
+				legbackleft2.rotateAngleX = 0.45378560551852565F + 0.2F * leapProgress;
+				legbackright2.rotateAngleX = 0.45378560551852565F + 0.2F * leapProgress;
+
+				legfrontleft1.rotateAngleZ = -0.3717860877513886F - 0.15F * leapProgress;
+				legfrontright1.rotateAngleZ = 0.37178999185562134F + 0.15F * leapProgress;
+				legfrontleft2.rotateAngleZ = 0.5948578119277954F - 0.15F * leapProgress;
+				legfrontright2.rotateAngleZ = -0.5948606133460999F + 0.15F * leapProgress;
+			}
+			if (frog.jumpticks > 7 && frog.jumpticks <= 14) {
+				legbackleft1.rotateAngleX = -0.296705972839036F + 1.05F - 0.075F * leapProgress;
+				legbackright1.rotateAngleX = -0.296705972839036F + 1.05F - 0.075F * leapProgress;
+				legbackleft2.rotateAngleX = 0.45378560551852565F + 1.4F - 0.1F * leapProgress;
+				legbackright2.rotateAngleX = 0.45378560551852565F + 1.4F - 0.1F * leapProgress;
+
+				legfrontleft1.rotateAngleZ = -0.3717860877513886F - 1.05F + 0.075F * leapProgress;
+				legfrontright1.rotateAngleZ = 0.37178999185562134F + 1.05F - 0.075F * leapProgress;
+				legfrontleft2.rotateAngleZ = 0.5948578119277954F - 1.05F + 0.075F * leapProgress;
+				legfrontright2.rotateAngleZ = -0.5948606133460999F + 1.05F - 0.075F * leapProgress;
+			}
+		}
 	}
 
 }
--- java/thebetweenlands/entities/mobs/EntityFrog.java
+++ java/thebetweenlands/entities/mobs/EntityFrog.java
@@ -3,56 +3,126 @@ package thebetweenlands.entities.mobs;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAIPanic;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
-import net.minecraft.entity.ai.EntityAIWatchClosest;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.pathfinding.PathEntity;
+import net.minecraft.pathfinding.PathPoint;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import thebetweenlands.items.BLItemRegistry;
 import thebetweenlands.items.misc.ItemGeneric;
 
 public class EntityFrog extends EntityCreature {
-
+	private int ticksOnGround = 0;
+	public int jumpticks;
+	public int prevJumpticks;
+	private boolean prevOnGround;
 	public EntityFrog(World world) {
 		super(world);
 		tasks.addTask(0, new EntityAISwimming(this));
-		tasks.addTask(1, new EntityAIPanic(this, 0.4D));
-		tasks.addTask(5, new EntityAIWander(this, 0.4D));
-		tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+		tasks.addTask(5, new EntityAIWander(this, 0.0D));
 		tasks.addTask(7, new EntityAILookIdle(this));
-		setSize(0.75F, 0.6F);
-		stepHeight = 0.0F;
+		setSize(0.5F, 0.5F);
+		stepHeight = 1.0F;
 	}
 
 	@Override
 	protected void entityInit() {
 		super.entityInit();
+		dataWatcher.addObject(18, (byte) 0);
 	}
 
 	@Override
-	public boolean isAIEnabled() {
+	protected boolean isAIEnabled() {
 		return true;
+	}
+
+	@Override
+	public void onUpdate() {
+		prevOnGround = onGround;
+		prevJumpticks = jumpticks;
+		super.onUpdate();
+			if(this.onGround) {
+				this.ticksOnGround++;
+				if(jumpticks > 0)
+					jumpticks = 0;
+			} else {
+				this.ticksOnGround = 0;
+				jumpticks ++;
+			}
+			//TODO: RIP TPS, 2016 - 2016
+			//Keeps trying to pathfind causing huge stress on TPS
+			if (!worldObj.isRemote) {
+				PathEntity path = getNavigator().getPath();
+				if (path != null && !path.isFinished() && onGround && !this.isMovementBlocked()) {
+					if (this.ticksOnGround > 20) {
+						int index = path.getCurrentPathIndex();
+						if (index < path.getCurrentPathLength()) {
+							PathPoint nextHopSpot = path.getPathPointFromIndex(index);
+							float x = (float) (nextHopSpot.xCoord - posX);
+							float z = (float) (nextHopSpot.zCoord - posZ);
+							float angle = (float) (Math.atan2(z, x));
+							float distance = (float) Math.sqrt(x * x + z * z);
+							if (distance > 1) {
+								motionY += 0.6;
+								motionX += 0.3 * MathHelper.cos(angle);
+								motionZ += 0.3 * MathHelper.sin(angle);
+							}/* else {
+								getNavigator().clearPathEntity();
+							}*/
+						}
+					}
+				}
+		}
+	}
+
+	@Override
+	public void onLivingUpdate() {
+		super.onLivingUpdate();
 	}
 
 	@Override
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
-		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.4D);
-		getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(5.0D);
-		getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(16.0D);
+		getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(3.0);
+		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.3);
 	}
 
 	@Override
-	public boolean getCanSpawnHere() {
-		return worldObj.getCollidingBoundingBoxes(this, boundingBox).isEmpty() && !worldObj.isAnyLiquid(boundingBox);
+	public void writeEntityToNBT(NBTTagCompound nbt) {
+		super.writeEntityToNBT(nbt);
+		// TODO Texture types
 	}
 
 	@Override
-	public int getMaxSpawnedInChunk() {
-		return 3;
+	public void readEntityFromNBT(NBTTagCompound nbt) {
+		super.readEntityFromNBT(nbt);
+		// TODO Texture types
+	}
+/*
+	protected String getJumpingSound() {
+		return null;
+
 	}
 
+	@Override
+	protected String getLivingSound() {
+		return null;
+
+	}
+
+	@Override
+	protected String getHurtSound() {
+		return null;
+
+	}
+
+	@Override
+	protected String getDeathSound() {
+		return null;
+	}
+*/
 	@Override
 	protected void dropFewItems(boolean recentlyHit, int looting) {
 		if (isBurning())
