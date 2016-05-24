edecde3402ff862fbaf825a6aff1e018c219aa19
imdutch21 <bartpklomp@gmail.com>
2016-05-24 21:22:52 +0200

    attempt on updating the Model3DTexture better

--- src/main/java/thebetweenlands/client/render/models/Model3DTexture.java
+++ src/main/java/thebetweenlands/client/render/models/Model3DTexture.java
@@ -2,6 +2,10 @@ package thebetweenlands.client.render.models;
 
 import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 public class Model3DTexture extends ModelBox {
     private int textureOffsetX;
@@ -31,30 +35,33 @@ public class Model3DTexture extends ModelBox {
         this(model, textureOffsetX, textureOffsetY, 0, 0, 0, width, height);
     }
 
-    /*@Override
+    //FIXME
+    @Override
     public void render(VertexBuffer renderer, float scale) {
-        GL11.glPushMatrix();
-        GL11.glTranslatef(posX1, posY1, posZ1);
-        GL11.glScalef(width / 16F, height / 16F, 1);
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(posX1, posY1, posZ1);
+        GlStateManager.scale(width / 16F, height / 16F, 1);
         float depth = 0.0625F;
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0.0F, 0.0F, 1.0F);
-        tessellator.addVertexWithUV(0.0D, 0.0D, 0.0D, u1, v2);
-        tessellator.addVertexWithUV(1.0D, 0.0D, 0.0D, u2, v2);
-        tessellator.addVertexWithUV(1.0D, 1.0D, 0.0D, u2, v1);
-        tessellator.addVertexWithUV(0.0D, 1.0D, 0.0D, u1, v1);
+        Tessellator tessellator = Tessellator.getInstance();
+
+        renderer.begin(7, DefaultVertexFormats.POSITION_TEX);
+        renderer.normal(0.0F, 0.0F, 1.0F);
+        renderer.pos(0.0D, 0.0D, 0.0D).tex(u1, v2).endVertex();
+        renderer.pos(1.0D, 0.0D, 0.0D).tex(u2, v2).endVertex();
+        renderer.pos(1.0D, 1.0D, 0.0D).tex(u2, v1).endVertex();
+        renderer.pos(0.0D, 1.0D, 0.0D).tex(u1, v1).endVertex();
         tessellator.draw();
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0.0F, 0.0F, -1.0F);
-        tessellator.addVertexWithUV(0.0D, 1.0D, (0.0F - depth), u1, v1);
-        tessellator.addVertexWithUV(1.0D, 1.0D, (0.0F - depth), u2, v1);
-        tessellator.addVertexWithUV(1.0D, 0.0D, (0.0F - depth), u2, v2);
-        tessellator.addVertexWithUV(0.0D, 0.0D, (0.0F - depth), u1, v2);
+        renderer.begin(7, DefaultVertexFormats.POSITION_TEX);
+        renderer.normal(0.0F, 0.0F, -1.0F);
+        renderer.pos(0.0D, 1.0D, (0.0F - depth)).tex(u1, v2).endVertex();
+        renderer.pos(1.0D, 1.0D, (0.0F - depth)).tex(u2, v2).endVertex();
+        renderer.pos(1.0D, 0.0D, (0.0F - depth)).tex(u2, v1).endVertex();
+        renderer.pos(0.0D, 0.0D, (0.0F - depth)).tex(u1, v1).endVertex();
         tessellator.draw();
         float f5 = 0.5F * (u1 - u2) / width;
         float f6 = 0.5F * (v2 - v1) / height;
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(-1.0F, 0.0F, 0.0F);
+        renderer.begin(7, DefaultVertexFormats.POSITION_TEX);
+        renderer.normal(-1.0F, 0.0F, 0.0F);
         int k;
         float f7;
         float f8;
@@ -62,55 +69,55 @@ public class Model3DTexture extends ModelBox {
         for (k = 0; k < width; k++) {
             f7 = (float) k / (float) width;
             f8 = u1 + (u2 - u1) * f7 - f5;
-            tessellator.addVertexWithUV(f7, 0.0D, (0.0F - depth), f8, v2);
-            tessellator.addVertexWithUV(f7, 0.0D, 0.0D, f8, v2);
-            tessellator.addVertexWithUV(f7, 1.0D, 0.0D, f8, v1);
-            tessellator.addVertexWithUV(f7, 1.0D, (0.0F - depth), f8, v1);
+            renderer.pos(f7, 0.0D, (0.0F - depth)).tex(f8, v2).endVertex();
+            renderer.pos(f7, 0.0D, 0.0D).tex(f8, v2).endVertex();
+            renderer.pos(f7, 1.0D, 0.0D).tex(f8, v1).endVertex();
+            renderer.pos(f7, 1.0D, (0.0F - depth)).tex(f8, v1).endVertex();
         }
 
         tessellator.draw();
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(1.0F, 0.0F, 0.0F);
+        renderer.begin(7, DefaultVertexFormats.POSITION_TEX);
+        renderer.normal(1.0F, 0.0F, 0.0F);
         float f9;
 
         for (k = 0; k < width; k++) {
             f7 = (float) k / (float) width;
             f8 = u1 + (u2 - u1) * f7 - f5;
             f9 = f7 + 1.0F / width;
-            tessellator.addVertexWithUV(f9, 1.0D, (0.0F - depth), f8, v1);
-            tessellator.addVertexWithUV(f9, 1.0D, 0.0D, f8, v1);
-            tessellator.addVertexWithUV(f9, 0.0D, 0.0D, f8, v2);
-            tessellator.addVertexWithUV(f9, 0.0D, (0.0F - depth), f8, v2);
+            renderer.pos(f9, 1.0D, (0.0F - depth)).tex(f8, v1).endVertex();
+            renderer.pos(f9, 1.0D, 0.0D).tex(f8, v1).endVertex();
+            renderer.pos(f9, 0.0D, 0.0D).tex(f8, v2).endVertex();
+            renderer.pos(f9, 0.0D, (0.0F - depth)).tex(f8, v2).endVertex();
         }
 
         tessellator.draw();
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0.0F, 1.0F, 0.0F);
+        renderer.begin(7, DefaultVertexFormats.POSITION_TEX);
+        renderer.normal(0.0F, 1.0F, 0.0F);
 
         for (k = 0; k < height; k++) {
             f7 = (float) k / (float) height;
             f8 = v2 + (v1 - v2) * f7 - f6;
             f9 = f7 + 1.0F / height;
-            tessellator.addVertexWithUV(0.0D, f9, 0.0D, u1, f8);
-            tessellator.addVertexWithUV(1.0D, f9, 0.0D, u2, f8);
-            tessellator.addVertexWithUV(1.0D, f9, (0.0F - depth), u2, f8);
-            tessellator.addVertexWithUV(0.0D, f9, (0.0F - depth), u1, f8);
+            renderer.pos(0.0D, f9, 0.0D).tex(u1, f8).endVertex();
+            renderer.pos(1.0D, f9, 0.0D).tex(u2, f8).endVertex();
+            renderer.pos(1.0D, f9, (0.0F - depth)).tex(u2, f8).endVertex();
+            renderer.pos(0.0D, f9, (0.0F - depth)).tex(u1, f8).endVertex();
         }
 
         tessellator.draw();
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0.0F, -1.0F, 0.0F);
+        renderer.begin(7, DefaultVertexFormats.POSITION_TEX);
+        renderer.normal(0.0F, -1.0F, 0.0F);
 
         for (k = 0; k < height; k++) {
             f7 = (float) k / (float) height;
             f8 = v2 + (v1 - v2) * f7 - f6;
-            tessellator.addVertexWithUV(1.0D, f7, 0.0D, u2, f8);
-            tessellator.addVertexWithUV(0.0D, f7, 0.0D, u1, f8);
-            tessellator.addVertexWithUV(0.0D, f7, (0.0F - depth), u1, f8);
-            tessellator.addVertexWithUV(1.0D, f7, (0.0F - depth), u2, f8);
+            renderer.pos(1.0D, f7, 0.0D).tex(u2, f8).endVertex();
+            renderer.pos(0.0D, f7, 0.0D).tex(u2, f8).endVertex();
+            renderer.pos(0.0D, f7, (0.0F - depth)).tex(u1, f8).endVertex();
+            renderer.pos(1.0D, f7, (0.0F - depth)).tex(u2, f8).endVertex();
         }
 
         tessellator.draw();
-        GL11.glPopMatrix();
-    }*/
+        GlStateManager.popMatrix();
+    }
 }
---
+++ src/main/java/thebetweenlands/common/item/misc/TestItem.java
@@ -0,0 +1,23 @@
+package thebetweenlands.common.item.misc;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+//MINE!!
+public class TestItem extends Item {
+    public TestItem() {
+        setUnlocalizedName("thebetweenlands.testItem");
+        this.setMaxStackSize(1);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return super.onItemUse(stack, playerIn, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+    }
+}
