a92ab81bbf863a9fc697f0be5b974a79ecac5f7a
dmf444 <becat444@gmail.com>
2016-05-30 17:51:03 -0400

    Fixes incorrect named files

---
+++ src/main/java/thebetweenlands/client/render/render/entity/render/RenderDragonFly.java
@@ -0,0 +1,33 @@
+package thebetweenlands.client.render.render.entity.render;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import thebetweenlands.client.render.models.entity.ModelDragonFly;
+import thebetweenlands.common.entity.mobs.EntityDragonFly;
+
+@SideOnly(Side.CLIENT)
+public class RenderDragonFly extends RenderLiving<EntityDragonFly> {
+    private final ResourceLocation texture = new ResourceLocation("thebetweenlands:textures/entity/dragon_fly.png");
+
+    public RenderDragonFly(RenderManager rendermanagerIn) {
+        super(rendermanagerIn, new ModelDragonFly(), 0.5F);
+    }
+
+    @Override
+    protected void preRenderCallback(EntityDragonFly entitylivingbaseIn, float partialTickTime) {
+        scaleDragonFly(entitylivingbaseIn, partialTickTime);
+    }
+
+    protected void scaleDragonFly(EntityDragonFly dragonFly, float partialTickTime) {
+        GlStateManager.scale(0.6F, 0.6F, 0.6F);
+    }
+
+    @Override
+    protected ResourceLocation getEntityTexture(EntityDragonFly entity) {
+        return texture;
+    }
+}
