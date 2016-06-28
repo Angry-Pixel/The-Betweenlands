12d7f7d16ee380ff04f8e5bf9acf2b22e490452a
vadis365 <curve_linear@hotmail.com>
2016-06-28 22:59:21 +0100

    err... ffs git

---
+++ src/main/java/thebetweenlands/common/block/BlockLeavesBetweenlands.java
@@ -0,0 +1,104 @@
+package thebetweenlands.common.block;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.BlockPlanks.EnumType;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import thebetweenlands.client.tab.BLCreativeTabs;
+
+public class BlockLeavesBetweenlands extends BlockLeaves {
+
+	public BlockLeavesBetweenlands() {
+		setHardness(0.2F);
+		setLightOpacity(1);
+		setCreativeTab(BLCreativeTabs.PLANTS);
+		setSoundType(SoundType.PLANT);
+		setDefaultState(blockState.getBaseState().withProperty(CHECK_DECAY, true).withProperty(DECAYABLE, true));
+	}
+
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubBlocks(Item item, CreativeTabs tab, List<ItemStack> list) {
+		list.add(new ItemStack(item));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+		return !Minecraft.getMinecraft().gameSettings.fancyGraphics && blockAccess.getBlockState(pos.offset(side)).getBlock() == this ? false : true;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return !Minecraft.getMinecraft().gameSettings.fancyGraphics;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return Blocks.LEAVES.getBlockLayer();
+	}
+
+	@Override
+	public ArrayList<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune) {
+		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+		ret.add(new ItemStack(this));
+		return ret;
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(DECAYABLE, Boolean.valueOf((meta & 4) == 0)).withProperty(CHECK_DECAY, Boolean.valueOf((meta & 8) > 0));
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		int i = 0;
+
+		if (!state.getValue(DECAYABLE).booleanValue())
+			i |= 4;
+		if (state.getValue(CHECK_DECAY).booleanValue())
+			i |= 8;
+
+		return i;
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] { CHECK_DECAY, DECAYABLE });
+	}
+
+	@Override
+	public EnumType getWoodType(int meta) {
+		return EnumType.OAK;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+		if(world.rand.nextInt(200) == 0) {
+			if(world.isAirBlock(pos.down())) {
+			//	BLParticle.LEAF.spawn(world, pos.add(rand.nextFloat(), 0, rand.nextFloat()));
+			}
+		}
+	}
+}
