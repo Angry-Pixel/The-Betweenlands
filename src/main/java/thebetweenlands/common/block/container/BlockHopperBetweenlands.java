36dc854f0d293020324654919f9fd211dd7fa020
imdutch21 <bartpklomp@gmail.com>
2016-09-21 19:30:13 +0200

    fixed errors

--- src/main/java/thebetweenlands/common/block/container/BlockHopperBetweenlands.java
+++ src/main/java/thebetweenlands/common/block/container/BlockHopperBetweenlands.java
@@ -3,10 +3,17 @@ package thebetweenlands.common.block.container;
 import net.minecraft.block.BlockHopper;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import thebetweenlands.common.registries.BlockRegistry;
 import thebetweenlands.common.tile.TileEntityHopperBetweenlands;
+import thebetweenlands.util.AdvancedStateMap;
 
-public class BlockHopperBetweenlands extends BlockHopper {
+public class BlockHopperBetweenlands extends BlockHopper implements BlockRegistry.IStateMappedBlock {
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileEntityHopperBetweenlands();
+    }
+
+    @Override
+    public void setStateMapper(AdvancedStateMap.Builder builder) {
+        builder.ignore(ENABLED);
     }
 }
