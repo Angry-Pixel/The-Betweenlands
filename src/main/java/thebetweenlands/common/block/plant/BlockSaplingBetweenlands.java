7e3e84d464c8a7bd93e095fadd5d8cb51a686b61
TheCyberBrick <samuelboerlin@bluewin.ch>
2017-01-28 17:40:32 +0100

    Fixed Saplings breaking

--- src/main/java/thebetweenlands/common/block/plant/BlockSaplingBetweenlands.java
+++ src/main/java/thebetweenlands/common/block/plant/BlockSaplingBetweenlands.java
@@ -3,14 +3,12 @@ package thebetweenlands.common.block.plant;
 import java.util.List;
 import java.util.Random;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockSapling;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -26,7 +24,6 @@ import thebetweenlands.common.world.gen.feature.tree.WorldGenWeedwoodTree;
 import thebetweenlands.util.AdvancedStateMap;
 
 public class BlockSaplingBetweenlands extends BlockSapling implements IStateMappedBlock  {
-
 	private String TREE_TYPE;
 
 	public BlockSaplingBetweenlands(String type) {
@@ -42,68 +39,52 @@ public class BlockSaplingBetweenlands extends BlockSapling implements IStateMapp
 	}
 
 	@Override
-    public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
-        if (!world.isRemote) {
-            super.updateTick(world, pos, state, rand);
-            if (world.getLightFromNeighbors(pos.up()) >= 9 && rand.nextInt(7) == 0) {
-            		grow(world, pos, state, rand);
-            }
-        }
-    }
+	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+		if (!world.isRemote) {
+			this.checkAndDropBlock(world, pos, state);
 
-	@Override
-	public boolean canPlaceBlockAt(World world, BlockPos pos) {
-		IBlockState state = world.getBlockState(pos.down());
-		Block block = state.getBlock();
-		if (block.canSustainPlant(state, world, pos.down(), EnumFacing.UP, this))
-			return true;
-		else if (block == BlockRegistry.SWAMP_DIRT || block == BlockRegistry.SWAMP_GRASS || block == BlockRegistry.DEAD_GRASS)
-			return true;
-		return false;
-	}
-
-	@Override
-	public void neighborChanged(IBlockState state, World world, BlockPos pos, Block blockIn) {
-		checkForDrop(world, pos, state);
-	}
-
-	protected final boolean checkForDrop(World world, BlockPos pos, IBlockState state) {
-		if (canPlaceBlockAt(world, pos))
-			return true;
-		else {
-			dropBlockAsItem(world, pos, state, 0);
-			world.setBlockToAir(pos);
-			return false;
+			if (rand.nextInt(7) == 0) {
+				this.grow(world, pos, state, rand);
+			}
 		}
+	}
+
+	@Override
+	protected boolean canSustainBush(IBlockState state) {
+		return state.getBlock() == BlockRegistry.SWAMP_DIRT || state.getBlock() == BlockRegistry.SWAMP_GRASS || state.getBlock() == BlockRegistry.DEAD_GRASS || super.canSustainBush(state);
 	}
 
 	@Override
 	public void generateTree(World world, BlockPos pos, IBlockState state, Random rand) {
-		if (!TerrainGen.saplingGrowTree(world, rand, pos))
+		if (!TerrainGen.saplingGrowTree(world, rand, pos)) {
 			return;
+		}
 
 		WorldGenerator worldGen = null;
 
 		switch (TREE_TYPE) {
-			case "WEEDWOOD":
-				worldGen = new WorldGenWeedwoodTree();
-				break;
-			case "SAP":
-				worldGen = new WorldGenSapTree();
-				break;
-			case "RUBBER":
-				worldGen = new WorldGenRubberTree();
-				break; 
-			default:
-				break;
+		case "WEEDWOOD":
+			worldGen = new WorldGenWeedwoodTree();
+			break;
+		case "SAP":
+			worldGen = new WorldGenSapTree();
+			break;
+		case "RUBBER":
+			worldGen = new WorldGenRubberTree();
+			break; 
+		default:
+			break;
 		}
 
-		if (worldGen == null)
+		if (worldGen == null) {
 			return;
+		}
 
 		world.setBlockToAir(pos);
-		if (!worldGen.generate(world, rand, pos))
+
+		if (!worldGen.generate(world, rand, pos)) {
 			world.setBlockState(pos, (IBlockState) this);
+		}
 	}
 
 	@Override
@@ -111,5 +92,4 @@ public class BlockSaplingBetweenlands extends BlockSapling implements IStateMapp
 	public void setStateMapper(AdvancedStateMap.Builder builder) {
 		builder.ignore(TYPE);
 	}
-
 }
