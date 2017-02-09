5f9d99e530013488fd11c9196cb94bc3e5485c28
TheCyberBrick <samuelboerlin@bluewin.ch>
2017-02-09 01:30:16 +0100

    Updated gradle build script

--- src/main/java/thebetweenlands/common/lib/ModInfo.java
+++ src/main/java/thebetweenlands/common/lib/ModInfo.java
@@ -5,7 +5,7 @@ public class ModInfo {
 	public static final String NAME_PREFIX = ID + ".";
 	public static final String ASSETS_PREFIX = ID + ":";
 	public static final String NAME = "The Betweenlands";
-	public static final String VERSION = "1.0.0-alpha";
+	public static final String VERSION = "2.0.0-alpha";
 	public static final String CHANNEL = ID;
 	public static final String CLIENTPROXY_LOCATION = "thebetweenlands.client.proxy.ClientProxy";
 	public static final String COMMONPROXY_LOCATION = "thebetweenlands.common.proxy.CommonProxy";
