e80b39004666c6513154728ee75e7b8beddf2b52
TheCyberBrick <samuelboerlin@bluewin.ch>
2017-03-18 13:36:58 +0100

    Fixed inventory of chests resetting when creating double chest, closes #377

--- src/main/java/thebetweenlands/common/tile/TileEntityChestBetweenlands.java
+++ src/main/java/thebetweenlands/common/tile/TileEntityChestBetweenlands.java
@@ -1,7 +1,14 @@
 package thebetweenlands.common.tile;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class TileEntityChestBetweenlands extends TileEntityChest {
-	//Nothing to change here
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
+		//Use vanilla behaviour to prevent inventory from resetting when creating double chest
+		return oldState.getBlock() != newSate.getBlock();
+	}
 }
