b032920ef0c0abf4f02b09710d56b2e55aec8f93
TheCyberBrick <samuelboerlin@bluewin.ch>
2017-11-21 22:41:53 +0100

    Fixed wisp texture artifacts

--- src/main/java/thebetweenlands/client/render/particle/entity/ParticleWisp.java
+++ src/main/java/thebetweenlands/client/render/particle/entity/ParticleWisp.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import thebetweenlands.client.render.particle.ParticleFactory;
 import thebetweenlands.client.render.particle.ParticleTextureStitcher;
@@ -43,7 +44,7 @@ public class ParticleWisp extends Particle implements IParticleSpriteReceiver {
 	}
 
 	@Override
-	public void renderParticle(BufferBuilder buff, Entity entityIn, float partialTicks, float rx, float rz, float ryz, float rxy, float rxz) {
+	public void renderParticle(BufferBuilder buff, Entity entityIn, float partialTicks, float rotationX, float rotationZ, float rotationYZ, float rotationXY, float rotationXZ) {
 		float currentX = (float) (prevPosX + (posX - prevPosX) * partialTicks);
 		float currentY = (float) (prevPosY + (posY - prevPosY) * partialTicks);
 		float currentZ = (float) (prevPosZ + (posZ - prevPosZ) * partialTicks);
@@ -53,7 +54,50 @@ public class ParticleWisp extends Particle implements IParticleSpriteReceiver {
 			distance = MathHelper.clamp(getDistanceToViewer(currentX, currentY, currentZ, partialTicks), 10, 20);
 		}
 		this.setAlphaF(1.0F - MathHelper.sin(MathUtils.PI / 20 * distance));
-		super.renderParticle(buff, entityIn, partialTicks, rx, rz, ryz, rxy, rxz);
+
+		float scale = 0.1F * this.particleScale;
+
+		float minU = this.particleTexture.getMinU();
+		float maxU = this.particleTexture.getMaxU();
+		float minV = this.particleTexture.getMinV();
+		float maxV = this.particleTexture.getMaxV();
+		
+		//remove 1px border to avoid artifacts from smooth filtering
+		float borderU = (maxU - minU) / this.particleTexture.getIconWidth();
+		float borderV = (maxV - minV) / this.particleTexture.getIconHeight();
+
+		minU += borderU;
+		maxU -= borderU;
+		minV += borderV;
+		maxV -= borderV;
+		
+		float f5 = (float)(this.prevPosX + (this.posX - this.prevPosX) * (double)partialTicks - interpPosX);
+		float f6 = (float)(this.prevPosY + (this.posY - this.prevPosY) * (double)partialTicks - interpPosY);
+		float f7 = (float)(this.prevPosZ + (this.posZ - this.prevPosZ) * (double)partialTicks - interpPosZ);
+		int i = this.getBrightnessForRender(partialTicks);
+		int j = i >> 16 & 65535;
+		int k = i & 65535;
+		Vec3d[] avec3d = new Vec3d[] {new Vec3d((double)(-rotationX * scale - rotationXY * scale), (double)(-rotationZ * scale), (double)(-rotationYZ * scale - rotationXZ * scale)), new Vec3d((double)(-rotationX * scale + rotationXY * scale), (double)(rotationZ * scale), (double)(-rotationYZ * scale + rotationXZ * scale)), new Vec3d((double)(rotationX * scale + rotationXY * scale), (double)(rotationZ * scale), (double)(rotationYZ * scale + rotationXZ * scale)), new Vec3d((double)(rotationX * scale - rotationXY * scale), (double)(-rotationZ * scale), (double)(rotationYZ * scale - rotationXZ * scale))};
+
+		if (this.particleAngle != 0.0F)
+		{
+			float f8 = this.particleAngle + (this.particleAngle - this.prevParticleAngle) * partialTicks;
+			float f9 = MathHelper.cos(f8 * 0.5F);
+			float f10 = MathHelper.sin(f8 * 0.5F) * (float)cameraViewDir.x;
+			float f11 = MathHelper.sin(f8 * 0.5F) * (float)cameraViewDir.y;
+			float f12 = MathHelper.sin(f8 * 0.5F) * (float)cameraViewDir.z;
+			Vec3d vec3d = new Vec3d((double)f10, (double)f11, (double)f12);
+
+			for (int l = 0; l < 4; ++l)
+			{
+				avec3d[l] = vec3d.scale(2.0D * avec3d[l].dotProduct(vec3d)).add(avec3d[l].scale((double)(f9 * f9) - vec3d.dotProduct(vec3d))).add(vec3d.crossProduct(avec3d[l]).scale((double)(2.0F * f9)));
+			}
+		}
+
+		buff.pos((double)f5 + avec3d[0].x, (double)f6 + avec3d[0].y, (double)f7 + avec3d[0].z).tex((double)maxU, (double)maxV).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
+		buff.pos((double)f5 + avec3d[1].x, (double)f6 + avec3d[1].y, (double)f7 + avec3d[1].z).tex((double)maxU, (double)minV).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
+		buff.pos((double)f5 + avec3d[2].x, (double)f6 + avec3d[2].y, (double)f7 + avec3d[2].z).tex((double)minU, (double)minV).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
+		buff.pos((double)f5 + avec3d[3].x, (double)f6 + avec3d[3].y, (double)f7 + avec3d[3].z).tex((double)minU, (double)maxV).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j, k).endVertex();
 	}
 
 	@Override
