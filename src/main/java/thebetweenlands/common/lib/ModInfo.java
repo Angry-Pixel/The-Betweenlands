89548534c5eaf6695fc19c7e1943e4d4155e0e06
TheCyberBrick <samuelboerlin@bluewin.ch>
2017-12-14 21:56:50 +0100

    Updated version number

--- src/main/java/thebetweenlands/common/lib/ModInfo.java
+++ src/main/java/thebetweenlands/common/lib/ModInfo.java
@@ -12,6 +12,6 @@ public class ModInfo {
 	public static final String API_NAME = "BetweenlandsAPI";
 
 	public static final String MC_VERSIONS = "1.12.1";
-	public static final String VERSION = "3.1.0-alpha-snapshot";
+	public static final String VERSION = "3.2.0-alpha-snapshot";
 	public static final String API_VERSION = "1.7.0";
 }
