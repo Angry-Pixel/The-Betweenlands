a7d17ef07bdb6eb7bc04f186f312fbd22ca14145
TheCyberBrick <samuelboerlin@bluewin.ch>
2018-02-02 16:47:43 +0100

    Removed unused keybindings

--- src/main/java/thebetweenlands/common/registries/KeyBindRegistry.java
+++ src/main/java/thebetweenlands/common/registries/KeyBindRegistry.java
@@ -8,16 +8,16 @@ public class KeyBindRegistry {
 	public static final KeyBindingBL RADIAL_MENU = new KeyBindingBL("key.radial_menu", Keyboard.KEY_C, "key.categories.betweenlands");
 	public static final KeyBindingBL USE_RING = new KeyBindingBL("key.use_ring", Keyboard.KEY_R, "key.categories.betweenlands");
 	public static final KeyBindingBL OPEN_POUCH = new KeyBindingBL("key.open_pouch", Keyboard.KEY_G, "key.categories.betweenlands");
-	public static final KeyBindingBL SEND_SCOUT = new KeyBindingBL("key.send_scout", Keyboard.KEY_K, "key.categories.betweenlands");
-	public static final KeyBindingBL RETURN_SCOUT = new KeyBindingBL("key.return_scout", Keyboard.KEY_L, "key.categories.betweenlands");
+	//public static final KeyBindingBL SEND_SCOUT = new KeyBindingBL("key.send_scout", Keyboard.KEY_K, "key.categories.betweenlands");
+	//public static final KeyBindingBL RETURN_SCOUT = new KeyBindingBL("key.return_scout", Keyboard.KEY_L, "key.categories.betweenlands");
 	public static final KeyBindingBL CONNECT_CAVING_ROPE = new KeyBindingBL("key.connect_caving_rope", Keyboard.KEY_V, "key.categories.betweenlands");
 	
 	public static void init() {
 		RADIAL_MENU.register();
 		USE_RING.register();
 		OPEN_POUCH.register();
-		SEND_SCOUT.register();
-		RETURN_SCOUT.register();
+		//SEND_SCOUT.register();
+		//RETURN_SCOUT.register();
 		CONNECT_CAVING_ROPE.register();
 	}
 }
