c4e6128ee2baaa2c5e545f52aa5a42aff8440892
imdutch21 <bartpklomp@gmail.com>
2018-02-28 21:17:03 +0100

    Smoll sludge renders now! Not sure why I did not have this....

--- src/main/java/thebetweenlands/client/render/entity/RenderSmollSludge.java
+++ src/main/java/thebetweenlands/client/render/entity/RenderSmollSludge.java
@@ -7,7 +7,9 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.GL11;
 import thebetweenlands.client.render.model.entity.ModelSludge;
+import thebetweenlands.client.render.model.entity.ModelSludgeBall;
 import thebetweenlands.client.render.model.entity.ModelSmollSludge;
 import thebetweenlands.common.entity.mobs.EntitySludge;
 import thebetweenlands.common.entity.mobs.EntitySmollSludge;
@@ -22,7 +24,29 @@ public class RenderSmollSludge extends RenderLiving<EntitySmollSludge> {
 
 	@Override
 	public void doRender(EntitySmollSludge entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		GlStateManager.pushMatrix();
+		float squishFactor = entity.getSquishFactor(partialTicks) / 1.5F;
+		float scale = 1.0F / (squishFactor + 1.0F);
 
+		GlStateManager.translate(x, y - (1.0F - entity.scale.getAnimationProgressSin(partialTicks)) * 2.5F, z);
+		GlStateManager.scale(scale, 1.0F / scale, scale);
+
+		GlStateManager.depthMask(false);
+
+		GlStateManager.cullFace(CullFace.FRONT);
+		super.doRender(entity, 0, 0, 0, entityYaw, partialTicks);
+
+		GlStateManager.cullFace(CullFace.BACK);
+		super.doRender(entity, 0, 0, 0, entityYaw, partialTicks);
+
+		GlStateManager.depthMask(true);
+		GlStateManager.colorMask(false, false, false, false);
+
+		super.doRender(entity, 0, 0, 0, entityYaw, partialTicks);
+
+		GlStateManager.colorMask(true, true, true, true);
+
+		GlStateManager.popMatrix();
 	}
 
 	@Override
