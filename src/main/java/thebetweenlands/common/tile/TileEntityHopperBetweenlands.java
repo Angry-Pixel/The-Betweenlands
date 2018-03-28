bbb1b3ba5335ff43a37a5132069b7ae45d87a113
TheCyberBrick <samuelboerlin@bluewin.ch>
2018-03-28 18:59:29 +0200

    Fixed syrmorite hoppers losing inventory content when changing state, closes #549

--- src/main/java/thebetweenlands/common/tile/TileEntityHopperBetweenlands.java
+++ src/main/java/thebetweenlands/common/tile/TileEntityHopperBetweenlands.java
@@ -1,9 +1,16 @@
 package thebetweenlands.common.tile;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntityHopper;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class TileEntityHopperBetweenlands extends TileEntityHopper {
-
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
+		return oldState.getBlock() != newSate.getBlock(); //Urgh why is this even a thing
+	}
+	
     @Override
     public String getName() {
         return this.hasCustomName() ? this.customName : "tile.thebetweenlands.syrmorite_hopper.name";
