33d5f1939165d292815701a9fdb20f130b3aa69b
TheCyberBrick <samuelboerlin@bluewin.ch>
2018-03-31 22:40:27 +0200

    Sneaky import c:

--- src/main/java/thebetweenlands/common/handler/OverworldItemHandler.java
+++ src/main/java/thebetweenlands/common/handler/OverworldItemHandler.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
-import com.mojang.realmsclient.dto.PlayerInfo;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockTorch;
 import net.minecraft.block.state.IBlockState;
