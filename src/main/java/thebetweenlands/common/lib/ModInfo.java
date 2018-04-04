577ecb059d02407993591e68ec86155a012cc3f9
TheCyberBrick <samuelboerlin@bluewin.ch>
2018-04-04 23:08:25 +0200

    Updated version number

--- src/main/java/thebetweenlands/common/lib/ModInfo.java
+++ src/main/java/thebetweenlands/common/lib/ModInfo.java
@@ -12,7 +12,7 @@ public class ModInfo {
 
 	public static final String DEPENDENCIES = "required-after:forge@[14.23.1.2602,)";
 	public static final String MC_VERSIONS = "[1.12.2]";
-	public static final String VERSION = "3.3.5";
+	public static final String VERSION = "3.3.6";
 	public static final String API_VERSION = "1.10.0";
 	public static final String CONFIG_VERSION = "1.0.0"; //Increment and add updater if properties are moved/renamed or removed
 }
