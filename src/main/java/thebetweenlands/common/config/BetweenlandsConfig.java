03eead589f45fb7656b2313cc97f9254c1edcda2
TheCyberBrick <samuelboerlin@bluewin.ch>
2018-09-11 20:03:31 +0200

    Added some more config options and gamerules

--- src/main/java/thebetweenlands/common/config/BetweenlandsConfig.java
+++ src/main/java/thebetweenlands/common/config/BetweenlandsConfig.java
@@ -187,6 +187,41 @@ public class BetweenlandsConfig {
 		@Comment("Whether food from the overworld should rot when going into the dimension")
 		public boolean useRottenFood = true;
 
+		@Name("use_decay")
+		@LangKey(LANG_PREFIX + "use_decay")
+		@Comment("Whether the player decay mechanics should be active in the dimension")
+		public boolean useDecay = true;
+		
+		@Name("use_corrosion")
+		@LangKey(LANG_PREFIX + "use_corrosion")
+		@Comment("Whether the tool corrosion mechanics should be active in the dimension")
+		public boolean useCorrosion = true;
+		
+		@Name("use_tool_weakness")
+		@LangKey(LANG_PREFIX + "use_tool_weakness")
+		@Comment("Whether overworld tools should be weakened in the dimension")
+		public boolean useToolWeakness = true;
+		
+		@Name("use_torch_blacklist")
+		@LangKey(LANG_PREFIX + "use_torch_blacklist")
+		@Comment("Whether torches should turn into damp torches in the dimension")
+		public boolean useTorchBlacklist = true;
+		
+		@Name("use_fire_tool_blacklist")
+		@LangKey(LANG_PREFIX + "use_fire_tool_blacklist")
+		@Comment("Whether fire tools (e.g. Flint and Steel) should be blacklisted from working in the dimension")
+		public boolean useFireToolBlacklist = true;
+		
+		@Name("use_potion_blacklist")
+		@LangKey(LANG_PREFIX + "use_potion_blacklist")
+		@Comment("Whether potions should turn into tainted potions in the dimension")
+		public boolean usePotionBlacklist = true;
+		
+		@Name("use_fertilizer_blacklist")
+		@LangKey(LANG_PREFIX + "use_fertilizer_blacklist")
+		@Comment("Whether fertilizers (e.g. Bonemeal) should be blacklisted from working in the dimension")
+		public boolean useFertilizerBlacklist = true;
+		
 		@Name("caving_rope_indicator")
 		@LangKey(LANG_PREFIX + "caving_rope_indicator")
 		@Comment("Adds an indicator next to the crosshair that shows whether the player is connected to the caving rope and how much rope is left")
--- src/main/java/thebetweenlands/common/registries/GameruleRegistry.java
+++ src/main/java/thebetweenlands/common/registries/GameruleRegistry.java
@@ -6,38 +6,51 @@ import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 public class GameruleRegistry {
+	public static final GameruleRegistry INSTANCE = new GameruleRegistry();
 
-    public static GameruleRegistry INSTANCE = new GameruleRegistry();
+	public static final String BL_FOOD_SICKNESS = "blFoodSickness";
+	public static final String BL_ROTTEN_FOOD = "blRottenFood";
+	public static final String BL_DECAY = "blDecay";
+	public static final String BL_CORROSION = "blCorrosion";
+	public static final String BL_TOOL_WEAKNESS = "blToolWeakness";
+	public static final String BL_TORCH_BLACKLIST = "blTorchBlacklist";
+	public static final String BL_FIRE_TOOL_BLACKLIST = "blFireToolBlacklist";
+	public static final String BL_POTION_BLACKLIST = "blPotionBlacklist";
+	public static final String BL_FERTILIZER_BLACKLIST = "blFertilizerBlacklist";
 
-    public static final String BL_DECAY = "blDecay";
-    public static final String BL_CORROSION = "blCorrosion";
+	public void onServerStarting(FMLServerStartingEvent event) {
+		this.createGamerule(BL_FOOD_SICKNESS, "true", GameRules.ValueType.BOOLEAN_VALUE);
+		this.createGamerule(BL_ROTTEN_FOOD, "true", GameRules.ValueType.BOOLEAN_VALUE);
+		this.createGamerule(BL_DECAY, "true", GameRules.ValueType.BOOLEAN_VALUE);
+		this.createGamerule(BL_CORROSION, "true", GameRules.ValueType.BOOLEAN_VALUE);
+		this.createGamerule(BL_TOOL_WEAKNESS, "true", GameRules.ValueType.BOOLEAN_VALUE);
+		this.createGamerule(BL_TORCH_BLACKLIST, "true", GameRules.ValueType.BOOLEAN_VALUE);
+		this.createGamerule(BL_FIRE_TOOL_BLACKLIST, "true", GameRules.ValueType.BOOLEAN_VALUE);
+		this.createGamerule(BL_POTION_BLACKLIST, "true", GameRules.ValueType.BOOLEAN_VALUE);
+		this.createGamerule(BL_FERTILIZER_BLACKLIST, "true", GameRules.ValueType.BOOLEAN_VALUE);
+	}
 
-    public void onServerStarting(FMLServerStartingEvent event) {
-        this.createGamerule(BL_DECAY, "true", GameRules.ValueType.BOOLEAN_VALUE);
-        this.createGamerule(BL_CORROSION, "true", GameRules.ValueType.BOOLEAN_VALUE);
-    }
+	private void createGamerule(String name, String value, GameRules.ValueType type) {
+		GameRules gameRules = DimensionManager.getWorld(0).getGameRules();
+		if(!gameRules.hasRule(name)) {
+			gameRules.addGameRule(name, value, type);
+		}
+	}
 
-    private void createGamerule(String name, String value, GameRules.ValueType type) {
-        GameRules gameRules = DimensionManager.getWorld(0).getGameRules();
-        if(!gameRules.hasRule(name)) {
-            gameRules.addGameRule(name, value, type);
-        }
-    }
+	public static GameRules getGameRules() {
+		WorldServer world = DimensionManager.getWorld(0);
+		if(world != null) {
+			return world.getGameRules();
+		}
+		return null;
+	}
 
-    public static GameRules getGameRules() {
-        WorldServer world = DimensionManager.getWorld(0);
-        if(world != null) {
-            return world.getGameRules();
-        }
-        return null;
-    }
+	public static boolean getGameRuleBooleanValue(String name) {
+		return getGameRules() != null && getGameRules().getBoolean(name);
+	}
 
-    public static boolean getGameRuleBooleanValue(String name) {
-        return getGameRules() != null && getGameRules().getBoolean(name);
-    }
-
-    public static String getGameRuleStringValue(String name) {
-        return getGameRules() != null ? getGameRules().getString(name) : "";
-    }
+	public static String getGameRuleStringValue(String name) {
+		return getGameRules() != null ? getGameRules().getString(name) : "";
+	}
 
 }
