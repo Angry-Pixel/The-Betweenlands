56dec126e79cc76131f63d2768f340cccdf47ce1
GoryMoon <gurreja@gmail.com>
2018-09-14 01:27:15 +0200

    Added name to mummy arms, showing name with other mods Removed unneeded class extension

--- src/main/java/thebetweenlands/client/gui/menu/GuiDownloadTerrainBetweenlands.java
+++ src/main/java/thebetweenlands/client/gui/menu/GuiDownloadTerrainBetweenlands.java
@@ -2,24 +2,25 @@ package thebetweenlands.client.gui.menu;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiDownloadTerrain;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import thebetweenlands.common.config.BetweenlandsConfig;
 import thebetweenlands.common.lib.ModInfo;
 
-public class GuiDownloadTerrainBetweenlands extends GuiDownloadTerrain {
+public class GuiDownloadTerrainBetweenlands extends GuiScreen {
 	private static int prevDimension;
 
 	private GuiMainMenuBackground background = new GuiMainMenuBackground(new ResourceLocation(ModInfo.ID, "textures/gui/main/layer"), 4);
 
 	@SubscribeEvent
 	public static void onGuiOpened(GuiOpenEvent event) {
-		if (event.getGui() instanceof GuiDownloadTerrain && 
-				!(event.getGui() instanceof GuiDownloadTerrainBetweenlands) &&
-				Minecraft.getMinecraft().player != null && 
-				(Minecraft.getMinecraft().player.dimension == BetweenlandsConfig.WORLD_AND_DIMENSION.dimensionId || prevDimension == BetweenlandsConfig.WORLD_AND_DIMENSION.dimensionId)) {
+		Minecraft client = FMLClientHandler.instance().getClient();
+		if (event.getGui() instanceof GuiDownloadTerrain && client.player != null &&
+				(client.player.dimension == BetweenlandsConfig.WORLD_AND_DIMENSION.dimensionId || prevDimension == BetweenlandsConfig.WORLD_AND_DIMENSION.dimensionId)) {
 			event.setGui(new GuiDownloadTerrainBetweenlands());
 			prevDimension = Minecraft.getMinecraft().player.dimension;
 		}
@@ -46,8 +47,14 @@ public class GuiDownloadTerrainBetweenlands extends GuiDownloadTerrain {
 	}
 
 	@Override
+	public boolean doesGuiPauseGame() {
+		return false;
+	}
+
+	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		this.background.drawScreen(mouseX, mouseY, partialTicks);
 		this.drawCenteredString(this.fontRenderer, I18n.format("multiplayer.downloadingTerrain"), this.width / 2, this.height / 2 - 50, 16777215);
+		super.drawScreen(mouseX, mouseY, partialTicks);
 	}
 }
