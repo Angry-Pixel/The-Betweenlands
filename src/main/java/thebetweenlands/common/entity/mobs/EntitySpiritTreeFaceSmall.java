6e9d30464497e57e010bb0b808fe225f3ed50c51
TheCyberBrick <samuelboerlin@bluewin.ch>
2018-09-18 18:31:11 +0200

    Removed variant datawatcher of small face

--- src/main/java/thebetweenlands/common/entity/mobs/EntitySpiritTreeFaceSmall.java
+++ src/main/java/thebetweenlands/common/entity/mobs/EntitySpiritTreeFaceSmall.java
@@ -2,22 +2,21 @@ package thebetweenlands.common.entity.mobs;
 
 import java.util.List;
 
+import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
-import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
 import thebetweenlands.common.registries.LootTableRegistry;
 import thebetweenlands.common.world.storage.BetweenlandsWorldStorage;
 import thebetweenlands.common.world.storage.location.LocationSpiritTree;
 
-public class EntitySpiritTreeFaceSmall extends EntitySpiritTreeFace {
-	private static final DataParameter<Integer> VARIANT = EntityDataManager.createKey(EntitySpiritTreeFaceSmall.class, DataSerializers.VARINT);
+public class EntitySpiritTreeFaceSmall extends EntitySpiritTreeFace implements IEntityAdditionalSpawnData {
+	private int variant;
 
 	public EntitySpiritTreeFaceSmall(World world) {
 		super(world);
@@ -35,12 +34,6 @@ public class EntitySpiritTreeFaceSmall extends EntitySpiritTreeFace {
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(2.0D);
 		this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.0D);
-	}
-
-	@Override
-	protected void entityInit() {
-		super.entityInit();
-		this.dataManager.register(VARIANT, 0);
 	}
 
 	@Override
@@ -79,10 +72,20 @@ public class EntitySpiritTreeFaceSmall extends EntitySpiritTreeFace {
 	}
 
 	public void setVariant(int variant) {
-		this.dataManager.set(VARIANT, variant);
+		this.variant = variant;
 	}
 
 	public int getVariant() {
-		return this.dataManager.get(VARIANT);
+		return this.variant;
+	}
+
+	@Override
+	public void writeSpawnData(ByteBuf buffer) {
+		buffer.writeInt(this.variant);
+	}
+
+	@Override
+	public void readSpawnData(ByteBuf additionalData) {
+		this.variant = additionalData.readInt();
 	}
 }
