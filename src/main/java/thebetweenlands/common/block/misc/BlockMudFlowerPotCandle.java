3c801f63f46f488ba2de25691c59f088594c781b
TheCyberBrick <samuelboerlin@bluewin.ch>
2018-10-18 10:51:19 +0200

    Fixed mud pot candle dropping wrong item

--- src/main/java/thebetweenlands/common/block/misc/BlockMudFlowerPotCandle.java
+++ src/main/java/thebetweenlands/common/block/misc/BlockMudFlowerPotCandle.java
@@ -8,6 +8,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
@@ -80,6 +81,11 @@ public class BlockMudFlowerPotCandle extends Block {
 		return BlockRenderLayer.CUTOUT;
 	}
 
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return Item.getItemFromBlock(BlockRegistry.MUD_FLOWER_POT);
+	}
+	
 	@Override
 	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
 		super.getDrops(drops, world, pos, state, fortune);
