5a9a41a5e65e5984fba80d2ed4b541fa84e87783
TheCyberBrick <samuelboerlin@bluewin.ch>
2018-10-18 17:31:51 +0200

    Added unlit mud flower pot candle

--- src/main/java/thebetweenlands/common/block/misc/BlockMudFlowerPotCandle.java
+++ src/main/java/thebetweenlands/common/block/misc/BlockMudFlowerPotCandle.java
@@ -5,13 +5,18 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.SoundCategory;
@@ -27,15 +32,38 @@ import thebetweenlands.common.registries.BlockRegistry;
 public class BlockMudFlowerPotCandle extends Block {
 	protected static final AxisAlignedBB CANDLE_AABB = new AxisAlignedBB(0.4D, 0.0D, 0.4D, 0.6D, 0.8D, 0.6D);
 
+	//Wow! So lit
+	public static final PropertyBool LIT = PropertyBool.create("lit");
+
 	public BlockMudFlowerPotCandle() {
 		super(Material.CIRCUITS);
 		this.setCreativeTab(BLCreativeTabs.BLOCKS);
 		this.setHardness(0.3F);
 		this.setSoundType(SoundType.STONE);
-		this.setLightLevel(0.85F);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(LIT, true));
 		this.useNeighborBrightness = true;
 	}
 
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] { LIT });
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(LIT, meta != 0);
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(LIT) ? 1 : 0;
+	}
+	
+	@Override
+	public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+		return this.getDefaultState().withProperty(LIT, true);
+	}
+	
 	@Override
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
 		return CANDLE_AABB;
@@ -55,11 +83,17 @@ public class BlockMudFlowerPotCandle extends Block {
 	public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
 		if(!worldIn.isRemote) {
 			worldIn.setBlockState(pos, BlockRegistry.MUD_FLOWER_POT.getDefaultState());
-			
+
 			worldIn.playSound(null, pos, SoundType.WOOD.getBreakSound(), SoundCategory.BLOCKS, (SoundType.WOOD.getVolume() + 1.0F) / 2.0F, SoundType.WOOD.getPitch() * 0.8F);
-			
+
 			spawnAsEntity(worldIn, pos, new ItemStack(BlockRegistry.SULFUR_TORCH));
 		}
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		worldIn.setBlockState(pos, state.cycleProperty(LIT));
+		return true;
 	}
 
 	@Override
@@ -85,7 +119,7 @@ public class BlockMudFlowerPotCandle extends Block {
 	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
 		return Item.getItemFromBlock(BlockRegistry.MUD_FLOWER_POT);
 	}
-	
+
 	@Override
 	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
 		super.getDrops(drops, world, pos, state, fortune);
@@ -97,14 +131,21 @@ public class BlockMudFlowerPotCandle extends Block {
 		return face == EnumFacing.DOWN ? BlockFaceShape.CENTER_SMALL : BlockFaceShape.UNDEFINED;
 	}
 
+	@Override
+	public int getLightValue(IBlockState state) {
+		return state.getValue(LIT) ? 13 : 0;
+	}
+
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
-		double x = (double)pos.getX() + 0.5D;
-		double y = (double)pos.getY() + 1.0D;
-		double z = (double)pos.getZ() + 0.5D;
+		if(stateIn.getValue(LIT)) {
+			double x = (double)pos.getX() + 0.5D;
+			double y = (double)pos.getY() + 1.0D;
+			double z = (double)pos.getZ() + 0.5D;
 
-		worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x, y, z, 0.0D, 0.0D, 0.0D);
-		worldIn.spawnParticle(EnumParticleTypes.FLAME, x, y, z, 0.0D, 0.0D, 0.0D);
+			worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x, y, z, 0.0D, 0.0D, 0.0D);
+			worldIn.spawnParticle(EnumParticleTypes.FLAME, x, y, z, 0.0D, 0.0D, 0.0D);
+		}
 	}
 }
