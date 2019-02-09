61d948e7479094e905893a076195f60812110c8a
TheCyberBrick <samuelboerlin@bluewin.ch>
2019-02-09 13:12:33 +0100

    Rune chain's blueprint is now set to null if rune chain nbt is null

--- src/main/java/thebetweenlands/common/capability/item/RuneChainItemCapability.java
+++ src/main/java/thebetweenlands/common/capability/item/RuneChainItemCapability.java
@@ -4,6 +4,7 @@ import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.Constants;
 import thebetweenlands.api.rune.impl.RuneChainComposition;
 import thebetweenlands.common.capability.base.ItemCapability;
 import thebetweenlands.common.herblore.rune.RuneChainData;
@@ -76,9 +77,11 @@ public class RuneChainItemCapability extends ItemCapability<RuneChainItemCapabil
 
 	private void initFromNbt() {
 		if(this.data == null) {
+			this.blueprint = null;
+			
 			NBTTagCompound itemNbt = this.getItemStack().getTagCompound();
 
-			if(itemNbt != null) {
+			if(itemNbt != null && itemNbt.hasKey(RUNE_CHAIN_BLUEPRINT_NBT_KEY, Constants.NBT.TAG_COMPOUND)) {
 				this.data = RuneChainData.readFromNBT(itemNbt.getCompoundTag(RUNE_CHAIN_BLUEPRINT_NBT_KEY));
 				this.blueprint = this.data.createBlueprint();
 			}
