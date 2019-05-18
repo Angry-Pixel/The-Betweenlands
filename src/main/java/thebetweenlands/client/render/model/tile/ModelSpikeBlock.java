e464c9ee61aab4f4444e1a65a04bb4c8ee442977
vadis365 <curve_linear@hotmail.com>
2019-05-18 19:20:18 +0100

    a bit of spike stuff

--- src/main/java/thebetweenlands/client/render/model/tile/ModelSpikeBlock.java
+++ src/main/java/thebetweenlands/client/render/model/tile/ModelSpikeBlock.java
@@ -1,9 +1,8 @@
 package thebetweenlands.client.render.model.tile;
 
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import thebetweenlands.common.tile.TileEntitySpikeTrap;
@@ -70,9 +69,9 @@ public class ModelSpikeBlock extends ModelBase {
 		float interpolatedAnimationTicks = tile.prevAnimationTicks + (tile.animationTicks - tile.prevAnimationTicks) * partialTicks;
 		if (tile.active || !tile.active && interpolatedAnimationTicks > 0) {
 			if (interpolatedAnimationTicks <= 5)
-				GL11.glTranslatef(0F, 0F - 1F / 5 * interpolatedAnimationTicks, 0F);
+				GlStateManager.translate(0F, 0F - 1F / 5 * interpolatedAnimationTicks, 0F);
 			if (interpolatedAnimationTicks > 5)
-				GL11.glTranslatef(0F, - 1F, 0F);
+				GlStateManager.translate(0F, - 1F, 0F);
 	        shaft4.render(0.0625F);
 	        shaft3.render(0.0625F);
 	        shaft8.render(0.0625F);
--- src/main/java/thebetweenlands/client/render/tile/RenderSpikeTrap.java
+++ src/main/java/thebetweenlands/client/render/tile/RenderSpikeTrap.java
@@ -1,13 +1,17 @@
 package thebetweenlands.client.render.tile;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.GlStateManager.DestFactor;
 import net.minecraft.client.renderer.GlStateManager.SourceFactor;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import thebetweenlands.client.render.model.tile.ModelSpikeBlock;
+import thebetweenlands.common.block.structure.BlockSpikeTrap;
+import thebetweenlands.common.registries.BlockRegistry;
 import thebetweenlands.common.tile.TileEntitySpikeTrap;
 
 @SideOnly(Side.CLIENT)
@@ -18,17 +22,47 @@ public class RenderSpikeTrap extends TileEntitySpecialRenderer<TileEntitySpikeTr
 
 	@Override
 	public void render(TileEntitySpikeTrap tile, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+
+		IBlockState state = tile.getWorld().getBlockState(tile.getPos());
+
+		if(state == null || state.getBlock() != BlockRegistry.SPIKE_TRAP)
+			return;
+
+		EnumFacing facing = state.getValue(BlockSpikeTrap.FACING);
+
 		if (tile.type == 1)
 			bindTexture(ACTIVE_TEXTURE);
 		else
 			bindTexture(INACTIVE_TEXTURE);
-
 		GlStateManager.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-		
+
 		GlStateManager.pushMatrix();
-		GlStateManager.translate((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+		GlStateManager.translate((float) x + 0.5F, (float) y + 0.5F, (float) z + 0.5F);
 		GlStateManager.scale(-1, -1, 1);
+
+		switch (facing) {
+		case UP:
+			GlStateManager.rotate(0F, 0.0F, 1F, 0F);
+			break;
+		case DOWN:
+			GlStateManager.rotate(180F, 1F, 0F, 0F);
+			break;
+		case NORTH:
+			GlStateManager.rotate(90F, 1F, 0F, 0F);
+			break;
+		case SOUTH:
+			GlStateManager.rotate(-90F, 1.0F, 0F, 0F);
+			break;
+		case WEST:
+			GlStateManager.rotate(90F, 0.0F, 0F, 1F);
+			break;
+		case EAST:
+			GlStateManager.rotate(-90F, 0.0F, 0F, 1F);
+			break;
+		}
+
+		GlStateManager.translate(0F, -1F, 0F);
 		GlStateManager.disableCull();
 		MODEL.renderSpikes(tile, partialTicks);
 		GlStateManager.enableCull();;
