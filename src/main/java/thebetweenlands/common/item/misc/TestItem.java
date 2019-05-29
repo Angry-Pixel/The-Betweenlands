244b4493ee79a8697e4e79b44c2303c0250f478b
vadis365 <curve_linear@hotmail.com>
2019-05-29 22:56:02 +0100

    lenny for len

--- src/main/java/thebetweenlands/common/item/misc/TestItem.java
+++ src/main/java/thebetweenlands/common/item/misc/TestItem.java
@@ -113,7 +113,7 @@ public class TestItem extends Item {
 		*/
 			WorldGenSludgeWormDungeon dungeon = new WorldGenSludgeWormDungeon();
 			//dungeon.makeMaze(worldIn, itemRand, pos.up().add(1, 0, 1));
-			dungeon.generate(worldIn, itemRand, pos);
+			dungeon.generateTower(worldIn, itemRand, pos.add(15, 0, 15));
 
 		/*
 			WorldGenNibbletwigTree tree = new WorldGenNibbletwigTree();
