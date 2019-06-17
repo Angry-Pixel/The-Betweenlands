813dfc8ef74589c48d3b2abef5d3e7458e225b1b
TheCyberBrick <samuelboerlin@bluewin.ch>
2019-06-17 15:13:59 +0200

    Added fully upgraded grappling hook to creative menu

--- src/main/java/thebetweenlands/common/item/misc/ItemGrapplingHook.java
+++ src/main/java/thebetweenlands/common/item/misc/ItemGrapplingHook.java
@@ -6,6 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -13,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.Vec3d;
@@ -126,6 +128,15 @@ public class ItemGrapplingHook extends Item {
 	@Override
 	public boolean showDurabilityBar(ItemStack stack) {
 		return false;
+	}
+	
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+		if(this.isInCreativeTab(tab)) {
+			ItemStack baseHook = new ItemStack(this);
+            items.add(baseHook);
+            items.add(new ItemStack(this, 1, baseHook.getMaxDamage()));
+        }
 	}
 
 	public void onGrapplingHookRipped(ItemStack stack, Entity user) {
