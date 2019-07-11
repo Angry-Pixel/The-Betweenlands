51e306a40672a091f0ec40c904a10d653f242f1c
TheCyberBrick <samuelboerlin@bluewin.ch>
2019-07-11 18:02:47 +0200

    Fixed bound fbo not being reset, fixes #843

--- src/main/java/thebetweenlands/client/handler/WorldRenderHandler.java
+++ src/main/java/thebetweenlands/client/handler/WorldRenderHandler.java
@@ -13,8 +13,6 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.GlStateManager.CullFace;
 import net.minecraft.client.renderer.GlStateManager.DestFactor;
 import net.minecraft.client.renderer.GlStateManager.SourceFactor;
-import net.minecraft.client.renderer.OpenGlHelper;
-import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.shader.Framebuffer;
 import net.minecraft.util.math.Vec3d;
@@ -30,7 +28,7 @@ import thebetweenlands.client.render.particle.DefaultParticleBatches;
 import thebetweenlands.client.render.shader.GeometryBuffer;
 import thebetweenlands.client.render.shader.ShaderHelper;
 import thebetweenlands.client.render.shader.postprocessing.WorldShader;
-import thebetweenlands.util.RenderUtils;
+import thebetweenlands.util.FramebufferStack;
 
 
 public class WorldRenderHandler {
@@ -64,14 +62,6 @@ public class WorldRenderHandler {
 	public static void renderWorld(RenderWorldLastEvent event) {
 		Framebuffer mainFramebuffer = MC.getFramebuffer();
 
-		int parentFboId = -1;
-		if(ShaderHelper.INSTANCE.isWorldShaderActive()) {
-			parentFboId = RenderUtils.getBoundFramebuffer();
-		}
-		if(parentFboId == -1) {
-			parentFboId = mainFramebuffer.framebufferObject;
-		}
-
 		GlStateManager.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
 		GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
 		GlStateManager.depthMask(true);
@@ -87,39 +77,39 @@ public class WorldRenderHandler {
 		if(ShaderHelper.INSTANCE.isWorldShaderActive() && sphereDispList >= 0) {
 			WorldShader shader = ShaderHelper.INSTANCE.getWorldShader();
 			if(shader != null) {
-				GeometryBuffer gBuffer = shader.getRepellerShieldBuffer();
-				gBuffer.updateGeometryBuffer(mainFramebuffer.framebufferWidth, mainFramebuffer.framebufferHeight);
-				if(gBuffer != null && gBuffer.isInitialized()) {
-					gBuffer.bind();
-					gBuffer.clear(1.0F, 0.0F, 0.0F, 1.0F);
+				try(FramebufferStack.State state = FramebufferStack.push()) {
+					GeometryBuffer gBuffer = shader.getRepellerShieldBuffer();
+					gBuffer.updateGeometryBuffer(mainFramebuffer.framebufferWidth, mainFramebuffer.framebufferHeight);
+					if(gBuffer != null && gBuffer.isInitialized()) {
+						gBuffer.bind();
+						gBuffer.clear(1.0F, 0.0F, 0.0F, 1.0F);
 
-					if(!REPELLER_SHIELDS.isEmpty()) {
-						GlStateManager.depthMask(true);
-						GlStateManager.disableTexture2D();
-						GlStateManager.disableBlend();
-						GlStateManager.alphaFunc(GL11.GL_GREATER, 0.0F);
-						GlStateManager.color(0.0F, 0.4F + (float)(Math.sin(System.nanoTime() / 500000000.0F) + 1.0F) * 0.2F, 0.8F - (float)(Math.cos(System.nanoTime() / 400000000.0F) + 1.0F) * 0.2F, 1.0F);
-						GlStateManager.disableCull();
+						if(!REPELLER_SHIELDS.isEmpty()) {
+							GlStateManager.depthMask(true);
+							GlStateManager.disableTexture2D();
+							GlStateManager.disableBlend();
+							GlStateManager.alphaFunc(GL11.GL_GREATER, 0.0F);
+							GlStateManager.color(0.0F, 0.4F + (float)(Math.sin(System.nanoTime() / 500000000.0F) + 1.0F) * 0.2F, 0.8F - (float)(Math.cos(System.nanoTime() / 400000000.0F) + 1.0F) * 0.2F, 1.0F);
+							GlStateManager.disableCull();
 
-						//Render to G-Buffer 1
-						for(Entry<Vec3d, Float> e : REPELLER_SHIELDS) {
-							Vec3d pos = e.getKey();
-							GlStateManager.pushMatrix();
-							GlStateManager.translate(pos.x, pos.y, pos.z);
-							GlStateManager.scale(e.getValue(), e.getValue(), e.getValue());
-							GL11.glCallList(sphereDispList);
-							GlStateManager.popMatrix();
+							//Render to G-Buffer 1
+							for(Entry<Vec3d, Float> e : REPELLER_SHIELDS) {
+								Vec3d pos = e.getKey();
+								GlStateManager.pushMatrix();
+								GlStateManager.translate(pos.x, pos.y, pos.z);
+								GlStateManager.scale(e.getValue(), e.getValue(), e.getValue());
+								GL11.glCallList(sphereDispList);
+								GlStateManager.popMatrix();
+							}
+
+							GlStateManager.enableTexture2D();
+							GlStateManager.enableCull();
+							GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+							GlStateManager.color(1, 1, 1, 1);
 						}
 
-						GlStateManager.enableTexture2D();
-						GlStateManager.enableCull();
-						GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
-						GlStateManager.color(1, 1, 1, 1);
+						gBuffer.updateDepthBuffer();
 					}
-
-					gBuffer.updateDepthBuffer();
-
-					OpenGlHelper.glBindFramebuffer(OpenGlHelper.GL_FRAMEBUFFER, parentFboId);
 				}
 			}
 		} else if(sphereDispList >= 0 && !REPELLER_SHIELDS.isEmpty()) {
@@ -154,32 +144,32 @@ public class WorldRenderHandler {
 		if(ShaderHelper.INSTANCE.isWorldShaderActive() && (!DefaultParticleBatches.HEAT_HAZE_PARTICLE_ATLAS.isEmpty() || !DefaultParticleBatches.HEAT_HAZE_BLOCK_ATLAS.isEmpty())) {
 			ShaderHelper.INSTANCE.require();
 		}
-		
+
 		//Gas clouds/Heat haze
 		if(ShaderHelper.INSTANCE.isWorldShaderActive() && MC.getRenderViewEntity() != null) {
 			GeometryBuffer fbo = ShaderHelper.INSTANCE.getWorldShader().getGasParticleBuffer();
 			if(fbo != null) {
-				fbo.updateGeometryBuffer(mainFramebuffer.framebufferWidth, mainFramebuffer.framebufferHeight);
-				fbo.clear(0, 0, 0, 0, 1);
+				try(FramebufferStack.State state = FramebufferStack.push()) {
+					fbo.updateGeometryBuffer(mainFramebuffer.framebufferWidth, mainFramebuffer.framebufferHeight);
+					fbo.clear(0, 0, 0, 0, 1);
 
-				if(!DefaultParticleBatches.GAS_CLOUDS.isEmpty()) {
-					MC.getTextureManager().bindTexture(RenderGasCloud.TEXTURE);
+					if(!DefaultParticleBatches.GAS_CLOUDS.isEmpty()) {
+						MC.getTextureManager().bindTexture(RenderGasCloud.TEXTURE);
 
-					BatchedParticleRenderer.INSTANCE.renderBatch(DefaultParticleBatches.GAS_CLOUDS, MC.getRenderViewEntity(), event.getPartialTicks());
+						BatchedParticleRenderer.INSTANCE.renderBatch(DefaultParticleBatches.GAS_CLOUDS, MC.getRenderViewEntity(), event.getPartialTicks());
+					}
+
+					if(!DefaultParticleBatches.HEAT_HAZE_PARTICLE_ATLAS.isEmpty()) {
+						BatchedParticleRenderer.INSTANCE.renderBatch(DefaultParticleBatches.HEAT_HAZE_PARTICLE_ATLAS, MC.getRenderViewEntity(), event.getPartialTicks());
+					}
+
+					if(!DefaultParticleBatches.HEAT_HAZE_BLOCK_ATLAS.isEmpty()) {
+						BatchedParticleRenderer.INSTANCE.renderBatch(DefaultParticleBatches.HEAT_HAZE_BLOCK_ATLAS, MC.getRenderViewEntity(), event.getPartialTicks());
+					}
+
+					//Update gas particles depth buffer
+					fbo.updateDepthBuffer();
 				}
-				
-				if(!DefaultParticleBatches.HEAT_HAZE_PARTICLE_ATLAS.isEmpty()) {
-					BatchedParticleRenderer.INSTANCE.renderBatch(DefaultParticleBatches.HEAT_HAZE_PARTICLE_ATLAS, MC.getRenderViewEntity(), event.getPartialTicks());
-				}
-				
-				if(!DefaultParticleBatches.HEAT_HAZE_BLOCK_ATLAS.isEmpty()) {
-					BatchedParticleRenderer.INSTANCE.renderBatch(DefaultParticleBatches.HEAT_HAZE_BLOCK_ATLAS, MC.getRenderViewEntity(), event.getPartialTicks());
-				}
-
-				//Update gas particles depth buffer
-				fbo.updateDepthBuffer();
-
-				OpenGlHelper.glBindFramebuffer(OpenGlHelper.GL_FRAMEBUFFER, parentFboId);
 			}
 		}
 
--- src/main/java/thebetweenlands/util/RenderUtils.java
+++ src/main/java/thebetweenlands/util/RenderUtils.java
@@ -41,62 +41,41 @@ public class RenderUtils {
 	}
 	
 	/**
-	 * Returns the currently bound framebuffer, or -1 if not supported
-	 * @return
-	 */
-	public static int getBoundFramebuffer() {
-		if (OpenGlHelper.framebufferSupported) {
-            switch (OpenGlHelper.framebufferType) {
-                case BASE:
-                	return GL11.glGetInteger(GL30.GL_FRAMEBUFFER_BINDING);
-                case ARB:
-                	return GL11.glGetInteger(ARBFramebufferObject.GL_FRAMEBUFFER_BINDING);
-                case EXT:
-                	return GL11.glGetInteger(EXTFramebufferObject.GL_FRAMEBUFFER_BINDING_EXT);
-            }
-        }
-		return -1;
-	}
-	
-	/**
 	 * Saves the texture of an FBO to the specified PNG file.
 	 * 
 	 * @param file
 	 * @param fbo
 	 */
 	public static void saveFboToFile(File file, Framebuffer fbo) {
-		int prevFBO = getBoundFramebuffer();
-
-		fbo.bindFramebuffer(false);
-
-		GL11.glReadBuffer(GL11.GL_FRONT);
-		int width = fbo.framebufferWidth;
-		int height= fbo.framebufferHeight;
-		int bpp = 4;
-		ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * bpp);
-		GL11.glReadPixels(0, 0, width, height, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
-
-		String format = "PNG";
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-
-		for(int x = 0; x < width; x++) {
-			for(int y = 0; y < height; y++) {
-				int i = (x + (width * y)) * bpp;
-				int r = buffer.get(i) & 0xFF;
-				int g = buffer.get(i + 1) & 0xFF;
-				int b = buffer.get(i + 2) & 0xFF;
-				int a = buffer.get(i + 3) & 0xFF;
-				image.setRGB(x, height - (y + 1), (a << 24) | (r << 16) | (g << 8) | b);
+		try(FramebufferStack.State state = FramebufferStack.push()) {
+			fbo.bindFramebuffer(false);
+	
+			GL11.glReadBuffer(GL11.GL_FRONT);
+			int width = fbo.framebufferWidth;
+			int height= fbo.framebufferHeight;
+			int bpp = 4;
+			ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * bpp);
+			GL11.glReadPixels(0, 0, width, height, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+	
+			String format = "PNG";
+			BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	
+			for(int x = 0; x < width; x++) {
+				for(int y = 0; y < height; y++) {
+					int i = (x + (width * y)) * bpp;
+					int r = buffer.get(i) & 0xFF;
+					int g = buffer.get(i + 1) & 0xFF;
+					int b = buffer.get(i + 2) & 0xFF;
+					int a = buffer.get(i + 3) & 0xFF;
+					image.setRGB(x, height - (y + 1), (a << 24) | (r << 16) | (g << 8) | b);
+				}
+			}
+	
+			try {
+				ImageIO.write(image, format, file);
+			} catch (IOException e) { 
+				e.printStackTrace(); 
 			}
 		}
-
-		try {
-			ImageIO.write(image, format, file);
-		} catch (IOException e) { 
-			e.printStackTrace(); 
-		}
-
-		//Bind previous fbo
-		OpenGlHelper.glBindFramebuffer(OpenGlHelper.GL_RENDERBUFFER, prevFBO);
 	}
 }
