a97f146941473441a61cc370ab4ede300b889f19
vadis365 <curve_linear@hotmail.com>
2019-08-03 20:41:17 +0100

    edgeplant stuff
    added better bounds and some plant based methods

--- src/main/java/thebetweenlands/common/block/plant/BlockEdgeShroom.java
+++ src/main/java/thebetweenlands/common/block/plant/BlockEdgeShroom.java
@@ -5,7 +5,9 @@ import java.util.Random;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.block.Block;
+import com.google.common.collect.ImmutableList;
+
+import net.minecraft.block.BlockBush;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -15,6 +17,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
@@ -22,14 +25,19 @@ import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.common.IShearable;
 import thebetweenlands.client.tab.BLCreativeTabs;
+import thebetweenlands.common.block.SoilHelper;
+import thebetweenlands.common.registries.ItemRegistry;
 
-public class BlockEdgeShroom extends Block {
+public class BlockEdgeShroom extends BlockBush implements IShearable {
     public static final PropertyDirection FACING = BlockHorizontal.FACING;
-
+    protected static final AxisAlignedBB PLANT_AABB_NORTH = new AxisAlignedBB(0D, 0D, 0.5D, 1D, 0.25D, 1D);
+    protected static final AxisAlignedBB PLANT_AABB_SOUTH = new AxisAlignedBB(0D, 0D, 0D, 1D, 0.25D, 0.5D);
+    protected static final AxisAlignedBB PLANT_AABB_EAST = new AxisAlignedBB(0.0D, 0D, 0D, 0.5D, 0.25D, 1D);
+    protected static final AxisAlignedBB PLANT_AABB_WEST = new AxisAlignedBB(0.5D, 0D, 0D, 1D, 0.25D, 1D);
     public BlockEdgeShroom() {
         super(Material.PLANTS);
         setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
@@ -43,7 +51,35 @@ public class BlockEdgeShroom extends Block {
 		//No solid collision
 	}
 
-    @Override
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		switch (state.getValue(FACING)) {
+		case DOWN:
+		case UP:
+		case NORTH:
+			return PLANT_AABB_NORTH;
+		case SOUTH:
+			return PLANT_AABB_SOUTH;
+		case EAST:
+			return PLANT_AABB_EAST;
+		case WEST:
+			return PLANT_AABB_WEST;
+		default:
+			return PLANT_AABB_NORTH;
+		}
+	}
+
+	@Override
+	protected boolean canSustainBush(IBlockState state) {
+		return SoilHelper.canSustainPlant(state);
+	}
+
+	@Override
+	public boolean canPlaceBlockAt(World world, BlockPos pos) {
+		return super.canPlaceBlockAt(world, pos) && world.isAirBlock(pos.up()) && this.canBlockStay(world, pos, world.getBlockState(pos));
+	}
+
+	@Override
     protected BlockStateContainer createBlockState() {
         return new BlockStateContainer(this, FACING);
     }
@@ -77,12 +113,12 @@ public class BlockEdgeShroom extends Block {
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
         return Item.getItemFromBlock(this);
     }
-    
+
 	@Override
 	public EnumBlockRenderType getRenderType(IBlockState state) {
 		return EnumBlockRenderType.MODEL;
 	}
-    
+
 	@Override
     public boolean canRenderInLayer(IBlockState state, BlockRenderLayer layer) {
 		return layer == BlockRenderLayer.SOLID || layer == BlockRenderLayer.TRANSLUCENT;
@@ -92,5 +128,15 @@ public class BlockEdgeShroom extends Block {
     public boolean isOpaqueCube(IBlockState state) {
         return false;
     }
+
+	@Override
+	public boolean isShearable(ItemStack item, IBlockAccess world, BlockPos pos) {
+		return item.getItem() == ItemRegistry.SYRMORITE_SHEARS;
+	}
+
+	@Override
+	public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune) {
+		return ImmutableList.of(new ItemStack(Item.getItemFromBlock(this)));
+	}
 
 }
