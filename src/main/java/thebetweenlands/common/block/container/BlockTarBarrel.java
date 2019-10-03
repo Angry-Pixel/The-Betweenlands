ccc7749c46e59e8b4cdd967e1d57da490dc51345
TheCyberBrick <samuelboerlin@bluewin.ch>
2019-10-03 18:36:55 +0200

    Fixed torches etc. placeable on side of tar barrel

--- src/main/java/thebetweenlands/common/block/container/BlockTarBarrel.java
+++ src/main/java/thebetweenlands/common/block/container/BlockTarBarrel.java
@@ -8,6 +8,7 @@ import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -186,5 +187,10 @@ public class BlockTarBarrel extends BasicBlock implements ITileEntityProvider, I
 				}
 			}
 		}
+	}
+	
+	@Override
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+		return BlockFaceShape.UNDEFINED;
 	}
 }
