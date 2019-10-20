33f3ad42c08924635eeb25e0f8a9c3958f68f754
TheCyberBrick <samuelboerlin@bluewin.ch>
2019-10-20 14:32:59 +0200

    Re-added wild emberling eyes

--- src/main/java/thebetweenlands/client/render/entity/RenderEmberlingWild.java
+++ src/main/java/thebetweenlands/client/render/entity/RenderEmberlingWild.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import thebetweenlands.client.render.entity.layer.LayerOverlay;
 import thebetweenlands.client.render.model.entity.ModelEmberlingWild;
 import thebetweenlands.common.entity.mobs.EntityEmberlingWild;
 
@@ -15,7 +16,14 @@ public class RenderEmberlingWild extends RenderLiving<EntityEmberlingWild> {
 
 	public RenderEmberlingWild(RenderManager renderManagerIn) {
         super(renderManagerIn, new ModelEmberlingWild(), 0.5F);
-        //addLayer(new LayerOverlay<EntityEmberlingWild>(this, new ResourceLocation("thebetweenlands:textures/entity/emberling_glow.png")).setGlow(true));
+        addLayer(new LayerOverlay<EntityEmberlingWild>(this, new ResourceLocation("thebetweenlands:textures/entity/emberling_glow.png")) {
+        	@Override
+        	public void doRenderLayer(EntityEmberlingWild entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        		if(!entity.isSitting()) {
+        			super.doRenderLayer(entity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale);
+        		}
+        	}
+        }.setGlow(true));
     }
 
 	@Override
