c61d4d8e761e0807a8a6c9c931e15965301ce8c6
TheCyberBrick <samuelboerlin@bluewin.ch>
2019-11-18 15:40:45 +0100

    - Fixed great axes not dealing extra damage on spirit tree faces - Fixed great axes not disabling shields - Fixed ancient battle axe still able to break blocks even when fully damaged - Improved great axe block breaking consistency - Valonite great axe now has twice the durability

--- src/main/java/thebetweenlands/common/item/tools/ItemGreataxe.java
+++ src/main/java/thebetweenlands/common/item/tools/ItemGreataxe.java
@@ -26,8 +26,9 @@ import thebetweenlands.client.tab.BLCreativeTabs;
 public class ItemGreataxe extends ItemGreatsword {
 	public ItemGreataxe(ToolMaterial material) {
 		super(material);
-		this.setHarvestLevel("axe", 0);
+		this.setHarvestLevel("axe", 3);
 		this.setCreativeTab(BLCreativeTabs.GEARS);
+		this.setMaxDamage(material.getMaxUses() * 2);
 	}
 
 	protected double getBlockBreakHalfAngle(EntityLivingBase entity, ItemStack stack) {
@@ -46,48 +47,67 @@ public class ItemGreataxe extends ItemGreatsword {
 			EntityPlayerMP player = (EntityPlayerMP) holder;
 
 			if(player.getHeldItemMainhand() == stack && this.isLongSwingInProgress(stack) && this.getSwingStartCooledAttackStrength(stack) > 0.85F) {
-				int ticksElapsed = player.ticksExisted - this.getSwingStartTicks(stack);
-				float longSwingProgressStart = ticksElapsed / this.getLongSwingDuration(player, stack);
-				float longSwingProgressEnd = (ticksElapsed + 1) / this.getLongSwingDuration(player, stack);
+				int ticksElapsed = player.ticksExisted - this.getSwingStartTicks(stack) - 1;
 
-				double breakReach = this.getBlockBreakReach(player, stack);
-				int blockReach = MathHelper.ceil(breakReach);
-
-				double breakHalfAngle = this.getBlockBreakHalfAngle(player, stack);
-
-				double minAngle = -breakHalfAngle + breakHalfAngle * 2 * longSwingProgressStart;
-				double maxAngle = -breakHalfAngle + breakHalfAngle * 2 * longSwingProgressEnd;
+				float longSwingTickProgress = 1.0F / (this.getLongSwingDuration(player, stack) - 1);
+				float longSwingProgressEnd = (ticksElapsed + 1) / (this.getLongSwingDuration(player, stack) - 1);
 
 				List<BlockPos> targetBlocks = new ArrayList<>();
 
-				Vec3d lookVec = player.getLookVec();
-				Vec3d rightVec = lookVec.crossProduct(new Vec3d(0, 1, 0)).normalize();
+				for(float longSwingProgressStart = Math.max(0, longSwingProgressEnd - Math.max(0.25F, longSwingTickProgress)); longSwingProgressStart < longSwingProgressEnd; longSwingProgressStart += longSwingTickProgress) {
+					double breakReach = this.getBlockBreakReach(player, stack);
+					int blockReach = MathHelper.ceil(breakReach);
 
-				for(int xo = -blockReach; xo <= blockReach; xo++) {
-					for(int yo = -blockReach; yo <= blockReach; yo++) {
-						for(int zo = -blockReach; zo <= blockReach; zo++) {
-							BlockPos pos = new BlockPos(player.posX + xo, player.posY + player.height * 0.5D + yo, player.posZ + zo);
-							Vec3d center = new Vec3d(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D);
+					double breakHalfAngle = this.getBlockBreakHalfAngle(player, stack);
 
-							double dist = center.distanceTo(player.getPositionEyes(1));
+					double minAngle = -breakHalfAngle + breakHalfAngle * 2 * longSwingProgressStart;
+					double maxAngle = -breakHalfAngle + breakHalfAngle * 2 * longSwingProgressEnd;
 
-							if(dist < breakReach) {
-								Vec3d dir = center.subtract(player.getPositionEyes(1)).normalize();
+					float yaw = player.rotationYaw;
+					float pitch = player.rotationPitch;
 
-								double py = lookVec.dotProduct(dir);
-								double px = rightVec.dotProduct(dir);
+					float yc = MathHelper.cos(-yaw * 0.017453292F - (float)Math.PI);
+					float ys = MathHelper.sin(-yaw * 0.017453292F - (float)Math.PI);
+					float pc = -MathHelper.cos(-pitch * 0.017453292F);
+					float ps = MathHelper.sin(-pitch * 0.017453292F);
 
-								double angle = Math.toDegrees(-Math.atan2(px, py));
+					Vec3d forward = new Vec3d((double)(ys * pc), (double)ps, (double)(yc * pc)).normalize();
 
-								if(angle >= minAngle && angle < maxAngle) {
-									double distXZ = Math.sqrt((center.x - player.posX)*(center.x - player.posX) + (center.z - player.posZ)*(center.z - player.posZ));
+					pc = -MathHelper.cos(-(pitch - 90) * 0.017453292F);
+					ps = MathHelper.sin(-(pitch - 90) * 0.017453292F);
 
-									if(player.getLookVec().y * distXZ + player.posY + player.height / 2 + 0.25D > pos.getY() - 0.25D && player.getLookVec().y * distXZ + player.posY + player.height / 2 + 0.25D < pos.getY() + 1.25D) {
-										IBlockState state = player.world.getBlockState(pos);
+					Vec3d up = new Vec3d((double)(ys * pc), (double)ps, (double)(yc * pc)).normalize();
 
-										if((state.getBlock().isWood(player.world, pos) || state.getMaterial() == Material.WOOD) && state.getBlockHardness(player.world, pos) <= 2.25F &&
-												state.getPlayerRelativeBlockHardness(player, player.world, pos) > 0.01F) {
-											targetBlocks.add(pos);
+					Vec3d right = forward.crossProduct(up);
+
+					for(int xo = -blockReach; xo <= blockReach; xo++) {
+						for(int yo = -blockReach; yo <= blockReach; yo++) {
+							for(int zo = -blockReach; zo <= blockReach; zo++) {
+								BlockPos pos = new BlockPos(player.posX + xo, player.posY + player.height * 0.5D + yo, player.posZ + zo);
+								Vec3d center = new Vec3d(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D);
+
+								double dist = center.distanceTo(player.getPositionEyes(1));
+
+								if(dist < breakReach) {
+									Vec3d dir = center.subtract(player.getPositionEyes(1)).normalize();
+
+									double py = forward.dotProduct(dir);
+									double px = right.dotProduct(dir);
+
+									double angle = Math.toDegrees(-Math.atan2(px, py));
+
+									if(angle >= minAngle && angle < maxAngle) {
+										double distUp = up.dotProduct(new Vec3d(center.x - player.posX, center.y - player.posY - player.getEyeHeight(), center.z - player.posZ));
+
+										double verticalRange = 1.0D + 1.5D * (3 - MathHelper.clamp(dist, 0, 3)) / 3.0D;
+
+										if(distUp >= -verticalRange - 0.5D && distUp <= verticalRange - 0.5D) {
+											IBlockState state = player.world.getBlockState(pos);
+
+											if((state.getBlock().isWood(player.world, pos) || state.getMaterial() == Material.WOOD) && state.getBlockHardness(player.world, pos) <= 2.25F &&
+													state.getPlayerRelativeBlockHardness(player, player.world, pos) > 0.01F) {
+												targetBlocks.add(pos);
+											}
 										}
 									}
 								}
@@ -101,11 +121,13 @@ public class ItemGreataxe extends ItemGreatsword {
 
 					int playedEffects = 0;
 					for(BlockPos pos : targetBlocks) {
-						IBlockState state = player.world.getBlockState(pos);
+						if(!world.isAirBlock(pos)) {
+							IBlockState state = player.world.getBlockState(pos);
 
-						if(player.interactionManager.tryHarvestBlock(pos)) {
-							if(++playedEffects <= 3) {
-								player.world.playEvent(null, 2001, pos, Block.getStateId(state));
+							if(player.interactionManager.tryHarvestBlock(pos)) {
+								if(++playedEffects <= 3) {
+									player.world.playEvent(null, 2001, pos, Block.getStateId(state));
+								}
 							}
 						}
 					}
@@ -144,6 +166,11 @@ public class ItemGreataxe extends ItemGreatsword {
 	@Override
 	public float getAttackDamage() {
 		return super.getAttackDamage() + 2.0f;
+	}
+
+	@Override
+	public boolean canDisableShield(ItemStack stack, ItemStack shield, EntityLivingBase entity, EntityLivingBase attacker) {
+		return true;
 	}
 
 	@Override
