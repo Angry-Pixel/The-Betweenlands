59e65ae3faddd60495f15ac39eac1f65d28ab22d
Nikita Kniazev <nok.raven@gmail.com>
2020-02-20 20:24:38 +0300

    Fixed java.lang.IllegalAccessError: tried to access field net.minecraft.item.Item.field_111210_e

--- src/main/java/thebetweenlands/common/item/tools/ItemBLShovel.java
+++ src/main/java/thebetweenlands/common/item/tools/ItemBLShovel.java
@@ -61,7 +61,7 @@ public class ItemBLShovel extends ItemSpade implements ICorrodible, IAnimatorRep
 
 	@Override
 	public Multimap<String, AttributeModifier> getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack) {
-		return CorrosionHelper.getAttributeModifiers(super.getAttributeModifiers(slot, stack), slot, stack, ItemTool.ATTACK_DAMAGE_MODIFIER, this.attackDamage);
+		return CorrosionHelper.getAttributeModifiers(super.getAttributeModifiers(slot, stack), slot, stack, ATTACK_DAMAGE_MODIFIER, this.attackDamage);
 	}
 
 	@SideOnly(Side.CLIENT)
