eaa38640c1407084fb31a790be4b0c7d7ea44b2a
TheCyberBrick <samuelboerlin@bluewin.ch>
2020-03-29 15:52:35 +0200

    Critter subitems are now taken from net's catchable map

--- src/main/java/thebetweenlands/common/item/misc/ItemCritters.java
+++ src/main/java/thebetweenlands/common/item/misc/ItemCritters.java
@@ -1,10 +1,13 @@
 package thebetweenlands.common.item.misc;
 
+import java.util.Map.Entry;
+import java.util.function.Supplier;
+
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
-import thebetweenlands.common.entity.mobs.EntityFirefly;
-import thebetweenlands.common.entity.mobs.EntityGecko;
+import thebetweenlands.common.item.tools.ItemNet;
 
 public class ItemCritters extends ItemMob {
 	public ItemCritters() {
@@ -14,8 +17,11 @@ public class ItemCritters extends ItemMob {
 	@Override
 	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
 		if(this.isInCreativeTab(tab)) {
-			items.add(this.capture(EntityGecko.class));
-			items.add(this.capture(EntityFirefly.class));
+			for(Entry<Class<? extends Entity>, Supplier<? extends ItemMob>> entry : ItemNet.CATCHABLE_ENTITIES.entrySet()) {
+				if(entry.getValue().get() == this) {
+					items.add(this.capture(entry.getKey()));
+				}
+			}
 		}
 	}
 }
