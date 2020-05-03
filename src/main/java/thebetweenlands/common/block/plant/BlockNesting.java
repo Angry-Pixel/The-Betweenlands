e359635ba0408ce4dcb7b82ff1f68e6e488b9ad7
vadis365 <curve_linear@hotmail.com>
2020-05-04 00:29:34 +0100

    so okl can sleep at night

--- src/main/java/thebetweenlands/common/block/plant/BlockNesting.java
+++ src/main/java/thebetweenlands/common/block/plant/BlockNesting.java
@@ -9,15 +9,19 @@ import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import thebetweenlands.api.entity.IEntityBL;
 import thebetweenlands.common.herblore.elixir.ElixirEffectRegistry;
+import thebetweenlands.common.item.misc.ItemMisc.EnumItemMisc;
+import thebetweenlands.common.registries.BlockRegistry;
 
 public class BlockNesting extends BlockWeedwoodBush {
 	private ItemStack drop;
@@ -55,5 +59,10 @@ public class BlockNesting extends BlockWeedwoodBush {
 	@Override
 	public int getColorMultiplier(IBlockState state, IBlockAccess worldIn, BlockPos pos, int tintIndex) {
 		return 0xFFFFFFFF;
+	}
+	
+	@Override
+	public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+		return EnumItemMisc.SLIMY_BONE.isItemOf(drop) ? new ItemStack(BlockRegistry.NESTING_BLOCK_BONES) : new ItemStack(BlockRegistry.NESTING_BLOCK_STICKS);
 	}
 }
