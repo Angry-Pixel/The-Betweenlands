96db71a5f2f15d4bbd8b20e184c6bf36fb89a632
vadis365 <curve_linear@hotmail.com>
2020-05-05 15:09:21 +0100

    removed valonite armour from sludgeon

--- src/main/java/thebetweenlands/common/world/gen/feature/structure/utils/SludgeWormMazeBlockHelper.java
+++ src/main/java/thebetweenlands/common/world/gen/feature/structure/utils/SludgeWormMazeBlockHelper.java
@@ -720,10 +720,10 @@ public class SludgeWormMazeBlockHelper {
 	}
 
 	public void placeArmourStandLoot(World world, BlockPos pos, EnumFacing facing, Random rand) {
-		ItemStack helm = new ItemStack(ItemRegistry.VALONITE_HELMET);
-		ItemStack chest = new ItemStack(ItemRegistry.VALONITE_CHESTPLATE);
-		ItemStack legs = new ItemStack(ItemRegistry.VALONITE_LEGGINGS);
-		ItemStack boots = new ItemStack(ItemRegistry.VALONITE_BOOTS);
+		ItemStack helm = new ItemStack(ItemRegistry.ANCIENT_HELMET);
+		ItemStack chest = new ItemStack(ItemRegistry.ANCIENT_CHESTPLATE);
+		ItemStack legs = new ItemStack(ItemRegistry.ANCIENT_LEGGINGS);
+		ItemStack boots = new ItemStack(ItemRegistry.ANCIENT_BOOTS);
 		CircleGemHelper.setGem(helm, getCircleGemType(rand));
 		CircleGemHelper.setGem(chest, getCircleGemType(rand));
 		CircleGemHelper.setGem(boots, getCircleGemType(rand));
