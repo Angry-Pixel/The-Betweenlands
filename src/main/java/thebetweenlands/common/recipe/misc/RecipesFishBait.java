cf84b09924ea3f4215a66fa77cfb1dcd3862e231
vadis365 <curve_linear@hotmail.com>
2020-07-30 19:51:46 +0100

    crafting stuff

--- src/main/java/thebetweenlands/common/recipe/misc/RecipesFishBait.java
+++ src/main/java/thebetweenlands/common/recipe/misc/RecipesFishBait.java
@@ -13,17 +13,12 @@ public class RecipesFishBait extends net.minecraftforge.registries.IForgeRegistr
     @Override
     public boolean matches(InventoryCrafting inv, World worldIn) {
         this.resultItem = ItemStack.EMPTY;
-        boolean hasBaitItem = false;
-        boolean addSaturation = false;
-        boolean addSinkSpeed = false;
-        boolean addDissolveTime = false;
-        boolean addRange = false;
+        int hasBaitItem = 0;
+        int addSaturation = 0;
+        int addSinkSpeed = 0;
+        int addDissolveTime = 0;
+        int addRange = 0;
         boolean addGlowing = false;
-
-        boolean minusSaturation = false;
-        boolean minusSinkSpeed = false;
-        boolean minusDissolveTime = false;
-        boolean minusRange = false;
         boolean minusGlowing = false;
 
 		for (int inputSlot = 0; inputSlot < inv.getSizeInventory(); ++inputSlot) {
@@ -31,39 +26,33 @@ public class RecipesFishBait extends net.minecraftforge.registries.IForgeRegistr
 
 			if (!itemstack.isEmpty()) {
 				if (itemstack.getItem() == ItemRegistry.FISH_BAIT)
-					hasBaitItem = true;
+					hasBaitItem++;
 				else if (itemstack.getItem() == ItemRegistry.SNAIL_FLESH_RAW)
-					addSaturation = true;
+					addSaturation++;
 				else if (itemstack.getItem() == ItemRegistry.CHRISTMAS_PUDDING)
-					addSinkSpeed = true;
+					addSinkSpeed++;
 				else if (itemstack.getItem() == ItemRegistry.GLUE)
-					addDissolveTime = true;
+					addDissolveTime++;
 				else if (itemstack.getItem() == ItemRegistry.CRAB_STICK)
-					addRange = true;
+					addRange++;
 				else if (itemstack.getItem() == ItemRegistry.BULB_CAPPED_MUSHROOM_ITEM)
 					addGlowing = true;
 				else if (itemstack.getItem() == ItemRegistry.CANDY_BLUE)
-					minusSaturation = true;
+					addSaturation--;
 				else if (itemstack.getItem() == ItemRegistry.MARSHMALLOW_PINK)
-					minusSinkSpeed = true;
+					addSinkSpeed--;
 				else if (itemstack.getItem() == ItemRegistry.SAP_SPIT)
-					minusDissolveTime = true;
+					addDissolveTime--;
 				else if (itemstack.getItem() == ItemRegistry.SLUDGE_BALL)
-					minusRange = true;
+					addRange--;
 				else if (itemstack.getItem() == ItemRegistry.FLAT_HEAD_MUSHROOM_ITEM)
 					minusGlowing = true;
+				else
+					return false;
 			}
 		}
 
-		if (hasBaitItem) {
-			if(addSaturation && minusSaturation)
-				return false;
-			if(addSinkSpeed && minusSinkSpeed)
-				return false;
-			if(addDissolveTime && minusDissolveTime)
-				return false;
-			if(addRange && minusRange)
-				return false;
+		if (hasBaitItem == 1) {
 			if(addGlowing && minusGlowing)
 				return false;
 
@@ -73,45 +62,33 @@ public class RecipesFishBait extends net.minecraftforge.registries.IForgeRegistr
 				if (stack.getItem() == ItemRegistry.FISH_BAIT && stack.hasTagCompound()) {
 
 					this.resultItem = stack.copy().splitStack(1);
+
 					if(stack.getTagCompound().getBoolean("glowing") && addGlowing || !stack.getTagCompound().getBoolean("glowing") && minusGlowing)
 						return false;
 
-					//add
-					if (addSaturation)
-						resultItem.getTagCompound().setInteger("saturation", stack.getTagCompound().getInteger("saturation") + 20);
-					if (addSinkSpeed)
-						resultItem.getTagCompound().setInteger("sink_speed", stack.getTagCompound().getInteger("sink_speed") + 1);
-					if (addDissolveTime)
-						resultItem.getTagCompound().setInteger("dissolve_time", stack.getTagCompound().getInteger("dissolve_time") + 20);
-					if (addRange)
-						resultItem.getTagCompound().setInteger("range", stack.getTagCompound().getInteger("range") + 1);
+					//add stuffs
+					if (stack.getTagCompound().getInteger("saturation") + addSaturation * 20 < 0)
+						return false;
+					else
+						resultItem.getTagCompound().setInteger("saturation", stack.getTagCompound().getInteger("saturation") + addSaturation * 20);
+
+					if (stack.getTagCompound().getInteger("sink_speed") + addSinkSpeed < 0)
+						return false;
+					else
+						resultItem.getTagCompound().setInteger("sink_speed", stack.getTagCompound().getInteger("sink_speed") + addSinkSpeed);
+
+					if (stack.getTagCompound().getInteger("dissolve_time") + addDissolveTime * 20 < 0)
+						return false;
+					else
+						resultItem.getTagCompound().setInteger("dissolve_time", stack.getTagCompound().getInteger("dissolve_time") + addDissolveTime * 20);
+
+					if (stack.getTagCompound().getInteger("range") + addRange < 0)
+						return false;
+					else
+						resultItem.getTagCompound().setInteger("range", stack.getTagCompound().getInteger("range") + addRange);
+
 					if (addGlowing)
 						resultItem.getTagCompound().setBoolean("glowing", true);
-
-					//minus
-					if (minusSaturation)
-						if (stack.getTagCompound().getInteger("saturation") <= 0)
-							return false;
-						else
-							resultItem.getTagCompound().setInteger("saturation", stack.getTagCompound().getInteger("saturation") - 20);
-	
-					if (minusSinkSpeed)
-						if (stack.getTagCompound().getInteger("sink_speed") <= 0)
-							return false;
-						else
-							resultItem.getTagCompound().setInteger("sink_speed", stack.getTagCompound().getInteger("sink_speed") - 1);
-
-					if (minusDissolveTime)
-						if (stack.getTagCompound().getInteger("dissolve_time") <= 0)
-							return false;
-						else
-							resultItem.getTagCompound().setInteger("dissolve_time", stack.getTagCompound().getInteger("dissolve_time") - 20);
-
-					if (minusRange)
-						if (stack.getTagCompound().getInteger("range") <= 0)
-							return false;
-						else
-							resultItem.getTagCompound().setInteger("range", stack.getTagCompound().getInteger("range") - 1);
 
 					if (minusGlowing)
 						resultItem.getTagCompound().setBoolean("glowing", false);
