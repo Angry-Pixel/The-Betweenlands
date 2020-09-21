b16aa19574c791757295cbecde8b62d4c32cf6ea
vadis365 <curve_linear@hotmail.com>
2020-09-22 00:11:15 +0100

    removed unused import

--- src/main/java/thebetweenlands/common/entity/mobs/EntityRockSnotTendril.java
+++ src/main/java/thebetweenlands/common/entity/mobs/EntityRockSnotTendril.java
@@ -1,11 +1,8 @@
 package thebetweenlands.common.entity.mobs;
 
 import java.util.List;
-import java.util.Random;
 
 import io.netty.buffer.ByteBuf;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.MoverType;
@@ -14,7 +11,6 @@ import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
