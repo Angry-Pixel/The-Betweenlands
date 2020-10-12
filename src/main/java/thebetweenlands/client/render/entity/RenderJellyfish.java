03a315956749985270b568a78b032ca32a19d78a
vadis365 <curve_linear@hotmail.com>
2020-10-12 13:46:23 +0100

    Revert "removed the good stuff"
    This reverts commit 72b6c0c375f1bb807fa722bd342bdeba905a12cf.

--- src/main/java/thebetweenlands/client/render/entity/RenderJellyfish.java
+++ src/main/java/thebetweenlands/client/render/entity/RenderJellyfish.java
@@ -4,6 +4,7 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import thebetweenlands.client.render.model.entity.ModelJellyfish;
@@ -33,12 +34,14 @@ public class RenderJellyfish extends RenderLiving<EntityJellyfish> {
 	@Override
 	public void doRender(EntityJellyfish jellyfish, double x, double y, double z, float entityYaw, float partialTicks) {
 		float smoothedYaw = jellyfish.prevRotationYaw + (jellyfish.rotationYaw - jellyfish.prevRotationYaw) * partialTicks;
+		float smoothedPitch = jellyfish.prevRotationPitch + (jellyfish.rotationPitch - jellyfish.prevRotationPitch) * partialTicks + 90F;
 		float scale = jellyfish.getJellyfishSize();
 		shadowSize = scale * 0.5F;
 		GlStateManager.pushMatrix();
 		GlStateManager.translate(x, y + scale * 0.5F, z);
 		GlStateManager.scale(scale, -scale, -scale);
 		GlStateManager.rotate(smoothedYaw, 0F, 1F, 0F);
+		GlStateManager.rotate(smoothedPitch, 1F, 0F, 0F);
 		bindTexture(TEXTURE[jellyfish.getJellyfishColour()]);
 		GlStateManager.pushMatrix();
 		GlStateManager.enableBlend();
@@ -47,6 +50,7 @@ public class RenderJellyfish extends RenderLiving<EntityJellyfish> {
 		GlStateManager.depthMask(!jellyfish.isInvisible());
 		GlStateManager.color(1F, 1F, 1F, 1F);
 		GlStateManager.translate(0F, -0.5F, 0F);
+		GlStateManager.scale(2F + MathHelper.sin(jellyfish.limbSwing * 1.0F) * jellyfish.limbSwingAmount * 2F, 1F - MathHelper.sin(jellyfish.limbSwing * 1.0F) * jellyfish.limbSwingAmount, 2F + MathHelper.sin(jellyfish.limbSwing * 1.0F)  * jellyfish.limbSwingAmount * 2F);
 		MODEL.render(jellyfish, jellyfish.limbSwing, jellyfish.limbSwingAmount, jellyfish.ticksExisted, 0F, 0F, 0.0625F);
 		GlStateManager.depthMask(true);
 		GlStateManager.disableBlend();
