1c42a29a00439e3a4d6494e88c94565308ef776e
TheCyberBrick <samuelboerlin@bluewin.ch>
2020-10-16 22:27:21 +0200

    Jellofooosh! Rendering stuff

--- src/main/java/thebetweenlands/client/render/entity/RenderJellyfish.java
+++ src/main/java/thebetweenlands/client/render/entity/RenderJellyfish.java
@@ -1,14 +1,19 @@
 package thebetweenlands.client.render.entity;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import thebetweenlands.client.render.model.entity.ModelJellyfish;
+import thebetweenlands.client.render.shader.LightSource;
+import thebetweenlands.client.render.shader.ShaderHelper;
 import thebetweenlands.common.entity.mobs.EntityJellyfish;
+import thebetweenlands.util.LightingUtil;
 
 @SideOnly(Side.CLIENT)
 public class RenderJellyfish extends RenderLiving<EntityJellyfish> {
@@ -18,7 +23,15 @@ public class RenderJellyfish extends RenderLiving<EntityJellyfish> {
 			new ResourceLocation("thebetweenlands:textures/entity/jellyfish_3.png"),
 			new ResourceLocation("thebetweenlands:textures/entity/jellyfish_4.png"),
 			new ResourceLocation("thebetweenlands:textures/entity/jellyfish_5.png")
-			};
+	};
+
+	private static final float[][] GLOW_COLORS = {
+			{238 / 255.0f, 173 / 255.0f, 114 / 255.0f},
+			{180 / 255.0f, 42 / 255.0f, 42 / 255.0f},
+			{224 / 255.0f, 83 / 255.0f, 108 / 255.0f},
+			{90 / 255.0f, 170 / 255.0f, 145 / 255.0f},
+			{215 / 255.0f, 176 / 255.0f, 195 / 255.0f},
+	};
 
 	public final static ModelJellyfish MODEL = new ModelJellyfish();
 
@@ -28,34 +41,90 @@ public class RenderJellyfish extends RenderLiving<EntityJellyfish> {
 
 	@Override
 	protected ResourceLocation getEntityTexture(EntityJellyfish entity) {
-		return TEXTURE[0];
+		return TEXTURE[entity.getJellyfishColour()];
 	}
 
 	@Override
 	public void doRender(EntityJellyfish jellyfish, double x, double y, double z, float entityYaw, float partialTicks) {
-		float smoothedYaw = jellyfish.prevRotationYaw + (jellyfish.rotationYaw - jellyfish.prevRotationYaw) * partialTicks;
-		float smoothedPitch = jellyfish.prevRotationPitch + (jellyfish.rotationPitch - jellyfish.prevRotationPitch) * partialTicks + 90F;
-		float scale = jellyfish.getJellyfishSize();
-		shadowSize = scale * 0.5F;
-		GlStateManager.pushMatrix();
-		GlStateManager.translate(x, y + scale * 0.5F, z);
-		GlStateManager.scale(scale, -scale, -scale);
-		GlStateManager.rotate(smoothedYaw, 0F, 1F, 0F);
-		GlStateManager.rotate(smoothedPitch, 1F, 0F, 0F);
-		bindTexture(TEXTURE[jellyfish.getJellyfishColour()]);
-		GlStateManager.pushMatrix();
+		super.doRender(jellyfish, x, y + jellyfish.height * 0.5f, z, entityYaw, partialTicks);
+
+		if (ShaderHelper.INSTANCE.isWorldShaderActive()) {
+			double interpX = jellyfish.lastTickPosX + (jellyfish.posX - jellyfish.lastTickPosX) * partialTicks;
+			double interpY = jellyfish.lastTickPosY + (jellyfish.posY - jellyfish.lastTickPosY) * partialTicks;
+			double interpZ = jellyfish.lastTickPosZ + (jellyfish.posZ - jellyfish.lastTickPosZ) * partialTicks;
+
+			float str = 5.0f * jellyfish.getJellyfishSize();
+
+			float[] colors = GLOW_COLORS[jellyfish.getJellyfishColour()];
+
+			ShaderHelper.INSTANCE.require();
+			ShaderHelper.INSTANCE.getWorldShader().addLight(new LightSource(interpX, interpY, interpZ, 8.0f, str * colors[0], str * colors[1], str * colors[2]));
+		}
+	}
+
+	@Override
+	public float prepareScale(EntityJellyfish entitylivingbaseIn, float partialTicks) {
+		float scale = entitylivingbaseIn.getJellyfishSize();
+
+		GlStateManager.scale(scale, scale * entitylivingbaseIn.getJellyfishLength(), scale);
+
+		float limbSwingAmount = entitylivingbaseIn.prevLimbSwingAmount + (entitylivingbaseIn.limbSwingAmount - entitylivingbaseIn.prevLimbSwingAmount) * partialTicks;
+		float limbSwing = entitylivingbaseIn.limbSwing - entitylivingbaseIn.limbSwingAmount * (1.0F - partialTicks);
+
+		GlStateManager.scale(
+				1.0f + MathHelper.sin(limbSwing) * Math.min(limbSwingAmount, 0.2f) * 2.0f,
+				1.0f - MathHelper.sin(limbSwing) * Math.min(limbSwingAmount, 0.2f),
+				1.0f + MathHelper.sin(limbSwing) * Math.min(limbSwingAmount, 0.2f) * 2.0f
+				);
+
+		return super.prepareScale(entitylivingbaseIn, partialTicks);
+	}
+
+	@Override
+	protected void renderModel(EntityJellyfish entitylivingbaseIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor) {
 		GlStateManager.enableBlend();
 		GlStateManager.enableAlpha();
-		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-		GlStateManager.depthMask(!jellyfish.isInvisible());
 		GlStateManager.color(1F, 1F, 1F, 1F);
-		GlStateManager.translate(0F, -0.5F, 0F);
-		GlStateManager.scale(2F + MathHelper.sin(jellyfish.limbSwing * 1.0F) * jellyfish.limbSwingAmount * 2F, 1F - MathHelper.sin(jellyfish.limbSwing * 1.0F) * jellyfish.limbSwingAmount, 2F + MathHelper.sin(jellyfish.limbSwing * 1.0F)  * jellyfish.limbSwingAmount * 2F);
-		MODEL.render(jellyfish, jellyfish.limbSwing, jellyfish.limbSwingAmount, jellyfish.ticksExisted, 0F, 0F, 0.0625F);
+		GlStateManager.enableCull();
+
+		LightingUtil.INSTANCE.setLighting(255);
+
+		GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+		GlStateManager.depthMask(false);
+		Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
+
+		super.renderModel(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+
+		Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
 		GlStateManager.depthMask(true);
+		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+
+		super.renderModel(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+
+		LightingUtil.INSTANCE.revert();
+
+		GlStateManager.disableCull();
+		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
 		GlStateManager.disableBlend();
-		GlStateManager.popMatrix();
-		GlStateManager.popMatrix();
-		MODEL.setLivingAnimations(jellyfish, jellyfish.limbSwing, jellyfish.limbSwingAmount, partialTicks);
+	}
+
+	protected static double interpolate(double prev, double now, double partialTicks) {
+		return prev + (now - prev) * partialTicks;
+	}
+
+	@Override
+	protected void applyRotations(EntityJellyfish jellofooosh, float ageInTicks, float rotationYaw, float partialTicks) {
+		Vec3d weightPos = jellofooosh.getOrientationPos(partialTicks);
+
+		double dx = interpolate(jellofooosh.lastTickPosX, jellofooosh.posX, partialTicks) - weightPos.x;
+		double dy = interpolate(jellofooosh.lastTickPosY, jellofooosh.posY, partialTicks) - weightPos.y;
+		double dz = interpolate(jellofooosh.lastTickPosZ, jellofooosh.posZ, partialTicks) - weightPos.z;
+
+		float yaw = -(float)Math.toDegrees(MathHelper.atan2(dz, dx));
+		float pitch = (float)Math.toDegrees(MathHelper.atan2(Math.sqrt(dx * dx + dz * dz), -dy)) - 180;
+
+		GlStateManager.rotate(yaw, 0, 1, 0);
+		GlStateManager.rotate(pitch, 0, 0, 1);
+		GlStateManager.rotate(-yaw, 0, 1, 0);
 	}
 }
--- src/main/java/thebetweenlands/common/entity/mobs/EntityJellyfish.java
+++ src/main/java/thebetweenlands/common/entity/mobs/EntityJellyfish.java
@@ -2,6 +2,7 @@ package thebetweenlands.common.entity.mobs;
 
 import javax.annotation.Nullable;
 
+import io.netty.buffer.ByteBuf;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.IEntityLivingData;
@@ -24,96 +25,113 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
 import thebetweenlands.api.entity.IEntityBL;
 import thebetweenlands.common.registries.BlockRegistry;
 
-public class EntityJellyfish extends EntityCreature implements IEntityBL {
+public class EntityJellyfish extends EntityCreature implements IEntityBL, IEntityAdditionalSpawnData {
+	protected Vec3d prevOrientationPos = Vec3d.ZERO;
+	protected Vec3d orientationPos = Vec3d.ZERO;
 
 	private static final DataParameter<Byte> JELLYFISH_COLOUR = EntityDataManager.createKey(EntityJellyfish.class, DataSerializers.BYTE);
 	private static final DataParameter<Float> JELLYFISH_SIZE = EntityDataManager.<Float>createKey(EntityJellyfish.class, DataSerializers.FLOAT);
 
+	protected float lengthScale = 1.0f;
+
 	public EntityJellyfish(World world) {
 		super(world);
-        setSize(0.8F, 0.8F);
-        moveHelper = new EntityJellyfish.JellyfishMoveHelper(this);
+		setSize(0.8F, 0.8F);
+		moveHelper = new EntityJellyfish.JellyfishMoveHelper(this);
 		setPathPriority(PathNodeType.WALKABLE, -8.0F);
 		setPathPriority(PathNodeType.BLOCKED, -8.0F);
 		setPathPriority(PathNodeType.WATER, 16.0F);
 	}
 
 	@Override
-    protected void initEntityAI() {
+	protected void initEntityAI() {
 		tasks.addTask(3, new EntityAIWander(this, 0.5D, 20));
-        tasks.addTask(2, new EntityAIMoveTowardsRestriction(this, 0.4D));
-    }
-
-    @Override
-    protected void entityInit() {
-        super.entityInit();
-        dataManager.register(JELLYFISH_SIZE, 0.5F);
-        dataManager.register(JELLYFISH_COLOUR, (byte)0);
-    }
+		tasks.addTask(2, new EntityAIMoveTowardsRestriction(this, 0.4D));
+	}
 
 	@Override
-    protected void applyEntityAttributes() {
-        super.applyEntityAttributes();
-        getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.2D);
-        getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(5.0D);
-        getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(12.0D);
-    }
-
-    @Nullable
-	@Override
-    public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata) {
-    	if(!getEntityWorld().isRemote) {
-    		setJellyfishSize(Math.round(Math.max(0.5F, rand.nextFloat()) * 16F) / 16F);
-	        setJellyfishColour((byte)((byte)rand.nextInt(5)));
-    	}
-        return super.onInitialSpawn(difficulty, livingdata);
-    }
+	protected void entityInit() {
+		super.entityInit();
+		dataManager.register(JELLYFISH_SIZE, 0.5F);
+		dataManager.register(JELLYFISH_COLOUR, (byte)0);
+	}
 
 	@Override
-    public float getEyeHeight(){
-        return this.height * 0.5F;
-    }
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.2D);
+		getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(5.0D);
+		getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(12.0D);
+	}
 
-    public float getJellyfishSize() {
-        return dataManager.get(JELLYFISH_SIZE);
-    }
+	@Nullable
+	@Override
+	public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata) {
+		if(!getEntityWorld().isRemote) {
+			setJellyfishSize(0.5f + rand.nextFloat() * 0.75f);
+			setJellyfishLength(0.5f + rand.nextFloat() * 0.5f);
+			setJellyfishColour((byte)((byte)rand.nextInt(5)));
+		}
+		return super.onInitialSpawn(difficulty, livingdata);
+	}
 
-    private void setJellyfishSize(float size) {
-        dataManager.set(JELLYFISH_SIZE, size);
-        setSize(getJellyfishSize() * 0.5F, getJellyfishSize());
-        setPosition(posX, posY, posZ);
-    }
+	@Override
+	public float getEyeHeight(){
+		return this.height * 0.5F;
+	}
+
+	public float getJellyfishSize() {
+		return dataManager.get(JELLYFISH_SIZE);
+	}
+
+	private void setJellyfishSize(float size) {
+		dataManager.set(JELLYFISH_SIZE, size);
+		setSize(getJellyfishSize() * 0.5F, getJellyfishSize());
+		setPosition(posX, posY, posZ);
+	}
+
+	public float getJellyfishLength() {
+		return this.lengthScale;
+	}
+
+	private void setJellyfishLength(float length) {
+		this.lengthScale = length;
+	}
 
 	public byte getJellyfishColour() {
 		return dataManager.get(JELLYFISH_COLOUR);
 	}
 
-    public void setJellyfishColour(byte colour) {
-        dataManager.set(JELLYFISH_COLOUR, colour);
-    }
+	public void setJellyfishColour(byte colour) {
+		dataManager.set(JELLYFISH_COLOUR, colour);
+	}
 
-    public void notifyDataManagerChange(DataParameter<?> key) {
-    	 if (JELLYFISH_SIZE.equals(key)) {
-             setSize(getJellyfishSize(), getJellyfishSize());
-             rotationYaw = rotationYawHead;
-             renderYawOffset = rotationYawHead;
-         }
-        if (JELLYFISH_COLOUR.equals(key)) {
-            setJellyfishColour(getJellyfishColour());
-        }
-        super.notifyDataManagerChange(key);
-    }
- 
+	@Override
+	public void notifyDataManagerChange(DataParameter<?> key) {
+		if (JELLYFISH_SIZE.equals(key)) {
+			setSize(getJellyfishSize(), getJellyfishSize());
+			rotationYaw = rotationYawHead;
+			renderYawOffset = rotationYawHead;
+		}
+		if (JELLYFISH_COLOUR.equals(key)) {
+			setJellyfishColour(getJellyfishColour());
+		}
+		super.notifyDataManagerChange(key);
+	}
+
 	@Override
 	public void writeEntityToNBT(NBTTagCompound nbt) {
 		super.writeEntityToNBT(nbt);
 		nbt.setFloat("jellyfishSize", getJellyfishSize());
+		nbt.setFloat("jellyfishLength", getJellyfishLength());
 		nbt.setByte("jellyfishColour", getJellyfishColour());
 
 	}
@@ -122,55 +140,56 @@ public class EntityJellyfish extends EntityCreature implements IEntityBL {
 	public void readEntityFromNBT(NBTTagCompound nbt) {
 		super.readEntityFromNBT(nbt);
 		setJellyfishSize(nbt.getFloat("jellyfishSize"));
+		setJellyfishLength(nbt.getFloat("jellyfishLength"));
 		setJellyfishColour(nbt.getByte("jellyfishColour"));
 	}
 
 	@Override
-    protected SoundEvent getHurtSound(DamageSource source) {
-        return super.getHurtSound(source);
-    }
-/*
+	protected SoundEvent getHurtSound(DamageSource source) {
+		return super.getHurtSound(source);
+	}
+	/*
     @Override
     protected SoundEvent getDeathSound() {
        return SoundRegistry.JELLYFISH_DEATH;
     }
-*/
-    @Override
-    protected SoundEvent getSwimSound() {
-        return SoundEvents.ENTITY_HOSTILE_SWIM;
-    }
-
-    @Override
-    protected float getSoundVolume() {
-        return 0.4F;
-    }
-
-    @Nullable
-    @Override
-    protected ResourceLocation getLootTable() {
-        return null;//LootTableRegistry.JELLYFISH;
-    }
-
-    @Override
-    public boolean getCanSpawnHere() {
-        return world.getDifficulty() != EnumDifficulty.PEACEFUL && world.getBlockState(new BlockPos(MathHelper.floor(posX), MathHelper.floor(posY), MathHelper.floor(posZ))).getBlock() == BlockRegistry.SWAMP_WATER;
-    }
-
-    public boolean isGrounded() {
-        return !isInWater() && world.isAirBlock(new BlockPos(MathHelper.floor(posX), MathHelper.floor(posY + 1), MathHelper.floor(posZ))) && world.getBlockState(new BlockPos(MathHelper.floor(posX), MathHelper.floor(posY - 1), MathHelper.floor(posZ))).getBlock().isCollidable();
-    }
+	 */
+	@Override
+	protected SoundEvent getSwimSound() {
+		return SoundEvents.ENTITY_HOSTILE_SWIM;
+	}
 
 	@Override
-    protected PathNavigate createNavigator(World world){
-        return new PathNavigateSwimmer(this, world);
-    }
+	protected float getSoundVolume() {
+		return 0.4F;
+	}
 
-    @Override
-    public float getBlockPathWeight(BlockPos pos) {
-        return world.getBlockState(pos).getMaterial() == Material.WATER ? 10.0F + world.getLightBrightness(pos) - 0.5F : super.getBlockPathWeight(pos);
-    }
+	@Nullable
+	@Override
+	protected ResourceLocation getLootTable() {
+		return null;//LootTableRegistry.JELLYFISH;
+	}
 
-    @Override
+	@Override
+	public boolean getCanSpawnHere() {
+		return world.getDifficulty() != EnumDifficulty.PEACEFUL && world.getBlockState(new BlockPos(MathHelper.floor(posX), MathHelper.floor(posY), MathHelper.floor(posZ))).getBlock() == BlockRegistry.SWAMP_WATER;
+	}
+
+	public boolean isGrounded() {
+		return !isInWater() && world.isAirBlock(new BlockPos(MathHelper.floor(posX), MathHelper.floor(posY + 1), MathHelper.floor(posZ))) && world.getBlockState(new BlockPos(MathHelper.floor(posX), MathHelper.floor(posY - 1), MathHelper.floor(posZ))).getBlock().isCollidable();
+	}
+
+	@Override
+	protected PathNavigate createNavigator(World world){
+		return new PathNavigateSwimmer(this, world);
+	}
+
+	@Override
+	public float getBlockPathWeight(BlockPos pos) {
+		return world.getBlockState(pos).getMaterial() == Material.WATER ? 10.0F + world.getLightBrightness(pos) - 0.5F : super.getBlockPathWeight(pos);
+	}
+
+	@Override
 	public void onLivingUpdate() {	
 		if (inWater) {
 			setAir(300);
@@ -183,71 +202,91 @@ public class EntityJellyfish extends EntityCreature implements IEntityBL {
 		float speedAngle = MathHelper.sqrt(motionX * motionX + motionZ * motionZ);
 		if(motionX != 0D && motionZ != 0D)
 			rotationPitch += (-((float)MathHelper.atan2((double)speedAngle, motionY)) * (180F / (float)Math.PI) - rotationPitch) * 0.1F;
-			
+
 		super.onLivingUpdate();
 	}
 
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
-		 if (getMoveHelper().isUpdating() && motionY < 0) //only way I can figure to stop the pathfinding messing up rendering rotations when looking for a new path atm
-			 rotationYaw = 0F;
+
+		if(this.world.isRemote) {
+			this.updateOrientationPos();
+		}
 	}
 
-    @Override
-    public void travel(float strafe, float up, float forward) {
-        if (isServerWorld()) {
-            if (isInWater()) {
-                moveRelative(strafe, up, forward, 0.075F);
-                move(MoverType.SELF, motionX, motionY, motionZ);
+	protected void updateOrientationPos() {
+		final double length = 0.5D;
+
+		Vec3d tether = this.getPositionVector();
+
+		this.prevOrientationPos = this.orientationPos;
+
+		this.orientationPos = this.orientationPos.add(0, -0.01f, 0);
+
+		if(this.orientationPos.distanceTo(tether) > length) {
+			this.orientationPos = tether.add(this.orientationPos.subtract(tether).normalize().scale(length));
+		}
+	}
+
+	public Vec3d getOrientationPos(float partialTicks) {
+		return this.prevOrientationPos.add(this.orientationPos.subtract(this.prevOrientationPos).scale(partialTicks));
+	}
+
+	@Override
+	public void travel(float strafe, float up, float forward) {
+		if (isServerWorld()) {
+			if (isInWater()) {
+				moveRelative(strafe, up, forward, 0.075F);
+				move(MoverType.SELF, motionX, motionY, motionZ);
 				motionX *= 0.75D;
 				motionY *= 0.75D;
 				motionZ *= 0.75D;
 
-                if (getAttackTarget() == null) {
-                    motionY -= 0.003D;
-                }
-            } else {
-                super.travel(strafe, up, forward);
-            }
-        } else {
-            super.travel(strafe, up, forward);
-        }
-    }
+				if (getAttackTarget() == null) {
+					motionY -= 0.003D;
+				}
+			} else {
+				super.travel(strafe, up, forward);
+			}
+		} else {
+			super.travel(strafe, up, forward);
+		}
+	}
 
 	@Override
-    public boolean isNotColliding() {
-		 return getEntityWorld().checkNoEntityCollision(getEntityBoundingBox(), this) && getEntityWorld().getCollisionBoxes(this, getEntityBoundingBox()).isEmpty();
-    }
+	public boolean isNotColliding() {
+		return getEntityWorld().checkNoEntityCollision(getEntityBoundingBox(), this) && getEntityWorld().getCollisionBoxes(this, getEntityBoundingBox()).isEmpty();
+	}
 
-    @Override
-    public boolean isPushedByWater() {
-        return false;
-    }
+	@Override
+	public boolean isPushedByWater() {
+		return false;
+	}
 
-    static class JellyfishMoveHelper extends EntityMoveHelper {
-        private final EntityJellyfish jellyfish;
+	static class JellyfishMoveHelper extends EntityMoveHelper {
+		private final EntityJellyfish jellyfish;
 
-        public JellyfishMoveHelper(EntityJellyfish jellyfishIn) {
-            super(jellyfishIn);
-            this.jellyfish= jellyfishIn;
-        }
+		public JellyfishMoveHelper(EntityJellyfish jellyfishIn) {
+			super(jellyfishIn);
+			this.jellyfish= jellyfishIn;
+		}
 
-        @Override
+		@Override
 		public void onUpdateMoveHelper() {
-            if (action == EntityMoveHelper.Action.MOVE_TO && !jellyfish.getNavigator().noPath()) {
-                double targetX = posX - jellyfish.posX;
-                double targetY = posY - jellyfish.posY;
-                double targetZ = posZ - jellyfish.posZ;
-                double targetDistance = targetX * targetX + targetY * targetY + targetZ * targetZ;
-                targetDistance = (double) MathHelper.sqrt(targetDistance);
-                targetY = targetY / targetDistance;
-                float targetAngle = (float) (MathHelper.atan2(targetZ, targetX) * (180D / Math.PI)) - 90.0F;
-                jellyfish.rotationYaw = limitAngle(jellyfish.rotationYaw, targetAngle, 180.0F);
-                jellyfish.renderYawOffset = jellyfish.rotationYaw;
-                float travelSpeed = (float) (speed * jellyfish.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue());
-                jellyfish.setAIMoveSpeed(jellyfish.getAIMoveSpeed() + (travelSpeed - jellyfish.getAIMoveSpeed()) * 0.125F);
-                jellyfish.motionY += (double) jellyfish.getAIMoveSpeed() * targetY * 0.2D;
+			if (action == EntityMoveHelper.Action.MOVE_TO && !jellyfish.getNavigator().noPath()) {
+				double targetX = posX - jellyfish.posX;
+				double targetY = posY - jellyfish.posY;
+				double targetZ = posZ - jellyfish.posZ;
+				double targetDistance = targetX * targetX + targetY * targetY + targetZ * targetZ;
+				targetDistance = (double) MathHelper.sqrt(targetDistance);
+				targetY = targetY / targetDistance;
+				float targetAngle = (float) (MathHelper.atan2(targetZ, targetX) * (180D / Math.PI)) - 90.0F;
+				jellyfish.rotationYaw = limitAngle(jellyfish.rotationYaw, targetAngle, 180.0F);
+				jellyfish.renderYawOffset = jellyfish.rotationYaw;
+				float travelSpeed = (float) (speed * jellyfish.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue());
+				jellyfish.setAIMoveSpeed(jellyfish.getAIMoveSpeed() + (travelSpeed - jellyfish.getAIMoveSpeed()) * 0.125F);
+				jellyfish.motionY += (double) jellyfish.getAIMoveSpeed() * targetY * 0.2D;
 				EntityLookHelper entitylookhelper = jellyfish.getLookHelper();
 				double targetDirectionX = jellyfish.posX + targetX / targetDistance * 2.0D;
 				double targetDirectionY = (double) jellyfish.getEyeHeight() + jellyfish.posY + targetY / targetDistance;
@@ -256,17 +295,27 @@ public class EntityJellyfish extends EntityCreature implements IEntityBL {
 				double lookY = entitylookhelper.getLookPosY();
 				double lookZ = entitylookhelper.getLookPosZ();
 
-                if (!entitylookhelper.getIsLooking()) {
-                	lookX = targetDirectionX;
-                	lookY = targetDirectionY;
-                	lookZ = targetDirectionZ;
-                }
+				if (!entitylookhelper.getIsLooking()) {
+					lookX = targetDirectionX;
+					lookY = targetDirectionY;
+					lookZ = targetDirectionZ;
+				}
 
-                jellyfish.getLookHelper().setLookPosition(lookX + (targetDirectionX - lookX) * 0.125D, lookY + (targetDirectionY - lookY) * 0.125D, lookZ + (targetDirectionZ - lookZ) * 0.125D, 10.0F, 40.0F);
-            
-            } else {
-            	jellyfish.setAIMoveSpeed(0.0F);
-           }
-        }
-    }
+				jellyfish.getLookHelper().setLookPosition(lookX + (targetDirectionX - lookX) * 0.125D, lookY + (targetDirectionY - lookY) * 0.125D, lookZ + (targetDirectionZ - lookZ) * 0.125D, 10.0F, 40.0F);
+
+			} else {
+				jellyfish.setAIMoveSpeed(0.0F);
+			}
+		}
+	}
+
+	@Override
+	public void writeSpawnData(ByteBuf buf) {
+		buf.writeFloat(this.lengthScale);
+	}
+
+	@Override
+	public void readSpawnData(ByteBuf buf) {
+		this.lengthScale = buf.readFloat();
+	}
 }
