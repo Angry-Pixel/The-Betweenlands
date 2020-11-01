6856d401d88e3b2e92bd5df74ff5ac6b760fb659
TheCyberBrick <samuelboerlin@bluewin.ch>
2020-11-01 17:15:11 +0100

    Attempt at fixing dev build descriptions #2

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -9,11 +9,36 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	return 0
   fi
   
+  #Check release type (release or dev build)
+  if [[ "$TRAVIS_TAG" == *"release"* ]]; then
+    release_type="release"
+	releaseTitle="Release Build ${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}"
+  else
+    release_type="development"
+	release_title="Development Build ${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}"
+  fi
+  
+  #Create release notes
+  rm -f release_notes
+
+  echo "Commit: https://github.com/Angry-Pixel/The-Betweenlands/commit/${TRAVIS_COMMIT}" >> release_notes
+  echo "" >> release_notes
+
+  if [[ "$release_type" == "release" ]]; then
+    #Get latest release tag and then list commits since that release as release notes
+    latest_release_tag=$(git describe --tags $(git rev-list --tags --max-count=1) --match *-release)
+    git log ${latest_release_tag}..HEAD --pretty=format:'%an, %ar:%n%B' --no-merges >> release_notes
+  else
+    #Use latest commit message as release note
+    git log -1 --pretty=format:'%an, %ar:%n%B' >> release_notes
+  fi
+  
   #Authenticate
   eval "$(ssh-agent -s)"
   chmod 600 deploy_key.pem
   ssh-add deploy_key.pem
   
+  #Clone dev build repo
   mkdir repo
   
   git clone --depth 1 $REPOSITORY_SSH_URI repo
@@ -25,51 +50,32 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   
   cd repo || return 0
   
+  #Set up git for deployment
   git config --local user.name "Travis Build Wizard"
   git config --local user.email "<>"
   git remote add deployment $REPOSITORY_SSH_URI
   git remote -v
   
+  #Create build infos
   rm -f build
-  
-  if [[ "$TRAVIS_TAG" == *"release"* ]]; then
-    releaseType="release"
-	releaseTitle="Release Build ${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}"
-  else
-    releaseType="development"
-	releaseTitle="Development Build ${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}"
-  fi
   
   cat <<EOT >> build
 [build number]:
 $(sed 's/\:/\\:/g' <<< "${TRAVIS_BUILD_NUMBER}")
 [type]:
-$(sed 's/\:/\\:/g' <<< "${releaseType}")
+$(sed 's/\:/\\:/g' <<< "${release_type}")
 [title]:
-$(sed 's/\:/\\:/g' <<< "${releaseTitle}")
+$(sed 's/\:/\\:/g' <<< "${release_title}")
 [branch]:
 $(sed 's/\:/\\:/g' <<< "${TRAVIS_BRANCH}")
 [commit]:
 $(sed 's/\:/\\:/g' <<< "${TRAVIS_COMMIT}")
 EOT
 
-  rm -f release_notes
-
-  echo "Commit: https://github.com/Angry-Pixel/The-Betweenlands/commit/${TRAVIS_COMMIT}" >> release_notes
-  echo "" >> release_notes
-
-  if [[ "$releaseType" == "release" ]]; then
-    #Get latest release tag and then list commits since that release as release notes
-    latest_release_tag=$(git describe --tags $(git rev-list --tags --max-count=1) --match *-release)
-    git log ${latest_release_tag}..HEAD --pretty=format:'%an, %ar:%n%B' --no-merges >> release_notes
-  else
-    #Use latest commit message as release note
-    git log -1 --pretty=format:'%an, %ar:%n%B' >> release_notes
-  fi
-
+  #Push to dev build repo
   git add release_notes
   git add build
-  git commit -m "${releaseTitle}"
+  git commit -m "${release_title}"
   git push deployment master
   
   cd -
