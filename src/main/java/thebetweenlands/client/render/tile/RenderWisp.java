247facfec6a50123e8a60e65392c95d43a9cc4f3
TheCyberBrick <samuelboerlin@bluewin.ch>
2020-11-08 20:02:34 +0100

    Readded coloured lights to wisps

--- src/main/java/thebetweenlands/client/render/tile/RenderWisp.java
+++ src/main/java/thebetweenlands/client/render/tile/RenderWisp.java
@@ -7,6 +7,8 @@ import thebetweenlands.client.render.particle.BLParticles;
 import thebetweenlands.client.render.particle.BatchedParticleRenderer;
 import thebetweenlands.client.render.particle.DefaultParticleBatches;
 import thebetweenlands.client.render.particle.ParticleFactory.ParticleArgs;
+import thebetweenlands.client.render.shader.LightSource;
+import thebetweenlands.client.render.shader.ShaderHelper;
 import thebetweenlands.common.block.terrain.BlockWisp;
 import thebetweenlands.common.config.BetweenlandsConfig;
 import thebetweenlands.common.tile.TileEntityWisp;
@@ -28,11 +30,11 @@ public class RenderWisp extends TileEntitySpecialRenderer<TileEntityWisp> {
 			}
 		}
 
+		int colorIndex = StatePropertyHelper.getStatePropertySafely(tileEntity, BlockWisp.class, BlockWisp.COLOR, 0);
+
 		if(!Minecraft.getMinecraft().isGamePaused()) {
 			if(System.nanoTime() - ((TileEntityWisp)tileEntity).lastSpawn >= (500f - 500.0f * BetweenlandsConfig.RENDERING.wispQuality / 150.0f) * 1000000L) {
 				((TileEntityWisp)tileEntity).lastSpawn = System.nanoTime();
-
-				int colorIndex = StatePropertyHelper.getStatePropertySafely(tileEntity, BlockWisp.class, BlockWisp.COLOR, 0);
 
 				int color = BlockWisp.COLORS[colorIndex * 2];
 				float r = (color >> 16 & 0xFF) / 255F;
@@ -55,6 +57,25 @@ public class RenderWisp extends TileEntitySpecialRenderer<TileEntityWisp> {
 						y + 0.5 + renderViewY, 
 						z + 0.5 + renderViewZ, 
 						ParticleArgs.get().withColor(r, g, b, 1.0F).withScale(2.0F)));
+			}
+		}
+
+		if(ShaderHelper.INSTANCE.isWorldShaderActive()) {
+			ShaderHelper.INSTANCE.require();
+			
+			for(int i = 0; i < 2; i++) {
+				int color = BlockWisp.COLORS[colorIndex * 2 + i];
+				float r = (color >> 16 & 0xFF) / 255F;
+				float g = (color >> 8 & 0xFF) / 255F;
+				float b = (color & 0xFF) / 255F;
+
+				float size = 1.5f;
+
+				ShaderHelper.INSTANCE.getWorldShader().addLight(new LightSource(tileEntity.getPos().getX() + 0.5f, tileEntity.getPos().getY(), tileEntity.getPos().getZ() + 0.5f,
+						i == 0 ? size : size * 0.5F,
+								r * (i == 0 ? 6 : 3),
+								g * (i == 0 ? 6 : 3),
+								b * (i == 0 ? 6 : 3)));
 			}
 		}
 	}
