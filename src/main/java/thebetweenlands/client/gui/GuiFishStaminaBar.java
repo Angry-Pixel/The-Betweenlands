8899a1de4e942169a6a8950d2c703c572c1a8e59
vadis365 <curve_linear@hotmail.com>
2020-12-29 15:51:05 +0000

    helps to save before pushing - whoops

--- src/main/java/thebetweenlands/client/gui/GuiFishStaminaBar.java
+++ src/main/java/thebetweenlands/client/gui/GuiFishStaminaBar.java
@@ -77,12 +77,12 @@ public class GuiFishStaminaBar extends Gui {
 				stencil.func(GL11.GL_EQUAL, true);
 				stencil.op(GL11.GL_KEEP);
 			}
-			
+
 			if (hasTreasure)
 				drawTexturedModalRect(posX - treasureTick, posY + 1, treasureUnlocked ? 16 : 0, 128, 16, 16); // chest
 
 			drawHangingRope(staminaTicks, posX - staminaTicks + 7, posY + 12, posX + 256 + 16, posY, 0.5F, 0D); // line
-			
+
 			mc.renderEngine.bindTexture(GUI_TEXTURE); // because depth // stuffs :p
 
 			drawTexturedModalRect(posX - staminaTicks - 8, posY + 1, 0 + aniFrame, 48, 16, 16); // fish
@@ -93,7 +93,7 @@ public class GuiFishStaminaBar extends Gui {
 			drawTexturedModalRect(posX - obstructionTicks4 - 8, posY + 2, 0 + aniFrame, 112, 16, 16); // jolly fush
 			GlStateManager.disableBlend();
 			GlStateManager.popMatrix();
-			
+
 			drawTexturedModalRect(posX - escapeTicks - 8, posY + 2, 0 + (getCrabScroll(escapeTicks) * 16), 144, 16, 16); // crab
 			drawTexturedModalRect(posX - obstructionTicks1 - 8, posY, 0 + aniFrame, 64, 16, 16); // weed
 			drawTexturedModalRect(posX - obstructionTicks3 - 8, posY, 0 + aniFrame, 96, 16, 16); // coral
