8e8cc78bc39a033b90b9dd2510db0e0f841fc310
vadis365 <curve_linear@hotmail.com>
2021-01-09 16:42:48 +0000

    urchin size and air

--- src/main/java/thebetweenlands/common/entity/mobs/EntityFreshwaterUrchin.java
+++ src/main/java/thebetweenlands/common/entity/mobs/EntityFreshwaterUrchin.java
@@ -2,10 +2,8 @@ package thebetweenlands.common.entity.mobs;
 
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
 import net.minecraft.entity.ai.EntityAIWander;
-import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.world.World;
@@ -15,15 +13,14 @@ public class EntityFreshwaterUrchin extends EntityCreature implements IEntityBL 
 
 	public EntityFreshwaterUrchin(World world) {
 		super(world);
-		this.setPathPriority(PathNodeType.WATER, 4.0F);
+		setSize(0.7F, 0.5F);
+		setPathPriority(PathNodeType.WATER, 4.0F);
 	}
 
 	@Override
 	protected void initEntityAI() {
-		this.tasks.addTask(0, new EntityAIPanic(this, 1.0D));
-		this.tasks.addTask(1, new EntityAIWander(this, 1.0D, 40));
-		this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
-		this.tasks.addTask(3, new EntityAILookIdle(this));
+		tasks.addTask(0, new EntityAIAvoidEntity<EntityPlayer>(this, EntityPlayer.class, 10.0F, 1D, 1D));
+		tasks.addTask(1, new EntityAIWander(this, 1.0D, 40));
 	}
 
 	@Override
@@ -35,12 +32,19 @@ public class EntityFreshwaterUrchin extends EntityCreature implements IEntityBL 
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
 		getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(3.0D);
-		getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.05D);
+		getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.15D);
 	}
 
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
 	}
-	
+
+	@Override
+	public void onLivingUpdate() {
+		super.onLivingUpdate();
+		if (inWater)
+			setAir(300);
+	}
+
 }
