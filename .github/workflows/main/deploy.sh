07880c18c494b22f4682449289a9abf7efbbbecf
TheCyberBrick <samuelboerlin@bluewin.ch>
2021-02-27 15:00:46 +0100

    no message

--- .github/workflows/main/deploy.sh
+++ .github/workflows/main/deploy.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-
-# Sets up the deployment enviroment, i.e. cloning the deployment repository and setting up all of its files
--- .github/workflows/main/setup.sh
+++ .github/workflows/main/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-# Sets up the deployment enviroment, i.e. cloning the deployment repository and setting up all of its files
+# Sets up the deployment enviroment, i.e. environment variables and cloning the deployment repository and setting up all of its files
+
+chmod +x .github/workflows/main/config/vars.sh
+./.github/workflows/main/config/vars.sh
