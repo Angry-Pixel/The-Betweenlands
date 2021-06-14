29eaf4d0c05427f887116cbe372feab29fab2d20
vadis365 <6518708+vadis365@users.noreply.github.com>
2021-06-14 20:49:16 +0100

    no fucking idea

--- src/main/java/thebetweenlands/common/block/misc/BlockFilteredSiltGlass.java
+++ src/main/java/thebetweenlands/common/block/misc/BlockFilteredSiltGlass.java
@@ -1,29 +1,36 @@
 package thebetweenlands.common.block.misc;
 
-import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.property.ExtendedBlockState;
+import net.minecraftforge.common.property.IExtendedBlockState;
+import net.minecraftforge.common.property.IUnlistedProperty;
 import thebetweenlands.common.block.IConnectedTextureBlock;
 
-public class BlockFilteredSiltGlass extends BlockPaneBetweenlands implements IConnectedTextureBlock {
-	
+public class BlockFilteredSiltGlass extends BlockGlassBetweenlands implements IConnectedTextureBlock {
+
 	public BlockFilteredSiltGlass() {
-		super(Material.GLASS, true);
-		this.setSoundType(SoundType.GLASS);
-		this.setHardness(0.3F);
+		super(Material.GLASS);
 	}
 
-	@SideOnly(Side.CLIENT)
-	@Override
-	public BlockRenderLayer getRenderLayer() {
-		return BlockRenderLayer.TRANSLUCENT;
-	}
-	
 	@Override
 	public boolean canRenderInLayer(IBlockState state, BlockRenderLayer layer) {
 		return layer == BlockRenderLayer.TRANSLUCENT || layer == BlockRenderLayer.CUTOUT_MIPPED;
 	}
+
+    @Override
+    public IBlockState getExtendedState(IBlockState oldState, IBlockAccess worldIn, BlockPos pos) {
+        IExtendedBlockState state = (IExtendedBlockState) oldState;
+        return this.getExtendedConnectedTextureState(state, worldIn, pos, p -> worldIn.getBlockState(p).getBlock() instanceof BlockFilteredSiltGlass, false);
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return this.getConnectedTextureBlockStateContainer(new ExtendedBlockState(this, new IProperty[]{}, new IUnlistedProperty[0]));
+    }
 }
