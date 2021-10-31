a91a58626e26cc81492f53177bdc3d470a2d3b16
vadis365 <6518708+vadis365@users.noreply.github.com>
2021-10-31 18:27:49 +0000

    Crab Pot and Filter now drop blocks

--- src/main/java/thebetweenlands/common/block/container/BlockCrabPot.java
+++ src/main/java/thebetweenlands/common/block/container/BlockCrabPot.java
@@ -180,6 +180,11 @@ public class BlockCrabPot extends BlockSwampWater implements ITileEntityProvider
     	return BlockFaceShape.UNDEFINED;
     }
 
+    @Override
+    public int quantityDropped(Random random) {
+        return 1;
+    }
+
 	@Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
         return Item.getItemFromBlock(this);
