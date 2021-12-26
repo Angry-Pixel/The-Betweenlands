0e50bf2c3c1a74b54330ef1ff11d265afae01498
vadis365 <6518708+vadis365@users.noreply.github.com>
2021-12-26 16:24:38 +0000

    keeping it consistent

--- src/main/java/thebetweenlands/common/registries/FluidRegistry.java
+++ src/main/java/thebetweenlands/common/registries/FluidRegistry.java
@@ -43,7 +43,7 @@ public class FluidRegistry {
 	public static final Fluid RUBBER = new Fluid("rubber", new ResourceLocation("thebetweenlands:fluids/rubber_still"), new ResourceLocation("thebetweenlands:fluids/rubber_flowing")).setDensity(1200).setViscosity(1500);
 	public static final Fluid FOG = new Fluid("fog", new ResourceLocation("thebetweenlands:fluids/fog"), new ResourceLocation("thebetweenlands:fluids/fog")).setDensity(2).setViscosity(10).setGaseous(true);
 	public static final Fluid SHALLOWBREATH = new Fluid("shallowbreath", new ResourceLocation("thebetweenlands:fluids/shallowbreath"), new ResourceLocation("thebetweenlands:fluids/shallowbreath")).setDensity(2).setViscosity(10).setGaseous(true);
-	public static final Fluid BL_DYE = new Fluid("dyed_water", new ResourceLocation("thebetweenlands:fluids/dyed_water_still"), new ResourceLocation("thebetweenlands:fluids/dyed_water_flowing")).setDensity(1000).setViscosity(1000);
+	public static final Fluid DYED_WATER = new Fluid("dyed_water", new ResourceLocation("thebetweenlands:fluids/dyed_water_still"), new ResourceLocation("thebetweenlands:fluids/dyed_water_flowing")).setDensity(1000).setViscosity(1000);
 
 	public static final List<Fluid> REGISTERED_FLUIDS = new ArrayList<Fluid>();
 
