212a81716d3ca915c4ef9cd1c6408d8e80943213
Andromander <androsa@protonmail.com>
2024-06-20 22:54:13 +1000

    Dirty move some events to ClientEvents
    Enforces sidedness

---
+++ src/main/java/thebetweenlands/client/ClientEvents.java
@@ -0,0 +1,154 @@
+package thebetweenlands.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.*;
+import thebetweenlands.client.gui.AmateMapRenderer;
+import thebetweenlands.client.rendering.BetweenlandsSkyShaderHandler;
+import thebetweenlands.client.rendering.entitys.RenderGecko;
+import thebetweenlands.client.rendering.entitys.RenderSwampHag;
+import thebetweenlands.client.rendering.entitys.RenderWight;
+import thebetweenlands.client.rendering.model.entity.ModelGecko;
+import thebetweenlands.client.rendering.model.entity.ModelSwampHag;
+import thebetweenlands.client.rendering.model.entity.ModelWight;
+import thebetweenlands.client.rendering.shader.BetweenlandsShaders;
+import thebetweenlands.client.rendering.shader.BetweenlandsSkyShaderInstance;
+import thebetweenlands.common.TheBetweenlands;
+import thebetweenlands.common.particles.BetweenlandsParticle;
+import thebetweenlands.common.particles.BetweenlandsPortalParticle;
+import thebetweenlands.common.registries.*;
+
+//Class for events relying on the client side
+public class ClientEvents {
+    public static BetweenlandsSkyShaderHandler skyTextureHandler;
+
+    public static void initClient(IEventBus eventbus) {
+        eventbus.addListener(ClientEvents::doClientStuff);
+        eventbus.addListener(ClientEvents::registerShaders);
+        eventbus.addListener(ClientEvents::registerLayerDefinition);
+        eventbus.addListener(ClientEvents::particleStuff);
+        eventbus.addListener(ClientEvents::registerBlockColors);
+        eventbus.addListener(ClientEvents::betweenlandsAmbienceHandler);
+    }
+
+    private static void doClientStuff(final FMLClientSetupEvent event) {
+        // Client only setup
+
+        // Amate Map renderer
+        TheBetweenlands.amateMapRenderer = new AmateMapRenderer(Minecraft.getInstance().textureManager);
+
+        // Entity renderers
+        EntityRenderers.register(EntityRegistry.SWAMP_HAG.get(), RenderSwampHag::new);
+        EntityRenderers.register(EntityRegistry.GECKO.get(), RenderGecko::new);
+        EntityRenderers.register(EntityRegistry.WIGHT.get(), RenderWight::new);
+
+
+        // Compile all rift textures into an atlas
+        TheBetweenlands.LOGGER.info("Building rift texture atlas");
+        skyTextureHandler = new BetweenlandsSkyShaderHandler(true);
+    }
+
+    private static void registerShaders(final RegisterShadersEvent event) {
+        //BetweenlandsShaderInstance betweenlandssky = new BetweenlandsShaderInstance(event.getResourceManager(), new ResourceLocation(Betweenlands.ID, "starfield"), BetweenlandsVertexFormats.BETWEENLANDS_SKY);
+
+        // todo: sky shader "starfield" name is being changed to "betweenlandsSky"
+        BetweenlandsShaders.preloadShaders(event.getResourceManager());
+        event.registerShader(BetweenlandsShaders.BetweenlandsSky, BetweenlandsSkyShaderInstance.onLoad);
+    }
+
+    private static void registerLayerDefinition(final EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(RenderSwampHag.SWAMP_HAG_MODEL_LAYER, ModelSwampHag::createModelLayer);
+        event.registerLayerDefinition(RenderGecko.GECKO_MODEL_LAYER, ModelGecko::createModelLayer);
+        event.registerLayerDefinition(RenderWight.WIGHT_MODEL_LAYER, ModelWight::createModelLayer);
+    }
+
+    private static void particleStuff(final RegisterParticleProvidersEvent event) {
+        event.registerSpriteSet(ParticleRegistry.SULFUR_GENERIC.get(), BetweenlandsParticle.Helper::new);
+        event.registerSpriteSet(ParticleRegistry.PORTAL_EFFECT.get(), BetweenlandsPortalParticle.Helper::new);
+    }
+
+    public static void registerBlockColors(RegisterColorHandlersEvent.Block event) {
+        // Changes particle and block colors
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.LEAVES_WEEDWOOD_TREE.get());
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.LEAVES_NIBBLETWIG_TREE.get());
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.LEAVES_RUBBER_TREE.get());
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_GRASS, BlockRegistry.SWAMP_GRASS.get());
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.POISON_IVY.get());
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_GRASS, BlockRegistry.SWAMP_REED.get());
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.MOSS.get());
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.HANGER.get());
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_GRASS, BlockRegistry.SWAMP_TALLGRASS.get());
+
+        // Only effects particle colors
+        event.getBlockColors().register(BlockColorRegistry.SWAMP_WATER, BlockRegistry.SWAMP_WATER_BLOCK.get());
+    }
+
+    // Ticks every render tick
+    // This is some very old code
+    public static void betweenlandsAmbienceHandler(final ClientTickEvent event) {
+        // Get time for shaders
+        //FractinalTime += 1 / Minecraft.getInstance().getFrameTime();
+        //Time = (int) FractinalTime;
+
+        // Init vars
+        Player player = Minecraft.getInstance().player;
+        if (player!=null) {
+            ClientLevel level = Minecraft.getInstance().level;
+            //double x = player.getX(), y = player.getY(), z = player.getZ();
+            //BlockPos blockpos = new BlockPos(x,y,z);
+            BlockPos eyeblockpos = player.eyeBlockPosition();
+            //render.render
+            //LOGGER.info(ModFluids.SWAMP_WATER_FLOW.get().getTags());
+
+            // TODO: replace with a more professional solution
+
+            if (level.dimension() == DimensionRegistries.BETWEENLANDS_DIMENSION_KEY) {
+
+                rotation += 0.001f;
+
+                // Sound
+                if (player.isEyeInFluid(FluidTags.WATER) && level.getFluidState(eyeblockpos).getType().isSame(FluidRegistry.SWAMP_WATER_STILL.get().getSource())) {
+                    // Water state
+                    if (loopstate!=3) {
+                        loopstate = 3;
+                        soundManager.stopLoopedSound();
+                    }
+                    soundManager.playLoopedSound(SoundRegistry.BETWEENLANDS_AMBIENT_WATER_LOOP.get());
+                }
+                else if (eyeblockpos.getY() <= 47)
+                {
+                    // Cave state
+                    if (loopstate!=2) {
+                        loopstate = 2;
+                        soundManager.stopLoopedSound();
+                    }
+                    soundManager.playLoopedSound(SoundRegistry.BETWEENLANDS_AMBIENT_CAVES_LOOP.get());
+                }
+                else
+                {
+                    // Default state
+                    if (loopstate!=1) {
+                        loopstate = 1;
+                        soundManager.stopLoopedSound();
+                    }
+                    soundManager.playLoopedSound(SoundRegistry.BETWEENLANDS_AMBIENT_SWAMP_LOOP.get());
+                }
+            }
+            else {
+
+                // Sound
+                // Off state
+                if (loopstate!=0) {
+                    loopstate = 0;
+                    soundManager.stopLoopedSound();
+                }
+            }
+        }
+    }
+}
--- src/main/java/thebetweenlands/common/TheBetweenlands.java
+++ src/main/java/thebetweenlands/common/TheBetweenlands.java
@@ -1,47 +1,19 @@
 package thebetweenlands.common;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.*;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoader;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.TickEvent.ClientTickEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
+import thebetweenlands.client.ClientEvents;
 import thebetweenlands.client.gui.AmateMapRenderer;
 import thebetweenlands.client.rendering.BetweenlandsSkyRenderer;
-import thebetweenlands.client.rendering.BetweenlandsSkyShaderHandler;
-import thebetweenlands.client.rendering.entitys.RenderGecko;
-import thebetweenlands.client.rendering.entitys.RenderSwampHag;
-import thebetweenlands.client.rendering.entitys.RenderWight;
-import thebetweenlands.client.rendering.model.entity.ModelGecko;
-import thebetweenlands.client.rendering.model.entity.ModelSwampHag;
-import thebetweenlands.client.rendering.model.entity.ModelWight;
-import thebetweenlands.client.rendering.shader.BetweenlandsShaders;
-import thebetweenlands.client.rendering.shader.BetweenlandsSkyShaderInstance;
 import thebetweenlands.common.ambientsounds.BetweenlandsSoundManager;
 import thebetweenlands.common.networking.BetweenlandsPacketHandler;
-import thebetweenlands.common.particles.BetweenlandsParticle;
-import thebetweenlands.common.particles.BetweenlandsPortalParticle;
 import thebetweenlands.common.registries.*;
 import thebetweenlands.common.world.BetweenlandsBiomeProvider;
 import thebetweenlands.common.world.BiomeDecoratorEvent.RegisterBiomeDecoratorEvent;
@@ -85,7 +57,7 @@ public class TheBetweenlands
 
 	// Betweenlands sound manager
 	public static BetweenlandsSoundManager soundManager = new BetweenlandsSoundManager();
-	public static BetweenlandsSkyShaderHandler skyTextureHandler;
+
 	public BetweenlandsSkyRenderer skyrenderer = new BetweenlandsSkyRenderer();
 	public int loopstate = 0;
 	
@@ -98,11 +70,11 @@ public class TheBetweenlands
 	// World provider settings untill i make a class for them
 	public static int LAYER_HEIGHT = 120;
 	public static int CAVE_WATER_HEIGHT = 15;
-
-    // Set up event bus
-	IEventBus eventbus = FMLJavaModLoadingContext.get().getModEventBus();
     
-    public TheBetweenlands() {
+    public TheBetweenlands(IEventBus eventbus) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			ClientEvents.initClient(eventbus);
+		}
 
     	// Register mod contents
 		SoundRegistry.register(eventbus);
@@ -126,18 +98,8 @@ public class TheBetweenlands
     	eventbus.addListener(this::setup);
         // Register the processIMC method for modloading
     	eventbus.addListener(this::processIMC);
-    	// Client side registration
-    	eventbus.addListener(this::doClientStuff);
-    	// Colors
-    	eventbus.addListener(this::registerBlockColors);
-    	// Particles
-    	eventbus.addListener(this::particleStuff);
-		// shaders
-    	eventbus.addListener(this::registershaders);
-    	// Render Layer
-    	eventbus.addListener(this::registerLayerDefinition);
 		// Gen layers event
-		eventbus.addListener(this::GenLayersEvent);
+		eventbus.addListener(this::genLayersEvent);
     	// Betweenlands cave & underwater ambience register
     	//eventbus.addListener(this::betweenlandsAmbienceHandler);
 
@@ -147,16 +109,8 @@ public class TheBetweenlands
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void registershaders(final RegisterShadersEvent event) {
-    	//BetweenlandsShaderInstance betweenlandssky = new BetweenlandsShaderInstance(event.getResourceManager(), new ResourceLocation(Betweenlands.ID, "starfield"), BetweenlandsVertexFormats.BETWEENLANDS_SKY);
-    	
-    	// todo: sky shader "starfield" name is being changed to "betweenlandsSky" 
-    	BetweenlandsShaders.preloadShaders(event.getResourceManager());
-		event.registerShader(BetweenlandsShaders.BetweenlandsSky, BetweenlandsSkyShaderInstance.onLoad);
-    }
-
 	// For whenever I make the gen layers lib a public thing (not sure about the legalitys)
-	private void GenLayersEvent(final RegisterGenLayersEvent event) {
+	private void genLayersEvent(final RegisterGenLayersEvent event) {
 		event.RegisterProvider(new ResourceLocation(TheBetweenlands.ID,"gen_layers_betweenlands"), ProviderGenLayerBetweenlands::new);
 	}
 
@@ -214,127 +168,12 @@ public class TheBetweenlands
         ItemBlockRenderTypes.setRenderLayer(FluidRegistry.SWAMP_WATER_FLOW.get(), RenderType.translucent());
         ItemBlockRenderTypes.setRenderLayer(FluidRegistry.SWAMP_WATER_STILL.get(), RenderType.translucent());
     }
-    
-	private void doClientStuff(final FMLClientSetupEvent event) {
-    	// Client only setup
 
-		// Amate Map renderer
-		TheBetweenlands.amateMapRenderer = new AmateMapRenderer(Minecraft.getInstance().textureManager);
-
-		// Entity renderers
-		EntityRenderers.register(EntityRegistry.SWAMP_HAG.get(), RenderSwampHag::new);
-		EntityRenderers.register(EntityRegistry.GECKO.get(), RenderGecko::new);
-		EntityRenderers.register(EntityRegistry.WIGHT.get(), RenderWight::new);
-		
-		
-		// Compile all rift textures into an atlas
-		LOGGER.info("Building rift texture atlas");
-		skyTextureHandler = new BetweenlandsSkyShaderHandler(true);
-    }
-	
-	private void registerLayerDefinition(final EntityRenderersEvent.RegisterLayerDefinitions event) {
-		
-		event.registerLayerDefinition(RenderSwampHag.SWAMP_HAG_MODEL_LAYER, ModelSwampHag::createModelLayer);
-		event.registerLayerDefinition(RenderGecko.GECKO_MODEL_LAYER, ModelGecko::createModelLayer);
-		event.registerLayerDefinition(RenderWight.WIGHT_MODEL_LAYER, ModelWight::createModelLayer);
-	}
-    
-    @SuppressWarnings("resource")
-	private void particleStuff(final ParticleFactoryRegisterEvent event)
-    {
-    	Minecraft.getInstance().particleEngine.register(ParticleRegistry.SULFUR_GENERIC.get(), BetweenlandsParticle.Helper::new);
-        Minecraft.getInstance().particleEngine.register(ParticleRegistry.PORTAL_EFFECT.get(), BetweenlandsPortalParticle.Helper::new);
-    }
-
+	//TODO: probably unnecessary
     private void processIMC(final InterModProcessEvent event)
     {
         // Recive from other mods
         LOGGER.info("Got IMC {}", event.getIMCStream().map(m->m.messageSupplier().get()).collect(Collectors.toList()));
-    }
-
-    @SubscribeEvent
-    public void registerBlockColors(ColorHandlerEvent.Block event) {
-    	
-    	// Changes particle and block colors
-    	event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.LEAVES_WEEDWOOD_TREE.get());
-    	event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.LEAVES_NIBBLETWIG_TREE.get());
-    	event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.LEAVES_RUBBER_TREE.get());
-    	event.getBlockColors().register(BlockColorRegistry.SWAMP_GRASS, BlockRegistry.SWAMP_GRASS.get());
-    	event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.POISON_IVY.get());
-    	event.getBlockColors().register(BlockColorRegistry.SWAMP_GRASS, BlockRegistry.SWAMP_REED.get());
-    	event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.MOSS.get());
-		event.getBlockColors().register(BlockColorRegistry.SWAMP_FOLIGE, BlockRegistry.HANGER.get());
-		event.getBlockColors().register(BlockColorRegistry.SWAMP_GRASS, BlockRegistry.SWAMP_TALLGRASS.get());
-    	
-    	// Only effects particle colors
-    	event.getBlockColors().register(BlockColorRegistry.SWAMP_WATER, BlockRegistry.SWAMP_WATER_BLOCK.get());
-    }
-    
-    // Ticks every render tick
-	// This is some very old code
-    @SuppressWarnings("resource")
-	@SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public void betweenlandsAmbienceHandler(final ClientTickEvent event) {
-    	
-    	// Get time for shaders
-    	//FractinalTime += 1 / Minecraft.getInstance().getFrameTime();
-    	//Time = (int) FractinalTime;
-    	
-    	// Init vars
-    	Player player = Minecraft.getInstance().player;
-    	if (player!=null) {
-    	ClientLevel level = Minecraft.getInstance().level;
-    	//double x = player.getX(), y = player.getY(), z = player.getZ();
-    	//BlockPos blockpos = new BlockPos(x,y,z);
-    	BlockPos eyeblockpos = player.eyeBlockPosition();
-    	//render.render
-    	//LOGGER.info(ModFluids.SWAMP_WATER_FLOW.get().getTags());
-    	
-    	// TODO: replace with a more professional solution
-    	
-    	if (level.dimension() == DimensionRegistries.BETWEENLANDS_DIMENSION_KEY) {
-    		
-    		rotation += 0.001f;
-    		
-    		// Sound
-    		if (player.isEyeInFluid(FluidTags.WATER) && level.getFluidState(eyeblockpos).getType().isSame(FluidRegistry.SWAMP_WATER_STILL.get().getSource())) {
-        		// Water state
-        		if (loopstate!=3) {
-        			loopstate = 3;
-        			soundManager.stopLoopedSound();
-        		}
-        		soundManager.playLoopedSound(SoundRegistry.BETWEENLANDS_AMBIENT_WATER_LOOP.get());
-        	}
-        	else if (eyeblockpos.getY() <= 47)
-        	{
-        		// Cave state
-        		if (loopstate!=2) {
-        			loopstate = 2;
-        			soundManager.stopLoopedSound();
-        		}
-        		soundManager.playLoopedSound(SoundRegistry.BETWEENLANDS_AMBIENT_CAVES_LOOP.get());
-        	}
-        	else
-        	{
-        		// Default state
-        		if (loopstate!=1) {
-        			loopstate = 1;
-        			soundManager.stopLoopedSound();
-        		}
-        		soundManager.playLoopedSound(SoundRegistry.BETWEENLANDS_AMBIENT_SWAMP_LOOP.get());
-        	}
-    	}
-    	else {
-    		
-    		// Sound
-    		// Off state
-    		if (loopstate!=0) {
-    			loopstate = 0;
-    			soundManager.stopLoopedSound();
-    		}
-    	}
-    }
     }
 }
 
