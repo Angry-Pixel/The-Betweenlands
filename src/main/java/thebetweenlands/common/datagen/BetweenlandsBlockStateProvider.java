84dd25f9c5d64938a97b29c9303d6210ad7ef0e2
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-08-15 00:14:19 -0600

    auto gen item models and get rid of old models

--- src/main/java/thebetweenlands/common/datagen/BetweenlandsBlockStateProvider.java
+++ src/main/java/thebetweenlands/common/datagen/BetweenlandsBlockStateProvider.java
@@ -25,7 +25,7 @@ public class BetweenlandsBlockStateProvider extends BlockStateProvider {
 		this.druidStone(BlockRegistry.DRUID_STONE_3);
 		this.druidStone(BlockRegistry.DRUID_STONE_4);
 		this.druidStone(BlockRegistry.DRUID_STONE_5);
-		this.simpleBlock(BlockRegistry.DRUID_STONE_6.get(), this.models().getExistingFile(this.blockTexture(Blocks.STONE)));
+		this.simpleBlockWithItem(BlockRegistry.DRUID_STONE_6.get(), this.models().getExistingFile(this.blockTexture(Blocks.STONE)));
 	}
 
 	private void druidStone(DeferredBlock<Block> stone) {
@@ -33,5 +33,10 @@ public class BetweenlandsBlockStateProvider extends BlockStateProvider {
 		ModelFile active = this.models().orientable(stone.getId() + "_active", this.blockTexture(Blocks.STONE), this.blockTexture(stone.get()), this.blockTexture(Blocks.STONE));
 
 		this.getVariantBuilder(stone.get()).forAllStates(state -> ConfiguredModel.builder().rotationY((int) state.getValue(DruidStoneBlock.FACING).toYRot()).modelFile(state.getValue(DruidStoneBlock.ACTIVE) ? active : inactive).build());
+		this.simpleBlockItem(stone);
+	}
+
+	public void simpleBlockItem(DeferredBlock<Block> block) {
+		this.itemModels().withExistingParent(block.getId().toString(), this.modLoc("block/" + block.getId().getPath()));
 	}
 }
