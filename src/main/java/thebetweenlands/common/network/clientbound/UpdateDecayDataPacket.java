f0eb20ff621b65b4506c9f7171c786f3eb897b5e
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2024-08-23 23:01:23 +1000

    Hack display health to make decay do decay properly

--- src/main/java/thebetweenlands/common/network/clientbound/UpdateDecayDataPacket.java
+++ src/main/java/thebetweenlands/common/network/clientbound/UpdateDecayDataPacket.java
@@ -1,10 +1,14 @@
 package thebetweenlands.common.network.clientbound;
 
+import com.mojang.authlib.minecraft.client.MinecraftClient;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.vehicle.Minecart;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import thebetweenlands.common.TheBetweenlands;
 import thebetweenlands.common.component.entity.DecayData;
@@ -31,10 +35,18 @@ public record UpdateDecayDataPacket(int level, int prevLevel, float saturation, 
 		context.enqueueWork(() -> {
 			Player player = context.player();
 			player.setData(AttachmentRegistry.DECAY, new DecayData(message.level(), message.prevLevel(), message.saturation(), message.acceleration()));
+			float initialMax = player.getMaxHealth();
 			// Prevent the game from playing the take damage animation
 			PlayerDecayHandler.applyDecayAttributeModifiers(player);
+			float finalMax = player.getMaxHealth();
 			if(player.getHealth() > player.getMaxHealth()) {
 				player.setHealth(player.getMaxHealth());
+			}
+			int difference = Math.round(initialMax - finalMax);
+			if(difference > 0) {
+				try(Minecraft mc = Minecraft.getInstance()) {
+					mc.gui.displayHealth -= difference;
+				} catch (Exception e) {}
 			}
 		});
 	}
