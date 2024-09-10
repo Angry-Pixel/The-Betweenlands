7770c5a7138d7c3b506f0426dcc84fde28397b7a
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2024-09-10 23:38:48 +1000

    Fix connections between glass types temporarily

--- src/main/java/thebetweenlands/common/block/FilteredSiltGlassBlock.java
+++ src/main/java/thebetweenlands/common/block/FilteredSiltGlassBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Direction.Axis;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.TransparentBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import thebetweenlands.api.client.IConnectedTextureBlock;
@@ -17,19 +18,20 @@ public class FilteredSiltGlassBlock extends TransparentBlock implements IConnect
 
 	@Override
 	public IConnectionRules createConnectionRules(BlockAndTintGetter level, BlockPos pos, BlockState state) {
+		final Block parent = this;
 		return new IConnectionRules() {
 			@Override
 			public boolean canConnectTo(BlockAndTintGetter world, BlockPos pos, Direction face, BlockPos to) {
 				Axis axis = face.getAxis();
 				boolean onSamePlane = (axis != Axis.X || (to.getX() - pos.getX()) == 0) && (axis != Axis.Y || (to.getY() - pos.getY()) == 0) && (axis != Axis.Z || (to.getZ() - pos.getZ()) == 0);
-				return onSamePlane && world.getBlockState(to).getBlock() instanceof FilteredSiltGlassBlock;
+				return onSamePlane && world.getBlockState(to).getBlock() == parent;
 			}
 
 			@Override
 			public boolean canConnectThrough(BlockAndTintGetter world, BlockPos pos, Direction face, BlockPos to) {
 				Axis axis = face.getAxis();
 				if((axis == Axis.X && to.getX() - pos.getX() != 0) || (axis == Axis.Y && to.getY() - pos.getY() != 0) || (axis == Axis.Z && to.getZ() - pos.getZ() != 0)) {
-					return !IConnectionRules.isSideSolid(world, to, face.getOpposite()) && world.getBlockState(to).getBlock() instanceof FilteredSiltGlassBlock == false;
+					return !IConnectionRules.isSideSolid(world, to, face.getOpposite()) && world.getBlockState(to).getBlock() != parent;
 				}
 				return false;
 			}
