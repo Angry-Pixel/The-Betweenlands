e3fbe0430f9cd46ba302be29c6f406192023c24e
Andromander <androsa@protonmail.com>
2024-09-24 16:23:30 +1000

    Cleaning

--- src/main/java/thebetweenlands/common/world/gen/BetweenlandsChunkGenerator.java
+++ src/main/java/thebetweenlands/common/world/gen/BetweenlandsChunkGenerator.java
@@ -1,7 +1,6 @@
 package thebetweenlands.common.world.gen;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.Util;
@@ -21,12 +20,12 @@ import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.levelgen.*;
 import net.minecraft.world.level.levelgen.blending.Blender;
 import net.minecraft.world.level.levelgen.synth.BlendedNoise;
+import org.apache.commons.lang3.mutable.MutableObject;
 import thebetweenlands.common.registries.BlockRegistry;
 import thebetweenlands.common.world.gen.warp.*;
 
 import java.util.List;
 import java.util.OptionalInt;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Predicate;
 
@@ -43,8 +42,6 @@ public class BetweenlandsChunkGenerator extends NoiseBasedChunkGenerator {
 	protected final TerrainWarper warper;
 	private final int cellWidth;
 	private final int cellHeight;
-
-	private static final BlockState[] EMPTY_COLUMN = new BlockState[0];
 
 	public BetweenlandsChunkGenerator(BiomeSource biomeSource, Holder<NoiseGeneratorSettings> settings) {
 		super(biomeSource, settings);
@@ -76,38 +73,6 @@ public class BetweenlandsChunkGenerator extends NoiseBasedChunkGenerator {
 	}
 
 	@Override
-	public int getBaseHeight(int x, int z, Heightmap.Types height, LevelHeightAccessor level, RandomState rand) {
-		NoiseSettings settings = this.settings.value().noiseSettings();
-		int minY = Math.max(settings.minY(), level.getMinBuildHeight());
-		int maxY = Math.min(settings.minY() + settings.height(), level.getMaxBuildHeight());
-		int mincell = Math.floorDiv(minY, this.cellHeight);
-		int maxcell = Math.floorDiv(maxY - minY, this.cellHeight);
-
-		if (maxcell <= 0) {
-			return level.getMinBuildHeight();
-		} else {
-			return this.iterateNoiseColumn(x, z, null, height.isOpaque(), mincell, maxcell).orElse(level.getMinBuildHeight());
-		}
-	}
-
-	@Override
-	public NoiseColumn getBaseColumn(int x, int z, LevelHeightAccessor level, RandomState random) {
-		NoiseSettings settings = this.settings.value().noiseSettings();
-		int minY = Math.max(settings.minY(), level.getMinBuildHeight());
-		int maxY = Math.min(settings.minY() + settings.height(), level.getMaxBuildHeight());
-		int mincell = Math.floorDiv(minY, this.cellHeight);
-		int maxcell = Math.floorDiv(maxY - minY, this.cellHeight);
-
-		if (maxcell <= 0) {
-			return new NoiseColumn(minY, EMPTY_COLUMN);
-		} else {
-			BlockState[] ablockstate = new BlockState[maxcell * settings.getCellHeight()];
-			this.iterateNoiseColumn(x, z, ablockstate, null, mincell, maxcell);
-			return new NoiseColumn(minY, ablockstate);
-		}
-	}
-
-	@Override
 	public CompletableFuture<ChunkAccess> createBiomes(RandomState random, Blender blender, StructureManager manager, ChunkAccess access) {
 		return CompletableFuture.supplyAsync(Util.wrapThreadWithTaskName("init_biomes", () -> {
 			access.fillBiomesFromNoise(this.getBiomeSource(), Climate.empty());
@@ -116,37 +81,7 @@ public class BetweenlandsChunkGenerator extends NoiseBasedChunkGenerator {
 	}
 
 	@Override
-	public CompletableFuture<ChunkAccess> fillFromNoise(Blender blender, RandomState random, StructureManager manager, ChunkAccess access) {
-		NoiseSettings settings = this.settings.value().noiseSettings();
-		int minY = Math.max(settings.minY(), access.getMinBuildHeight());
-		int maxY = Math.min(settings.minY() + settings.height(), access.getMaxBuildHeight());
-		int mincell = Math.floorDiv(minY, this.cellHeight);
-		int maxcell = Math.floorDiv(maxY - minY, this.cellHeight);
-
-		return maxcell <= 0 ? CompletableFuture.completedFuture(access) : CompletableFuture.supplyAsync(Util.wrapThreadWithTaskName("wgen_fill_noise_bl", () -> {
-			int maxIndex = access.getSectionIndex(maxcell * this.cellHeight - 1 + minY);
-			int minIndex = access.getSectionIndex(minY);
-			Set<LevelChunkSection> sections = Sets.newHashSet();
-
-			for (int index = maxIndex; index >= minIndex; index--) {
-				LevelChunkSection section = access.getSection(index);
-				section.acquire();
-				sections.add(section);
-			}
-
-			ChunkAccess chunk;
-			try {
-				chunk = this.doFill(access, mincell, maxcell);
-			} finally {
-				for (LevelChunkSection section : sections) {
-					section.release();
-				}
-			}
-			return chunk;
-		}), Util.backgroundExecutor());
-	}
-
-	private ChunkAccess doFill(ChunkAccess access, int min, int max) {
+	public ChunkAccess doFill(Blender blender, StructureManager structureManager, RandomState random, ChunkAccess access, int min, int max) {
 		int cellCountX = 16 / this.cellWidth;
 		int cellCountZ = 16 / this.cellWidth;
 		Heightmap oceanfloor = access.getOrCreateHeightmapUnprimed(Heightmap.Types.OCEAN_FLOOR_WG);
@@ -214,48 +149,62 @@ public class BetweenlandsChunkGenerator extends NoiseBasedChunkGenerator {
 		return access;
 	}
 
-	protected OptionalInt iterateNoiseColumn(int x, int z, BlockState[] states, Predicate<BlockState> predicate, int min, int max) {
-		int xDiv = Math.floorDiv(x, this.cellWidth);
-		int zDiv = Math.floorDiv(z, this.cellWidth);
-		int xMod = Math.floorMod(x, this.cellWidth);
-		int zMod = Math.floorMod(z, this.cellWidth);
-		int xMin = xMod / this.cellWidth;
-		int zMin = zMod / this.cellWidth;
-		double[][] columns = new double[][]{
-			this.makeAndFillNoiseColumn(xDiv, zDiv, min, max),
-			this.makeAndFillNoiseColumn(xDiv, zDiv + 1, min, max),
-			this.makeAndFillNoiseColumn(xDiv + 1, zDiv, min, max),
-			this.makeAndFillNoiseColumn(xDiv + 1, zDiv + 1, min, max)
-		};
-		//Aquifers?
+	@Override
+	public OptionalInt iterateNoiseColumn(LevelHeightAccessor level, RandomState random, int x, int z, MutableObject<NoiseColumn> column, Predicate<BlockState> stoppingState) {
+		NoiseSettings noise = this.settings.value().noiseSettings().clampToHeightAccessor(level);
+		int min = Math.floorDiv(noise.minY(), this.cellHeight);
+		int max = Math.floorDiv(noise.height(), this.cellHeight);
 
-		for (int cell = max - 1; cell >= 0; cell--) {
-			double d10 = columns[0][cell];
-			double d20 = columns[1][cell];
-			double d30 = columns[2][cell];
-			double d40 = columns[3][cell];
-			double d11 = columns[0][cell + 1];
-			double d21 = columns[1][cell + 1];
-			double d31 = columns[2][cell + 1];
-			double d41 = columns[3][cell + 1];
+		if (max <= 0) {
+			return OptionalInt.empty();
+		} else {
+			BlockState[] states = null;
+			if (column != null) {
+				states = new BlockState[max * noise.getCellHeight()];
+				column.setValue(new NoiseColumn(noise.minY(), states));
+			}
+			int xDiv = Math.floorDiv(x, this.cellWidth);
+			int zDiv = Math.floorDiv(z, this.cellWidth);
+			int xMod = Math.floorMod(x, this.cellWidth);
+			int zMod = Math.floorMod(z, this.cellWidth);
+			int xMin = xMod / this.cellWidth;
+			int zMin = zMod / this.cellWidth;
+			double[][] columns = new double[][]{
+				this.makeAndFillNoiseColumn(xDiv, zDiv, min, max),
+				this.makeAndFillNoiseColumn(xDiv, zDiv + 1, min, max),
+				this.makeAndFillNoiseColumn(xDiv + 1, zDiv, min, max),
+				this.makeAndFillNoiseColumn(xDiv + 1, zDiv + 1, min, max)
+			};
+			//Aquifers?
 
-			for (int height = this.cellHeight - 1; height >= 0; height--) {
-				double dcell = height / (double)this.cellHeight;
-				double lcell = Mth.lerp3(dcell, xMin, zMin, d10, d11, d30, d31, d20, d21, d40, d41);
-				int layer = cell * this.cellHeight + height;
-				int maxlayer = layer + min * this.cellHeight;
-				BlockState state = this.generateBaseState(lcell, layer);
-				if (states != null) {
-					states[layer] = state;
-				}
+			for (int cell = max - 1; cell >= 0; cell--) {
+				double d10 = columns[0][cell];
+				double d20 = columns[1][cell];
+				double d30 = columns[2][cell];
+				double d40 = columns[3][cell];
+				double d11 = columns[0][cell + 1];
+				double d21 = columns[1][cell + 1];
+				double d31 = columns[2][cell + 1];
+				double d41 = columns[3][cell + 1];
 
-				if (predicate != null && predicate.test(state)) {
-					return OptionalInt.of(maxlayer + 1);
+				for (int height = this.cellHeight - 1; height >= 0; height--) {
+					double dcell = height / (double)this.cellHeight;
+					double lcell = Mth.lerp3(dcell, xMin, zMin, d10, d11, d30, d31, d20, d21, d40, d41);
+					int layer = cell * this.cellHeight + height;
+					int maxlayer = layer + min * this.cellHeight;
+					BlockState state = this.generateBaseState(lcell, layer);
+					if (states != null) {
+						states[layer] = state;
+					}
+
+					if (stoppingState != null && stoppingState.test(state)) {
+						return OptionalInt.of(maxlayer + 1);
+					}
 				}
 			}
-		}
 
-		return OptionalInt.empty();
+			return OptionalInt.empty();
+		}
 	}
 
 	private double[] makeAndFillNoiseColumn(int x, int z, int min, int max) {
