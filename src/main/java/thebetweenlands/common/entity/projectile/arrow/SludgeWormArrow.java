e41e64c326297ae1a67243cb4f96974af53ab630
vadis365 <6518708+vadis365@users.noreply.github.com>
2024-10-14 15:09:34 +0100

    made worm arrow spawns respect hitting parts of entities

--- src/main/java/thebetweenlands/common/entity/projectile/arrow/SludgeWormArrow.java
+++ src/main/java/thebetweenlands/common/entity/projectile/arrow/SludgeWormArrow.java
@@ -8,6 +8,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.neoforged.neoforge.entity.PartEntity;
 import thebetweenlands.common.entity.monster.TinySludgeWormHelper;
 import thebetweenlands.common.registries.EntityRegistry;
 import thebetweenlands.common.registries.ItemRegistry;
@@ -26,19 +28,29 @@ public class SludgeWormArrow extends AbstractArrow {
 		super(EntityRegistry.SLUDGE_WORM_ARROW.get(), owner, level, pickupItemStack, firedFromWeapon);
 	}
 
+	@SuppressWarnings("rawtypes")
 	@Override
-	protected void doPostHurtEffects(LivingEntity target) {
-		super.doPostHurtEffects(target);
+	protected void onHitEntity(EntityHitResult result) {
+		super.onHitEntity(result);
 		if (!this.level().isClientSide()) {
-			TinySludgeWormHelper worm = EntityRegistry.TINY_SLUDGE_WORM_HELPER.get().create(this.level());
-			worm.moveTo(this.getX(), this.getY(), this.getZ(), this.getYRot(), this.getXRot());
-			worm.setTarget(target);
-			if (this.getOwner() instanceof Player player) {
-				worm.setOwnerUUID(player.getUUID());
+			if (result.getEntity() instanceof LivingEntity)
+				spawnWorm((LivingEntity) result.getEntity());
+			else if (result.getEntity() instanceof PartEntity) {
+				PartEntity part = (PartEntity) result.getEntity();
+				if (part.getParent() instanceof LivingEntity)
+					spawnWorm((LivingEntity) part.getParent());
 			}
-			this.level().addFreshEntity(worm);
-			this.discard();
 		}
+	}
+
+	private void spawnWorm(LivingEntity target) {
+		TinySludgeWormHelper worm = EntityRegistry.TINY_SLUDGE_WORM_HELPER.get().create(this.level());
+		worm.moveTo(this.getX(), this.getY(), this.getZ(), this.getYRot(), this.getXRot());
+		worm.setTarget(target);
+		if (this.getOwner() instanceof Player player)
+			worm.setOwnerUUID(player.getUUID());
+		this.level().addFreshEntity(worm);
+		this.discard();
 	}
 
 	@Override
