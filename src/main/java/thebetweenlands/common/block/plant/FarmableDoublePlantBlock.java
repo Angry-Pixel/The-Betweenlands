641231986789420335a83452fa35401885b8af95
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2025-10-04 02:43:22 +1000

    Add composting of double plants on click
    That handsome man in #1370 would be proud

--- src/main/java/thebetweenlands/common/block/plant/FarmableDoublePlantBlock.java
+++ src/main/java/thebetweenlands/common/block/plant/FarmableDoublePlantBlock.java
@@ -2,7 +2,10 @@ package thebetweenlands.common.block.plant;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
@@ -13,7 +16,10 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.phys.BlockHitResult;
 import thebetweenlands.api.block.FarmablePlant;
+import thebetweenlands.common.block.farming.DugSoilBlock;
+import thebetweenlands.common.registries.ItemRegistry;
 
 import javax.annotation.Nullable;
 
@@ -41,6 +47,23 @@ public class FarmableDoublePlantBlock extends DoublePlantBlock implements Farmab
 		level.setBlock(blockpos, copyWaterloggedFrom(level, blockpos, this.defaultBlockState().setValue(HALF, DoubleBlockHalf.UPPER).setValue(FACING, level.getBlockState(pos).getValue(FACING))), 3);
 	}
 
+	@Override
+	protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+		if (stack.is(ItemRegistry.COMPOST)) {
+			pos = pos.below();
+			for (int i = 0; i < 3; i++) {
+				state = level.getBlockState(pos);
+				if (state.getBlock() instanceof DugSoilBlock) {
+					return state.useItemOn(stack, level, player, hand, hitResult);
+				} else if (!state.is(this)) {
+					break;
+				}
+				pos = pos.below();
+			}
+		}
+		return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
+	}
+	
 	@Override
 	public boolean isFarmable(Level level, BlockPos pos, BlockState state) {
 		return true;
