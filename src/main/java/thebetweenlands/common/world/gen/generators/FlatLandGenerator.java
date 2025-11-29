e0d5d4e08b18741e0cc039a573f37851c1443e9d
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2025-11-29 18:29:05 +1000

    Update Flat Land Generator to work as a proper early generator

--- src/main/java/thebetweenlands/common/world/gen/generators/FlatLandGenerator.java
+++ src/main/java/thebetweenlands/common/world/gen/generators/FlatLandGenerator.java
@@ -1,22 +1,27 @@
 package thebetweenlands.common.world.gen.generators;
 
+import java.util.EnumSet;
 import java.util.stream.IntStream;
 
 import com.mojang.serialization.Codec;
 
-import net.minecraft.core.BlockPos.MutableBlockPos;
+import it.unimi.dsi.fastutil.ints.IntIntPair;
+import net.minecraft.core.SectionPos;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.LegacyRandomSource;
-import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.synth.ImprovedNoise;
 import net.minecraft.world.level.levelgen.synth.PerlinNoise;
+import thebetweenlands.api.world.BiomeWeights;
+import thebetweenlands.api.world.ExtraChunkInfoTypes;
 import thebetweenlands.api.world.generator.EarlyGenerationContext;
+import thebetweenlands.api.world.generator.EarlyGenerationContext.ChunkHeightmaps;
 import thebetweenlands.api.world.generator.EarlyGenerator;
 import thebetweenlands.common.TheBetweenlands;
-import thebetweenlands.common.world.gen.feature.config.FlatLandConfiguration;
 import thebetweenlands.common.world.gen.generators.config.FlatLandGeneratorConfiguration;
 
 // TODO fix this up so we don't need to generate new perlin noise every single time
@@ -28,6 +33,11 @@ public class FlatLandGenerator extends EarlyGenerator<FlatLandGeneratorConfigura
 		super(codec);
 	}
 
+	@Override
+	public EnumSet<ExtraChunkInfoTypes> getRequiredExtraInfo(FlatLandGeneratorConfiguration config) {
+		return EnumSet.of(ExtraChunkInfoTypes.BIOME_WEIGHTS);
+	}
+	
 	@Override
 	public boolean place(EarlyGenerationContext<FlatLandGeneratorConfiguration> context) {
 		long seed = context.worldSeed();
@@ -42,66 +52,151 @@ public class FlatLandGenerator extends EarlyGenerator<FlatLandGeneratorConfigura
 		
 		// Compute all the noise values for this chunk
 		
-		ChunkPos chunkPos = context.chunkAccess().getPos();
+		ChunkAccess chunkAccess = context.chunkAccess();
+		ChunkPos chunkPos = chunkAccess.getPos();
 
 		double[] landNoise = computeLandNoise(landNoiseGen, chunkPos.x, chunkPos.z);
 		double[] riverNoise = computeRiverNoise(riverNoiseGen, chunkPos.x, chunkPos.z);
+
+		int[] minBlockY = new int[256];
+		int[] maxBlockY = new int[256];
 		
-		for(int x = 0; x < 16; ++x) {
-			for(int z = 0; z < 16; ++z) {
-				this.placeColumnBlocks(context, chunkPos, x, z, landNoise, riverNoise);
+		IntIntPair minHeightMaxHeight = computeColumnBlocks(context, chunkPos, landNoise, riverNoise, minBlockY, maxBlockY);
+		int totalMinBlockY = minHeightMaxHeight.leftInt();
+		int totalMaxBlockY = minHeightMaxHeight.rightInt();
+
+//		TheBetweenlands.LOGGER.info("Generated noise values for chunk {}: Global Min = {}, Global Max = {}, Min Array = {}, Max Array = {}", chunkPos, totalMinBlockY, totalMaxBlockY, minBlockY, maxBlockY);
+		
+		// If the min block is above the max block, then no blocks can be placed
+		if(totalMinBlockY + 1 > totalMaxBlockY - 1) {
+			return false;
+		}
+
+		// Minimum section index (inclusive) that needs blocks placed
+		int minSectionIndex = chunkAccess.getSectionIndex(totalMinBlockY + 1);
+
+		// Maximum section index (inclusive) that needs blocks placed
+		int maxSectionIndex = chunkAccess.getSectionIndex(totalMaxBlockY - 1);
+		
+		// The block we will be setting
+		BlockState terrainBlock = context.blockGenerator().defaultTerrainState();
+		
+		// The heightmaps (for updating)
+		ChunkHeightmaps heightmaps = context.chunkHeightmaps();
+		
+		for(int sectionIndex = minSectionIndex; sectionIndex <= maxSectionIndex; ++sectionIndex) {
+			// The section we'll be setting blocks in
+			LevelChunkSection section = chunkAccess.getSection(sectionIndex);
+			
+			// The y value of block 0 in this section
+			int sectionMinY = SectionPos.sectionToBlockCoord(chunkAccess.getSectionYFromSectionIndex(sectionIndex));
+			
+			for(int x = 0; x < 16; ++x) {
+				for(int z = 0; z < 16; ++z) {
+					final int index = x * 16 + z;
+					
+					final int yMin = Math.max(
+							minBlockY[index] - sectionMinY + 1,
+							0
+						);
+					
+					// if lowest block for this x/z is above this section, don't place anything
+					if(yMin >= 16) {
+						continue;
+					}
+					
+					final int yMax = Math.min(
+							maxBlockY[index] - sectionMinY - 1,
+							15
+						);
+					
+					// if highest block for this x/z is below this section, don't place anything
+					if(yMax < 0) {
+						continue;
+					}
+					
+					for(int y = yMin; y <= yMax; ++y) {
+						// Important: disable locks since the section is already acquired by the chunk generator
+						section.setBlockState(x, y, z, terrainBlock, false);
+						heightmaps.update(x, sectionMinY + y, z, terrainBlock);
+					}
+				}
 			}
 		}
 		
 		return true;
 	}
 
-	public void placeColumnBlocks(EarlyGenerationContext<FlatLandGeneratorConfiguration> context, ChunkPos chunkPos, int x, int z, double[] landNoise, double[] riverNoise) {
-//		WorldGenLevel level = context.level();
+	/**
+	 * Computes the min (exclusive) and max (exclusive) height that blocks will need to be placed in each column
+	 * @param minBlockYOut the output array for the minimum height that blocks need to be placed in each column
+	 * @param maxBlockYOut the output array for the maximum height that blocks need to be placed in each column
+	 * @return an int pair containing the global minimum height to set (left) and global maximum height to set (right) of the entire chunk
+	 */
+	public IntIntPair computeColumnBlocks(EarlyGenerationContext<FlatLandGeneratorConfiguration> context, ChunkPos chunkPos, double[] landNoise, double[] riverNoise, int[] minBlockYOut, int[] maxBlockYOut) {
+		// Get info on where these blocks should even go
 		FlatLandGeneratorConfiguration config = context.config();
-		
-		// Used to "flatten" the terrain to the water level (so we don't leave gaps between the terrain and seafloor)
-//		final int lowestBlock = findHighestBlockBelowWaterLevel(level, x, z, config.waterLevel());
-		final int lowestBlock = context.chunkHeightmaps().oceanfloorHeightmap().getHighestTaken(x, z);
+		// Use heightmap so we don't have to manually check each block
+		Heightmap oceanfloorHeightmap = context.chunkHeightmaps().oceanfloorHeightmap();
+		// Get biome weights
+		BiomeWeights biomeWeights = context.extraChunkInfo().biomeWeights().orElseThrow();
 
-		// Fetch noise values
-		final double landNoiseValue = landNoise[x * 16 + z];
-		final double riverNoiseValue = riverNoise[x * 16 + z];
+		final int waterLevel = config.waterLevel();
+		final int terrainHeight = config.terrainHeight();
+		
+		int minHeightTotal = waterLevel;
+		int maxHeightTotal = waterLevel;
+		
+		for(int x = 0; x < 16; ++x) {
+			for(int z = 0; z < 16; ++z) {
+				final int index = x * 16 + z;
+				
+				// Used to "flatten" the terrain to the water level (so we don't leave gaps between the terrain and seafloor)
+				final int lowestBlock = Math.min(oceanfloorHeightmap.getHighestTaken(x, z), waterLevel);
+				
+				// Store lowest block out
+				minBlockYOut[index] = lowestBlock;
+				minHeightTotal = Math.min(minHeightTotal, lowestBlock);
 
-		// Calculate chance of a river spawning, and how much it'll affect the terrain height
-		int terrainHeight = (int)Math.ceil(Math.abs(landNoiseValue * (config.waterLevel() - lowestBlock + config.terrainHeight())));
-		float riverThreshold = 6.0f * (terrainHeight + 2);
-		double riverPercentage = 1.0D - (riverNoiseValue / riverThreshold);
-		
-//		float weight = (Math.min(biomeWeight + 0.5F, 1.0F) - 0.5F) * 2.0F;
-		float weight = 0.0F; // Weight: biome weight based number for the blending at the edge of biomes
-		
-		final double maxHeight;
-		if(riverNoiseValue < riverThreshold) {
-			// If a river should be generating, generate a river (w/ biome blending)
-			maxHeight = config.waterLevel() + terrainHeight - riverPercentage * (terrainHeight + ((riverThreshold - riverNoiseValue) / 16.0D));
-		} else {
-			// If not, just blend the terrain
-			maxHeight = config.waterLevel() + terrainHeight;
+				// Fetch noise values
+				final double landNoiseValue = landNoise[index];
+				final double riverNoiseValue = riverNoise[index];
+
+				// Calculate chance of a river spawning, and how much it'll affect the terrain height
+				int columnTerrainHeight = (int)Math.ceil(Math.abs(landNoiseValue * (waterLevel - lowestBlock + terrainHeight)));
+				float riverThreshold = 6.0f * (columnTerrainHeight + 2);
+				double riverPercentage = 1.0D - (riverNoiseValue / riverThreshold);
+				
+				// Get biome weighting (for blending)
+				float biomeWeight = biomeWeights.get(x, z);
+				
+				// Calculate lerp weight
+				float weight = (Math.min(biomeWeight + 0.5F, 1.0F) - 0.5F) * 2.0F;
+				
+				// If weight is less than or equal to 0, don't bother calculating what the max height would have been
+				if(weight <= 0.0F) {
+					maxBlockYOut[index] = lowestBlock;
+					continue;
+				}
+				
+				// Calculate max height
+				final double maxHeight;
+				if(riverNoiseValue < riverThreshold) {
+					// If a river should be generating, generate a river (w/ biome blending)
+					maxHeight = waterLevel + columnTerrainHeight - riverPercentage * (columnTerrainHeight + ((riverThreshold - riverNoiseValue) / 16.0D));
+				} else {
+					// If not, just blend the terrain
+					maxHeight = waterLevel + columnTerrainHeight;
+				}
+				
+				final int maxBlockY = Mth.ceil(Mth.clampedLerp(lowestBlock, maxHeight, weight));
+				maxBlockYOut[index] = maxBlockY;
+				maxHeightTotal = Math.max(maxHeightTotal, maxBlockY);
+			}
 		}
-//		TheBetweenlands.LOGGER.info("Min Height: {}, Max Height: {}", lowestBlock, maxHeight);
 		
-		for(int y = lowestBlock; y < Mth.clampedLerp(maxHeight, lowestBlock, weight); y++) {
-//			this.setBlock(level, pos.setY(y), context.blockGenerator().defaultTerrainState());
-		}
+		return IntIntPair.of(minHeightTotal, maxHeightTotal);
 	}
-	
-//	public static int findHighestBlockBelowWaterLevel(WorldGenLevel level, int x, int z, int waterLevel) {
-//		MutableBlockPos pos = new MutableBlockPos(x, 0, z);
-//		for(int y = waterLevel; y > level.getMinBuildHeight(); y--) {
-//			BlockState currentBlock = level.getBlockState(pos.setY(y));
-//			if(!currentBlock.isAir() && !currentBlock.canBeReplaced()) {
-//				return y;
-//			}
-//		}
-//		
-//		return level.getMinBuildHeight();
-//	}
 	
 	public static double[] computeLandNoise(PerlinNoise landNoise, int chunkX, int chunkZ) {
 		double[] noise = new double[256];
