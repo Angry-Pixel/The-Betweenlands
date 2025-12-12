673c92e03613e42ff460617dec0fe127881c01f8
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2025-12-12 19:26:24 +1000

    Add helper method for computing noise from settings & biome weights

--- src/main/java/thebetweenlands/common/world/gen/generators/CoarseIslandsGenerator.java
+++ src/main/java/thebetweenlands/common/world/gen/generators/CoarseIslandsGenerator.java
@@ -1,20 +1,64 @@
 package thebetweenlands.common.world.gen.generators;
 
+import java.util.EnumSet;
+
 import com.mojang.serialization.Codec;
 
+import net.minecraft.world.level.ChunkPos;
+import thebetweenlands.api.world.BiomeWeights;
+import thebetweenlands.api.world.ExtraChunkInfoTypes;
 import thebetweenlands.api.world.generator.EarlyGenerationContext;
 import thebetweenlands.api.world.generator.EarlyGenerator;
 import thebetweenlands.common.world.gen.generators.config.CoarseIslandsGeneratorConfiguration;
+import thebetweenlands.common.world.gen.generators.util.EarlyGeneratorHelper;
+import thebetweenlands.common.world.gen.util.BiSimplexData;
+import thebetweenlands.common.world.gen.util.config.BiSimplexNoiseConfiguration;
+import thebetweenlands.common.world.gen.util.config.SimplexNoiseSettings;
 
 public class CoarseIslandsGenerator extends EarlyGenerator<CoarseIslandsGeneratorConfiguration> {
 
 	public CoarseIslandsGenerator(Codec<CoarseIslandsGeneratorConfiguration> codec) {
 		super(codec);
 	}
-
+	
+	@Override
+	public EnumSet<ExtraChunkInfoTypes> getRequiredExtraInfo(CoarseIslandsGeneratorConfiguration config) {
+		return EnumSet.of(ExtraChunkInfoTypes.BIOME_WEIGHTS);
+	}
+	
 	@Override
 	public boolean place(EarlyGenerationContext<CoarseIslandsGeneratorConfiguration> context) {
+		CoarseIslandsGeneratorConfiguration config = context.config();
+		
+		// Get the noise values for this chunk
+		CoarseIslandNoise noise = this.computeNoise(context);
+		
 		return false;
 	}
 
+	protected record CoarseIslandNoise(double[] islandNoiseScaled, double[] cragNoiseScaled) {}
+	
+	public CoarseIslandNoise computeNoise(EarlyGenerationContext<CoarseIslandsGeneratorConfiguration> context) {
+		CoarseIslandsGeneratorConfiguration config = context.config();
+		long seed = context.worldSeed();
+		ChunkPos chunkPos = context.chunkAccess().getPos();
+
+		// Get biome weights
+		BiomeWeights weights = context.extraChunkInfo().biomeWeights().orElseThrow();
+		
+		// Get or create noise generators for this seed
+		BiSimplexNoiseConfiguration noiseConfig = config.noiseConfig();
+		BiSimplexData noiseData = noiseConfig.getNoise(seed);
+
+		// Compute island noise values
+		SimplexNoiseSettings islandNoiseSettings = noiseConfig.firstNoiseSettings();
+		double[] islandNoiseScaled = EarlyGeneratorHelper.computeNoiseFromSettings(noiseData.first(), chunkPos, islandNoiseSettings, (x, z) -> weights.get(x, z, 2, 12));
+
+		// Compute crag noise values
+		SimplexNoiseSettings cragNoiseSettings = noiseConfig.secondNoiseSettings();
+		double[] cragNoiseScaled = EarlyGeneratorHelper.computeNoiseFromSettings(noiseData.first(), chunkPos, cragNoiseSettings);
+		
+		return new CoarseIslandNoise(islandNoiseScaled, cragNoiseScaled);
+	}
+	
 }
--- src/main/java/thebetweenlands/common/world/gen/generators/util/EarlyGeneratorHelper.java
+++ src/main/java/thebetweenlands/common/world/gen/generators/util/EarlyGeneratorHelper.java
@@ -1,11 +1,16 @@
 package thebetweenlands.common.world.gen.generators.util;
 
+import java.util.Optional;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.ChunkPos;
+import thebetweenlands.api.world.BiomeWeights;
+import thebetweenlands.common.world.gen.util.config.SimplexNoiseSettings;
 import thebetweenlands.util.legacy.BLLegacyPerlinSimplexNoise;
 
 public class EarlyGeneratorHelper {
 
+	// ======== Compute Noise Raw with Size ========
 	public static double[] computeNoiseRawWithSize(BLLegacyPerlinSimplexNoise noiseGenerator, int x, int z, double scale, int size) {
 		if(size <= 0) {
 			throw new IllegalArgumentException("\"size\" must be greater than 0");
@@ -22,6 +27,8 @@ public class EarlyGeneratorHelper {
 		
 		return noise;
 	}
+
+	// ======== Compute Noise Raw ========
 	
 	public static double[] computeNoiseRaw(BLLegacyPerlinSimplexNoise noiseGenerator, int x, int z, double scale) {
 		double[] noise = new double[256];
@@ -43,6 +50,59 @@ public class EarlyGeneratorHelper {
 	public static double[] computeNoiseRaw(BLLegacyPerlinSimplexNoise noiseGenerator, ChunkPos pos, double scale) {
 		return computeNoiseRaw(noiseGenerator, (pos.x * 16), (pos.z * 16), scale);
 	}
+
+	// ======== Compute Noise from Settings ========
+
+	@FunctionalInterface
+	public static interface NoiseWeightGetter {
+		/**
+		 * Gets a weighting for the noise value at the offset x, z. Multiplies the noise value and noise multiplier, but not the noise offset.
+		 * @param x
+		 * @param z
+		 * @return
+		 */
+		public float getWeight(int x, int z);
+	}
+	
+	public static double[] computeNoiseFromSettings(BLLegacyPerlinSimplexNoise noiseGenerator, int x, int z, SimplexNoiseSettings settings, NoiseWeightGetter weightProvider) {
+		double[] noise = computeNoiseRaw(noiseGenerator, z, x, settings.noiseScale());
+
+		final double noiseValueMultiplier = settings.noiseValueMultiplier();
+		final double noiseValueOffset = settings.noiseValueOffset();
+		
+		for(int xOffset = 0; xOffset < 16; ++xOffset) {
+			for(int zOffset = 0; zOffset < 16; ++zOffset) {
+				final int index = xOffset * 16 + zOffset;
+				final float weight = weightProvider.getWeight(xOffset, zOffset);
+				
+				noise[index] = noise[index] * noiseValueMultiplier * weight + noiseValueOffset;
+			}
+		}
+		
+		return noise;
+	}
+
+	public static double[] computeNoiseFromSettings(BLLegacyPerlinSimplexNoise noiseGenerator, int x, int z, SimplexNoiseSettings settings) {
+		return computeNoiseFromSettings(noiseGenerator, x, z, settings, (_x, _z) -> 1.0F);
+	}
+	
+	public static double[] computeNoiseFromSettings(BLLegacyPerlinSimplexNoise noiseGenerator, BlockPos pos, SimplexNoiseSettings settings, NoiseWeightGetter weightProvider) {
+		return computeNoiseFromSettings(noiseGenerator, pos.getX(), pos.getZ(), settings, weightProvider);
+	}
+
+	public static double[] computeNoiseFromSettings(BLLegacyPerlinSimplexNoise noiseGenerator, BlockPos pos, SimplexNoiseSettings settings) {
+		return computeNoiseFromSettings(noiseGenerator, pos, settings, (_x, _z) -> 1.0F);
+	}
+	
+	public static double[] computeNoiseFromSettings(BLLegacyPerlinSimplexNoise noiseGenerator, ChunkPos pos, SimplexNoiseSettings settings, NoiseWeightGetter weightProvider) {
+		return computeNoiseFromSettings(noiseGenerator, (pos.x * 16), (pos.z * 16), settings, weightProvider);
+	}
+
+	public static double[] computeNoiseFromSettings(BLLegacyPerlinSimplexNoise noiseGenerator, ChunkPos pos, SimplexNoiseSettings settings) {
+		return computeNoiseFromSettings(noiseGenerator, pos, settings, (_x, _z) -> 1.0F);
+	}
+	
+	// ======== Compute Single Noise ========
 	
 	public static double computeSingleNoiseRaw(BLLegacyPerlinSimplexNoise noiseGenerator, int x, int z, double scale) {
 		// Note: 1.12.2 swaps X and Z when calculating noise for this, so we swap it here to line up with 1.12
