a778fffb06ec4323fc6cf0c6618f50dce3ec56e8
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2025-12-12 17:16:49 +1000

    Remove unused import in AlgaeBlock.java

--- src/main/java/thebetweenlands/common/block/plant/AlgaeBlock.java
+++ src/main/java/thebetweenlands/common/block/plant/AlgaeBlock.java
@@ -3,7 +3,6 @@ package thebetweenlands.common.block.plant;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Direction.Axis;
-import net.minecraft.core.Direction.AxisDirection;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.BlockGetter;
