5ad7308a30b3a604b314b8789ced31d21a062f14
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2025-12-23 20:31:55 +1000

    Call compose on parent layers

--- src/main/java/thebetweenlands/api/world/biome/layer/DoubleParentBiomeLayer.java
+++ src/main/java/thebetweenlands/api/world/biome/layer/DoubleParentBiomeLayer.java
@@ -3,12 +3,20 @@ package thebetweenlands.api.world.biome.layer;
 import thebetweenlands.api.world.biome.layer.context.BiomeLayerChainState;
 import thebetweenlands.api.world.biome.layer.context.BiomeLayerConfigured;
 import thebetweenlands.api.world.biome.layer.context.BiomeLayerContext;
+import thebetweenlands.api.world.biome.layer.util.BiomeLayerChain;
 
 public interface DoubleParentBiomeLayer extends BiomeLayer {
 
 	public BiomeLayerConfigured getFirstParentLayer();
 
 	public BiomeLayerConfigured getSecondParentLayer();
+
+	@Override
+	public default <A extends Area> void compose(BiomeLayerContext<A> context, BiomeLayerChain biomeLayerChain) {
+		BiomeLayer.super.compose(context, biomeLayerChain);
+		this.getFirstParentLayer().compose(context, biomeLayerChain);
+		this.getSecondParentLayer().compose(context, biomeLayerChain);
+	}
 	
 	@Override
 	public default <A extends Area> AreaFactory<A> createAreaFactory(BiomeLayerContext<A> context, BiomeLayerChainState chainState) {
--- src/main/java/thebetweenlands/api/world/biome/layer/SingleParentBiomeLayer.java
+++ src/main/java/thebetweenlands/api/world/biome/layer/SingleParentBiomeLayer.java
@@ -3,10 +3,17 @@ package thebetweenlands.api.world.biome.layer;
 import thebetweenlands.api.world.biome.layer.context.BiomeLayerChainState;
 import thebetweenlands.api.world.biome.layer.context.BiomeLayerConfigured;
 import thebetweenlands.api.world.biome.layer.context.BiomeLayerContext;
+import thebetweenlands.api.world.biome.layer.util.BiomeLayerChain;
 
 public interface SingleParentBiomeLayer extends BiomeLayer {
 
 	public BiomeLayerConfigured getParentLayer();
+
+	@Override
+	public default <A extends Area> void compose(BiomeLayerContext<A> context, BiomeLayerChain biomeLayerChain) {
+		BiomeLayer.super.compose(context, biomeLayerChain);
+		this.getParentLayer().compose(context, biomeLayerChain);
+	}
 	
 	@Override
 	public default <A extends Area> AreaFactory<A> createAreaFactory(BiomeLayerContext<A> context, BiomeLayerChainState chainState) {
