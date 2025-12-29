af5f43cb6f436fe31765937955b3b94164960e37
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-12-29 05:06:05 +0000

    zzzzzzzzzzzzzzzzzzzz

--- src/main/java/thebetweenlands/client/gui/overlay/CorrosiveBootsOverlay.java
+++ src/main/java/thebetweenlands/client/gui/overlay/CorrosiveBootsOverlay.java
@@ -9,7 +9,7 @@ import thebetweenlands.common.TheBetweenlands;
 import thebetweenlands.common.registries.DataComponentRegistry;
 
 public class CorrosiveBootsOverlay implements IItemDecorator {
-    private static final ResourceLocation TEXTURE = TheBetweenlands.prefix("textures/item/corrosive_boots_overlay.png");
+    private static final ResourceLocation TEXTURE = TheBetweenlands.prefix("textures/gui/overlay/corrosive_boots_overlay.png");
 
     @Override
     public boolean render(GuiGraphics guiGraphics, Font font, ItemStack stack, int xOffset, int yOffset) {
