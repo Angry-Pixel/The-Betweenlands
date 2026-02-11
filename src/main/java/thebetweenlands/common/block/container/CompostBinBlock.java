16111ca434c7eaf8e3cf4fbf3a6caaa916f55920
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2026-02-11 15:23:07 +1000

    Add analog signals to compost bin

--- src/main/java/thebetweenlands/common/block/container/CompostBinBlock.java
+++ src/main/java/thebetweenlands/common/block/container/CompostBinBlock.java
@@ -3,11 +3,13 @@ package thebetweenlands.common.block.container;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -56,6 +58,7 @@ public class CompostBinBlock extends HorizontalBaseEntityBlock {
 	@Override
 	protected void attack(BlockState state, Level level, BlockPos pos, Player player) {
 		if (!level.isClientSide()) {
+			// setBlockAndUpdate will call level.updateNeighbourForOutputSignal(...) for us
 			level.setBlockAndUpdate(pos, state.cycle(OPEN));
 		}
 	}
@@ -131,5 +134,28 @@ public class CompostBinBlock extends HorizontalBaseEntityBlock {
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder.add(OPEN));
+	}
+	
+	@Override
+	protected boolean hasAnalogOutputSignal(BlockState state) {
+		return true;
+	}
+	
+	@Override
+	protected int getAnalogOutputSignal(BlockState state, Level level, BlockPos pos) {
+		BlockEntity blockEntity = level.getBlockEntity(pos);
+		if(state.getValue(OPEN)) {
+			int itemFullness = AbstractContainerMenu.getRedstoneSignalFromBlockEntity(blockEntity);
+			if(blockEntity instanceof CompostBinBlockEntity compostBin) {
+				float compostValue = (float)compostBin.getTotalCompostAmount() / (float)compostBin.getMaximimumCompostAmount();
+				int compostFullness = Mth.lerpDiscrete(compostValue, 0, 15);
+				return Math.max(itemFullness, compostFullness);
+			}
+			return itemFullness;
+		} else if(blockEntity instanceof CompostBinBlockEntity compostBin) {
+			float compostValue = (float)compostBin.getCompostedAmount() / (float)compostBin.getTotalCompostAmount();
+			return Mth.lerpDiscrete(compostValue, 0, 15);
+		}
+		return super.getAnalogOutputSignal(state, level, pos);
 	}
 }
