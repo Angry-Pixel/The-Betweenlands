4b23d44ff29a9f076e4c9a07aab11088e92f5c2a
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2026-03-25 10:55:06 +1000

    Create aspect vials tag

--- src/main/java/thebetweenlands/common/datagen/tags/BLItemTagProvider.java
+++ src/main/java/thebetweenlands/common/datagen/tags/BLItemTagProvider.java
@@ -32,6 +32,7 @@ public class BLItemTagProvider extends ItemTagsProvider {
 	public static final TagKey<Item> REPAIRS_SYRMORITE_SHIELD = tag("repairs_syrmorite_shield");
 	public static final TagKey<Item> REPAIRS_LURKER_SKIN_SHIELD = tag("repairs_lurker_skin_shield");
 
+	public static final TagKey<Item> ASPECT_VIALS = tag("aspect_vials");
 	public static final TagKey<Item> BL_LOGS = tag("betweenlands_logs");
 	public static final TagKey<Item> FILTERED_SILT_GLASS = tag("filtered_silt_glass");
 	public static final TagKey<Item> MUD_BRICK_SHINGLES = tag("mud_brick_shingles");
@@ -75,6 +76,7 @@ public class BLItemTagProvider extends ItemTagsProvider {
 	@Override
 	protected void addTags(HolderLookup.Provider provider) {
 		this.copy(BLBlockTagProvider.OCTINE_IGNITES, OCTINE_IGNITES);
+		this.tag(ASPECT_VIALS).add(ItemRegistry.GREEN_ASPECT_VIAL.getKey(), ItemRegistry.ORANGE_ASPECT_VIAL.getKey());
 		this.tag(BL_LOGS).add(
 			BlockRegistry.WEEDWOOD.asItem(), BlockRegistry.WEEDWOOD_LOG.asItem(), BlockRegistry.WEEDWOOD_BARK.asItem(),
 			BlockRegistry.ROTTEN_BARK.asItem(), BlockRegistry.RUBBER_LOG.asItem(),
