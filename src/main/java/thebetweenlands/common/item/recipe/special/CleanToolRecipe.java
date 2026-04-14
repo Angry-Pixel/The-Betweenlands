340f9913bbc880db510aeaefbd46b43a046eea9e
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2026-04-14 21:57:51 +1000

    Re-implement remainder sap spit in clean tool recipe

--- src/main/java/thebetweenlands/common/item/recipe/special/CleanToolRecipe.java
+++ src/main/java/thebetweenlands/common/item/recipe/special/CleanToolRecipe.java
@@ -1,6 +1,7 @@
 package thebetweenlands.common.item.recipe.special;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
@@ -10,7 +11,6 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
 import thebetweenlands.api.capability.corrosion.ICorrosionHandler;
 import thebetweenlands.api.item.CorrosionHelper;
-import thebetweenlands.common.datagen.tags.BLItemTagProvider;
 import thebetweenlands.common.registries.ItemRegistry;
 import thebetweenlands.common.registries.RecipeRegistry;
 
@@ -22,6 +22,42 @@ public class CleanToolRecipe extends CustomRecipe {
 
 	@Override
 	public boolean matches(CraftingInput input, Level level) {
+		ItemStack tool = ItemStack.EMPTY;
+		ICorrosionHandler toolHandler = null;
+		int sap = 0;
+		
+		for (int i = 0; i < input.size(); ++i) {
+			ItemStack stack = input.getItem(i);
+			if (stack.isEmpty()) {
+				continue;
+			}
+			
+			if (stack.is(ItemRegistry.SAP_SPIT)) {
+				sap++;
+			} else if (!tool.isEmpty()) {
+				return false; // Only accept 1 tool
+			} else {
+				ICorrosionHandler handler = CorrosionHelper.getCorrosionHandler(stack);
+				if(handler == null || handler.getCorrosion() <= 0)
+					return false;
+				tool = stack;
+				toolHandler = handler;
+			}
+		}
+
+		// If we have a tool and at least one sap spit
+		if(!tool.isEmpty() && sap > 0 && toolHandler != null) {
+			// Figure out how much cleaning to attempt
+			int cleaning = Mth.ceil(sap * toolHandler.getMaxCorrosion() / 3.0f);
+			// Only match if the tool can have corrosion removed
+			return toolHandler.removeCorrosion(cleaning, true) > 0;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public ItemStack assemble(CraftingInput input, HolderLookup.Provider registries) {
 		int sap = 0;
 		ItemStack tool = ItemStack.EMPTY;
 		for (int i = 0; i < input.size(); ++i) {
@@ -32,36 +68,88 @@ public class CleanToolRecipe extends CustomRecipe {
 			
 			if (stack.is(ItemRegistry.SAP_SPIT)) {
 				sap++;
-			} else if (!tool.isEmpty()) {
-				return false;
-			} else {
-				ICorrosionHandler handler = CorrosionHelper.getCorrosionHandler(stack);
-				if(handler == null || handler.getCorrosion() <= 0)
-					return false;
+			} else if(!tool.isEmpty()) {
+				return ItemStack.EMPTY; // Only accept 1 tool
+			} else if (CorrosionHelper.isCorrodible(stack)) {
 				tool = stack;
 			}
 		}
-		return sap > 0 && !tool.isEmpty();
-	}
-
-	@Override
-	public ItemStack assemble(CraftingInput input, HolderLookup.Provider registries) {
-		int sap = 0;
-		ItemStack tool = ItemStack.EMPTY;
-		for (int i = 0; i < input.size(); ++i) {
-			ItemStack stack = input.getItem(i);
-			if (!stack.isEmpty()) {
-				if (stack.is(ItemRegistry.SAP_SPIT)) {
-					sap++;
-				} else if (stack.is(BLItemTagProvider.CORRODIBLE)) {
-					tool = stack;
-				}
-			}
-		}
+		
 		tool = tool.copy();
 		ICorrosionHandler handler = CorrosionHelper.getCorrosionHandler(tool);
 		handler.removeCorrosion(Mth.ceil(sap * handler.getMaxCorrosion() / 3.0f), false);
 		return tool;
+	}
+	
+	@Override
+	public NonNullList<ItemStack> getRemainingItems(CraftingInput input) {
+		final int inputSize = input.size();
+        NonNullList<ItemStack> nonnulllist = NonNullList.withSize(inputSize, ItemStack.EMPTY);
+
+		int sap = 0;
+		ItemStack tool = ItemStack.EMPTY;
+		for (int i = 0; i < inputSize; ++i) {
+			ItemStack stack = input.getItem(i);
+			if (stack.isEmpty()) {
+				continue;
+			}
+			
+			if (stack.is(ItemRegistry.SAP_SPIT)) {
+				sap++;
+			} else if(tool.isEmpty() && CorrosionHelper.isCorrodible(stack)) {
+				tool = stack; // Only accept 1 tool
+			}
+		}
+		
+		ICorrosionHandler handler = CorrosionHelper.getCorrosionHandler(tool);
+		final int currentCorrosion = handler.getCorrosion();
+		final int maxCorrosion = handler.getMaxCorrosion();
+
+		// If the tool doesn't end up fully cleaned, all sap spit is consumed
+		{
+			// How much the recipe attempts to clean the tool
+			final int totalCleaning = Mth.ceil(sap * maxCorrosion / 3.0f);
+			// How much it actually gets cleaned by
+			final int actualTotalCleaning = handler.removeCorrosion(totalCleaning, true);
+
+			// If the tool doesn't end up fully cleaned, all sap spit is consumed
+			if(currentCorrosion - actualTotalCleaning > 0) {
+				return nonnulllist;
+			}
+		}
+
+		// The tool *was* fully cleaned by the sap spit, meaning there might be sap spit that went unused
+		int sapChecked = 0;
+		boolean fullyCleaned = false;
+        for (int i = 0; i < inputSize; i++) {
+			ItemStack stack = input.getItem(i);
+			if (stack.isEmpty()) {
+				continue;
+			}
+			
+			if (stack.is(ItemRegistry.SAP_SPIT)) {
+				if(fullyCleaned) {
+					// If we've met the cleaning quota, sap spit is no longer consumed
+					nonnulllist.set(i, stack.copyWithCount(1));
+					continue;
+				}
+				
+				++sapChecked;
+
+				// How much this amount of sap spit attempts to clean the tool
+				final int partialCleaning = Mth.ceil(sapChecked * maxCorrosion / 3.0f);
+				// How much this amount of sap spit actually cleans the tool
+				final int actualPartialCleaning = handler.removeCorrosion(partialCleaning, true);
+				
+				// If the crystal ends up fully cleaned due to this amount of sap spit,
+				// then we don't consume any further sap spit
+				if(currentCorrosion - actualPartialCleaning <= 0) {
+					fullyCleaned = true;
+				}
+			}
+        }
+
+        return nonnulllist;
 	}
 
 	@Override
