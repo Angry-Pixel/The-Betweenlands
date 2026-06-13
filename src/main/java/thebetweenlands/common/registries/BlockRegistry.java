fb89a78cf6d8e3a8c1a8f3efcb993eb00cd109e2
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2026-06-13 11:49:18 +1000

    Allow players to stand inside BL portals

--- src/main/java/thebetweenlands/common/registries/BlockRegistry.java
+++ src/main/java/thebetweenlands/common/registries/BlockRegistry.java
@@ -800,7 +800,7 @@ public class BlockRegistry {
 	public static final DeferredBlock<Block> SNOW = register("snow", () -> new BLSnowLayerBlock(BlockBehaviour.Properties.of().replaceable().forceSolidOff().randomTicks().strength(0.1F).sound(SoundType.SNOW)));
 	//MISC
 	public static final DeferredBlock<Block> PORTAL_LOG = register("portal_log", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASS).strength(2.0F).sound(SoundType.WOOD)));
-	public static final DeferredBlock<Block> PORTAL = register("portal", () -> new TreePortalBlock(BlockBehaviour.Properties.of().noLootTable().strength(-1.0F, 6000000.0F).lightLevel(value -> 15).sound(SoundType.GLASS)));
+	public static final DeferredBlock<Block> PORTAL = register("portal", () -> new TreePortalBlock(BlockBehaviour.Properties.of().noLootTable().strength(-1.0F, 6000000.0F).lightLevel(value -> 15).sound(SoundType.GLASS).noOcclusion().noCollission().isSuffocating(BlockRegistry::never)));
 	public static final DeferredBlock<Block> PORTAL_FRAME_TOP_LEFT = register("portal_frame_top_left", () -> new PortalFrameBlock(BlockBehaviour.Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASS).strength(2.0F).sound(SoundType.WOOD)));
 	public static final DeferredBlock<Block> PORTAL_FRAME_TOP = register("portal_frame_top", () -> new PortalFrameBlock(BlockBehaviour.Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASS).strength(2.0F).sound(SoundType.WOOD)));
 	public static final DeferredBlock<Block> PORTAL_FRAME_TOP_RIGHT = register("portal_frame_top_right", () -> new PortalFrameBlock(BlockBehaviour.Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASS).strength(2.0F).sound(SoundType.WOOD)));
