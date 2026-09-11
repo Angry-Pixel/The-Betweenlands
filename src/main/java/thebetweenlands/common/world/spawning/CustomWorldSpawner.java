0fe209718d5418124b45cd3bd212ce3183944be8
vadis365 <6518708+vadis365@users.noreply.github.com>
2026-09-11 16:07:01 +0100

    Spawn caps

--- src/main/java/thebetweenlands/common/world/spawning/CustomWorldSpawner.java
+++ src/main/java/thebetweenlands/common/world/spawning/CustomWorldSpawner.java
@@ -3,6 +3,7 @@ package thebetweenlands.common.world.spawning;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -29,8 +30,57 @@ import thebetweenlands.common.registries.DataMapRegistry;
 import thebetweenlands.common.world.gen.SurfaceType;
 
 public class CustomWorldSpawner {
+	// TODO these numbers are sort of a smudged and simpler analogue to what vanilla counts are in 1.12.2
+	// we will probably need to adjust them a lot and make it some sort of config or data (again)
+    public static final int HOSTILE_CAP_PER_PLAYER = 70; 
+    public static final int CREATURE_CAP_PER_PLAYER = 15;
+    public static final int AMBIENT_CAP_PER_PLAYER = 15;
+
+    public static int getMaxHostileCap(ServerLevel level) {
+        int playerQuantity = level.players().size();
+        return playerQuantity * HOSTILE_CAP_PER_PLAYER;
+    }
+
+    public static int getMaxCreatureCap(ServerLevel level) {
+        int playerQuantity = level.players().size();
+        return playerQuantity * CREATURE_CAP_PER_PLAYER;
+    }
+
+    public static int getMaxAmbientCap(ServerLevel level) {
+        int playerQuantity = level.players().size();
+        return playerQuantity * AMBIENT_CAP_PER_PLAYER;
+    }
+    
+    public static  boolean isUnderMobCap(ServerLevel level, MobCategory category) {
+        int maxAllowed = 0;
+        
+        if (category == MobCategory.MONSTER)
+            maxAllowed = getMaxHostileCap(level);
+        else if (category == MobCategory.CREATURE || category == MobCategory.WATER_CREATURE)
+            maxAllowed = getMaxCreatureCap(level);
+        else if (category == MobCategory.AMBIENT || category == MobCategory.WATER_AMBIENT)
+            maxAllowed = getMaxAmbientCap(level);
+        else
+            return true;
+
+        AtomicInteger currentCount = new AtomicInteger(0);
+
+        level.getEntities().getAll().forEach(entity -> {
+            if (entity instanceof Mob mob && mob.getType().getCategory() == category)
+                currentCount.incrementAndGet();
+        });
+
+        return currentCount.get() < maxAllowed;
+    }
 
 	public static void spawnNearPlayer(ServerLevel level, BlockPos playerPos) {
+	/*	// just here to stop all the logic if the cap is already reached, so moved it,
+		// for all types, to the weighted list logic
+	    if (!isUnderMobCap(level, MobCategory.MONSTER)) {
+	    	System.out.println("[SPAWNER DEBUG] Hostile Spawn Cap reached!!! ");
+	        return;
+	        }
+*/
 	    Holder<Biome> playerBiomeHolder = level.getBiome(playerPos);
 	    var optionalKey = playerBiomeHolder.unwrapKey();
 	    if (optionalKey.isEmpty())
@@ -54,38 +104,31 @@ public class CustomWorldSpawner {
 				if (level.getRandom().nextInt(Math.max(1, mobSpec.spawningInterval())) != 0)
 					continue;
 
-				if (mobSpec.mobType().getCategory() == MobCategory.AMBIENT || mobSpec.mobType().getCategory() == MobCategory.WATER_AMBIENT)
+				if ((mobSpec.mobType().getCategory() == MobCategory.AMBIENT || mobSpec.mobType().getCategory() == MobCategory.WATER_AMBIENT) && isUnderMobCap(level, MobCategory.AMBIENT))
 					Ambientpool.add(WeightedEntry.wrap(mobSpec, mobSpec.weight()));
 
-				else if (mobSpec.mobType().getCategory() == MobCategory.MONSTER)
+				else if (mobSpec.mobType().getCategory() == MobCategory.MONSTER && isUnderMobCap(level, MobCategory.MONSTER))
 					Hostilepool.add(WeightedEntry.wrap(mobSpec, mobSpec.weight()));
 
-				else
+				else if (isUnderMobCap(level, mobSpec.mobType().getCategory())) // dumps everything else here atm
 					Creaturepool.add(WeightedEntry.wrap(mobSpec, mobSpec.weight()));
-
 			}
 
-			if (Ambientpool.isEmpty())
-				continue;
-
-			spawnCatagoryType(level, Ambientpool, playerPos, zone, "Ambient");
+			if (!Ambientpool.isEmpty())
+				preSpawnPosCheck(level, Ambientpool, playerPos, zone, "Ambient");
 			
 
-			if (Creaturepool.isEmpty())
-				continue;
+			if (!Creaturepool.isEmpty())
+				preSpawnPosCheck(level, Creaturepool, playerPos, zone, "Creature");
 
-			spawnCatagoryType(level, Creaturepool, playerPos, zone, "Creature");
-
-			if (Hostilepool.isEmpty())
-				continue;
-
-			spawnCatagoryType(level, Hostilepool, playerPos, zone, "Hostile");
+			if (!Hostilepool.isEmpty())
+				preSpawnPosCheck(level, Hostilepool, playerPos, zone, "Hostile");
 
 			return;
 		}
 	}
 
-	private static void spawnCatagoryType(ServerLevel level, List<Wrapper<BaseSpawnProperties>> pool, BlockPos playerPos, BiomeSpawnerZone zone, String string) {
+	private static void preSpawnPosCheck(ServerLevel level, List<Wrapper<BaseSpawnProperties>> pool, BlockPos playerPos, BiomeSpawnerZone zone, String string) {
 		 System.out.println("[SPAWNER DEBUG] Checking weighted list for: " + string);
 		Optional<WeightedEntry.Wrapper<BaseSpawnProperties>> selectedSpec = WeightedRandom.getRandomItem(level.getRandom(), pool);
 		 if (selectedSpec.isPresent()) {
@@ -258,7 +301,8 @@ public class CustomWorldSpawner {
 
 			if (SpawnPlacements.checkSpawnRules(type, level, MobSpawnType.NATURAL, finalSpawnPos, level.getRandom())) {
 				Entity entity = type.create(level);
-				if (entity instanceof Mob mob) {
+				// double check for sanity in the case of something else happening that spawns shit
+				if (entity instanceof Mob mob && isUnderMobCap(level, mob.getType().getCategory())) {
 					mob.moveTo(finalSpawnPos.getX() + 0.5, finalSpawnPos.getY(), finalSpawnPos.getZ() + 0.5, level.random.nextFloat() * 360F, 0.0F);
 					mob.finalizeSpawn(level, level.getCurrentDifficultyAt(finalSpawnPos), MobSpawnType.NATURAL, null);
 					if (level.addFreshEntity(mob)) {
